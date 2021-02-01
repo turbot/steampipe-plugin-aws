0b7681a507d22b373cd1babf56710d2fcfca76c8
LalitTurbot <lalit@turbot.com>
2021-02-01 15:51:08 +0530

    update docs and test case

--- aws-test/tests/aws_iam_user/test-hydrate-query.sql
+++ aws-test/tests/aws_iam_user/test-hydrate-query.sql
@@ -1,3 +1,3 @@
-select name, arn, inline_policies, attached_policy_arns, permissions_boundary_arn, permissions_boundary_type, tags_src
+select name, arn, inline_policies, attached_policy_arns, permissions_boundary_arn, permissions_boundary_type, mfa_enabled, tags_src
 from aws.aws_iam_user
 where name = '{{ resourceName }}'
