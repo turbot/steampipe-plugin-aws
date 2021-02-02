81ce0754cc4f255348062de249f65c7cac62ae5c
LalitTurbot <lalit@turbot.com>
2021-02-02 21:10:18 +0530

    add more regions to list

--- aws/multi_region.go
+++ aws/multi_region.go
@@ -1,10 +1,15 @@
 package aws
 
 var regions = []string{
-	"us-east-1",
-	"us-east-2",
+	"eu-north-1",
+	"eu-south-1",
 	"eu-west-1",
 	"eu-west-2",
+	"eu-west-3",
+	"us-east-1",
+	"us-east-2",
+	"us-west-1",
+	"us-west-2",
 }
 
 const fetchMetdataKeyRegion = "region"
