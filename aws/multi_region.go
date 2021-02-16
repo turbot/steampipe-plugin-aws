c7c46e5fb22a35bb22ad96d0d81f779476089aa3
LalitTurbot <lalit@turbot.com>
2021-02-16 17:28:33 +0530

    update BuildRegionList function for no region in the config

--- aws/multi_region.go
+++ aws/multi_region.go
@@ -11,10 +11,18 @@ const matrixKeyRegion = "region"
 // BuildRegionList :: return a list of matrix items, one per region specified in the connection config
 func BuildRegionList(_ context.Context, connection *plugin.Connection) []map[string]interface{} {
 	// retrieve regions from connection config
-	regions := GetConfig(connection).Regions
-	matrix := make([]map[string]interface{}, len(regions))
-	for i, region := range regions {
-		matrix[i] = map[string]interface{}{matrixKeyRegion: region}
+	awsConfig := GetConfig(connection)
+
+	if &awsConfig != nil && awsConfig.Regions != nil {
+		regions := GetConfig(connection).Regions
+		matrix := make([]map[string]interface{}, len(regions))
+		for i, region := range regions {
+			matrix[i] = map[string]interface{}{matrixKeyRegion: region}
+		}
+		return matrix
 	}
-	return matrix
+
+	return []map[string]interface{}{
+		{matrixKeyRegion: GetDefaultRegion()},
+	}
 }
