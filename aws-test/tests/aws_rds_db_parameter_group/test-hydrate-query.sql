ee4bc3704a6e5c1cb264c09de4de90d442f0befd
Rajesh Bal <46851617+rajeshbal65@users.noreply.github.com>
2021-02-18 19:08:08 +0530

    Fix: aws_rds_* tables should have `tags_src` column (not `tag_list`). Closes #56 (#61)

--- aws-test/tests/aws_rds_db_parameter_group/test-hydrate-query.sql
+++ aws-test/tests/aws_rds_db_parameter_group/test-hydrate-query.sql
@@ -1,3 +1,8 @@
-select name, parameters, tag_list
-from aws.aws_rds_db_parameter_group
-where name = '{{ resourceName }}'
+SELECT
+  name,
+  parameters,
+  tags_src
+FROM
+  aws.aws_rds_db_parameter_group
+WHERE
+  name = '{{ resourceName }}'
