b2f88806a623a7103a686f54e143cb179ba7b48f
LalitTurbot <lalit@turbot.com>
2021-02-18 21:15:35 +0530

    update multi-region code to validate for regions list passed in the config

--- aws/multi_region.go
+++ aws/multi_region.go
@@ -2,7 +2,9 @@ package aws
 
 import (
 	"context"
+	"strings"
 
+	"github.com/turbot/go-kit/helpers"
 	"github.com/turbot/steampipe-plugin-sdk/plugin"
 )
 
@@ -15,6 +17,12 @@ func BuildRegionList(_ context.Context, connection *plugin.Connection) []map[str
 
 	if &awsConfig != nil && awsConfig.Regions != nil {
 		regions := GetConfig(connection).Regions
+
+		if len(getInvalidRegions(regions)) > 0 {
+			panic("\n\nConnection config have invalid regions: " + strings.Join(getInvalidRegions(regions), ","))
+		}
+
+		// validate regions list
 		matrix := make([]map[string]interface{}, len(regions))
 		for i, region := range regions {
 			matrix[i] = map[string]interface{}{matrixKeyRegion: region}
@@ -25,4 +33,17 @@ func BuildRegionList(_ context.Context, connection *plugin.Connection) []map[str
 	return []map[string]interface{}{
 		{matrixKeyRegion: GetDefaultRegion()},
 	}
+}
+
+func getInvalidRegions(regions []string) []string {
+	awsRegions := []string{
+		"af-south-1", "ap-east-1", "ap-northeast-1", "ap-northeast-2", "ap-south-1", "ap-southeast-1", "ap-southeast-2", "ca-central-1", "eu-central-1", "eu-north-1", "eu-south-1", "eu-west-1", "eu-west-2", "eu-west-3", "me-south-1", "sa-east-1", "us-east-1", "us-east-2", "us-west-1", "us-west-2", "us-gov-east-1", "us-gov-west-1", "cn-north-1", "cn-northwest-1"}
+
+	invalidRegions := []string{}
+	for _, region := range regions {
+		if !helpers.StringSliceContains(awsRegions, region) {
+			invalidRegions = append(invalidRegions, region)
+		}
+	}
+	return invalidRegions
 }
