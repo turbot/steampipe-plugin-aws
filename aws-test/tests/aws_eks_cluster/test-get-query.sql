6d4fd1cf0a6b837105d0ab106e7121536cd17067
rajeshbal65 <rajesh.bal@turbot.com>
2021-03-16 16:00:52 +0530

    update

--- aws-test/tests/aws_eks_cluster/test-get-query.sql
+++ aws-test/tests/aws_eks_cluster/test-get-query.sql
@@ -1,3 +1,3 @@
 select name, arn, endpoint, role_arn, version, platform_version, status
 from aws.aws_eks_cluster
-where name = '{{ resourceName }}'
+where name = '{{ resourceName }}';
--- aws-test/tests/aws_eks_cluster/test-turbot-query.sql
+++ aws-test/tests/aws_eks_cluster/test-turbot-query.sql
@@ -1,3 +1,3 @@
 select name, title, tags, akas, account_id
 from aws.aws_eks_cluster
-where arn = '{{ output.resource_aka.value }}'
+where arn = '{{ output.resource_aka.value }}';
