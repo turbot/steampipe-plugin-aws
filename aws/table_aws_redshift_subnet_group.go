913814ec23e349da4bfe17b211233fc5211c3805
Turbot <paulami@turbot.com>
2021-03-19 17:08:06 +0530

    Add table aws_redshift_subnet_group closes #198

---
+++ aws/table_aws_redshift_subnet_group.go
@@ -0,0 +1,182 @@
+package aws
+
+import (
+	"context"
+
+	"github.com/turbot/steampipe-plugin-sdk/grpc/proto"
+	"github.com/turbot/steampipe-plugin-sdk/plugin/transform"
+
+	"github.com/aws/aws-sdk-go/aws"
+	"github.com/aws/aws-sdk-go/service/redshift"
+	"github.com/turbot/steampipe-plugin-sdk/plugin"
+)
+
+//// TABLE DEFINITION
+
+func tableAwsRedshiftSubnetGroup(_ context.Context) *plugin.Table {
+	return &plugin.Table{
+		Name:        "aws_redshift_subnet_group",
+		Description: "AWS Redshift Subnet Group",
+		Get: &plugin.GetConfig{
+			KeyColumns:        plugin.SingleColumn("cluster_subnet_group_name"),
+			ShouldIgnoreError: isNotFoundError([]string{"ClusterSubnetGroupNotFoundFault"}),
+			Hydrate:           getRedshiftSubnetGroup,
+		},
+		List: &plugin.ListConfig{
+			Hydrate: listRedshiftSubnetGroup,
+		},
+		GetMatrixItem: BuildRegionList,
+		Columns: awsRegionalColumns([]*plugin.Column{
+			{
+				Name:        "cluster_subnet_group_name",
+				Description: "The name of the cluster subnet group.",
+				Type:        proto.ColumnType_STRING,
+			},
+			{
+				Name:        "description",
+				Description: "The description of the cluster subnet group.",
+				Type:        proto.ColumnType_STRING,
+			},
+			{
+				Name:        "subnet_group_status",
+				Description: "The status of the cluster subnet group.",
+				Type:        proto.ColumnType_STRING,
+			},
+			{
+				Name:        "vpc_id",
+				Description: "The VPC ID of the cluster subnet group.",
+				Type:        proto.ColumnType_STRING,
+			},
+			{
+				Name:        "subnets",
+				Description: "A list of the VPC Subnet elements.",
+				Type:        proto.ColumnType_JSON,
+			},
+			{
+				Name:        "tags_src",
+				Description: "A list of tags attached to the subnet group.",
+				Type:        proto.ColumnType_JSON,
+				Hydrate:     getRedshiftSubnetGroup,
+				Transform:   transform.FromField("Tags"),
+			},
+
+			// Standard columns
+			{
+				Name:        "tags",
+				Description: resourceInterfaceDescription("tags"),
+				Type:        proto.ColumnType_JSON,
+				Transform:   transform.From(getRedshiftSubnetGroupTurbotTags),
+			},
+			{
+				Name:        "title",
+				Description: resourceInterfaceDescription("title"),
+				Type:        proto.ColumnType_STRING,
+				Transform:   transform.FromField("ClusterSubnetGroupName"),
+			},
+			{
+				Name:        "akas",
+				Description: resourceInterfaceDescription("akas"),
+				Type:        proto.ColumnType_JSON,
+				Hydrate:     getRedshiftSubnetGroupAkas,
+				Transform:   transform.FromValue(),
+			},
+		}),
+	}
+}
+
+//// LIST FUNCTION
+
+func listRedshiftSubnetGroup(ctx context.Context, d *plugin.QueryData, _ *plugin.HydrateData) (interface{}, error) {
+	// TODO put me in helper function
+	var region string
+	matrixRegion := plugin.GetMatrixItem(ctx)[matrixKeyRegion]
+	if matrixRegion != nil {
+		region = matrixRegion.(string)
+	}
+	plugin.Logger(ctx).Trace("listRedshiftSubnetGroup", "AWS_REGION", region)
+
+	// Create Session
+	svc, err := RedshiftService(ctx, d, region)
+	if err != nil {
+		return nil, err
+	}
+
+	// List call
+	err = svc.DescribeClusterSubnetGroupsPages(
+		&redshift.DescribeClusterSubnetGroupsInput{},
+		func(page *redshift.DescribeClusterSubnetGroupsOutput, isLast bool) bool {
+			for _, subnetGroup := range page.ClusterSubnetGroups {
+				d.StreamListItem(ctx, subnetGroup)
+			}
+			return !isLast
+		},
+	)
+	return nil, err
+}
+
+//// HYDRATE FUNCTIONS
+
+func getRedshiftSubnetGroup(ctx context.Context, d *plugin.QueryData, _ *plugin.HydrateData) (interface{}, error) {
+	// TODO put me in helper function
+	var region string
+	matrixRegion := plugin.GetMatrixItem(ctx)[matrixKeyRegion]
+	if matrixRegion != nil {
+		region = matrixRegion.(string)
+	}
+	clusterSubnetGroupName := d.KeyColumnQuals["cluster_subnet_group_name"].GetStringValue()
+
+	// Create service
+	svc, err := RedshiftService(ctx, d, region)
+	if err != nil {
+		return nil, err
+	}
+
+	params := &redshift.DescribeClusterSubnetGroupsInput{
+		ClusterSubnetGroupName: aws.String(clusterSubnetGroupName),
+	}
+
+	op, err := svc.DescribeClusterSubnetGroups(params)
+	if err != nil {
+		return nil, err
+	}
+
+	if op.ClusterSubnetGroups != nil && len(op.ClusterSubnetGroups) > 0 {
+		return op.ClusterSubnetGroups[0], nil
+	}
+	return nil, nil
+}
+
+//// TRANSFORM FUNCTIONS
+
+func getRedshiftSubnetGroupTurbotTags(_ context.Context, d *transform.TransformData) (interface{}, error) {
+	data := d.HydrateItem.(*redshift.ClusterSubnetGroup)
+	if data.Tags == nil {
+		return nil, nil
+	}
+	// Get the resource tags
+	var turbotTagsMap map[string]string
+	if data.Tags != nil {
+		turbotTagsMap = map[string]string{}
+		for _, i := range data.Tags {
+			turbotTagsMap[*i.Key] = *i.Value
+		}
+	}
+	return turbotTagsMap, nil
+}
+
+func getRedshiftSubnetGroupAkas(ctx context.Context, d *plugin.QueryData, h *plugin.HydrateData) (interface{}, error) {
+	plugin.Logger(ctx).Trace("getRedshiftSubnetGroupAkas")
+	data := h.Item.(*redshift.ClusterSubnetGroup)
+	commonData, err := getCommonColumns(ctx, d, h)
+	if err != nil {
+		return nil, err
+	}
+	commonColumnData := commonData.(*awsCommonColumnData)
+
+	arn := "arn:" + commonColumnData.Partition + ":redshift:" + commonColumnData.Region + ":" + commonColumnData.AccountId + ":subnetgroup:" + *data.ClusterSubnetGroupName
+
+	// Get data for turbot defined properties
+	akas := []string{arn}
+
+	return akas, nil
+}
---
+++ docs/tables/aws_redshift_subnet_group.md
@@ -0,0 +1,44 @@
+# Table: aws_redshift_subnet_group
+
+A cluster subnet group is a collection of subnets (typically private) that are created for a VPC and then designated for redshift clusters.
+
+## Examples
+
+### Basic subnet group info
+
+```sql
+select
+  cluster_subnet_group_name,
+  description,
+  subnet_group_status,
+  vpc_id
+from
+  aws_redshift_subnet_group;
+```
+
+
+### Get each subnet info in the subnet group
+
+```sql
+select
+  cluster_subnet_group_name,
+  subnet -> 'SubnetAvailabilityZone' ->> 'Name' as subnet_availability_zone,
+  subnet -> 'SubnetAvailabilityZone' ->> 'SupportedPlatforms' as supported_platforms,
+  subnet ->> 'SubnetIdentifier' as subnet_identifier,
+  subnet ->> 'SubnetStatus' as subnet_status
+from
+  aws_redshift_subnet_group
+  cross join jsonb_array_elements(subnets) as subnet;
+```
+
+
+### List subnet groups without application tag key
+
+```sql
+select
+  name,
+  tags
+from
+  aws_redshift_subnet_group
+where
+  not tags
