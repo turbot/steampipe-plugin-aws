1e6e45c1d93b70dd448dae42a45a8128888483f7
Subhajit Kumar Mondal <subhajit@turbot.com>
2021-03-23 21:13:17 +0530

    Add table aws_wafv2_web_acl. Closes #224

--- aws/table_aws_wafv2_web_acl.go
+++ aws/table_aws_wafv2_web_acl.go
@@ -26,7 +26,7 @@ func tableAwsWafv2WebAcl(_ context.Context) *plugin.Table {
 			Hydrate: listAwsWafv2WebAcl,
 		},
 		GetMatrixItem: BuildRegionList,
-		Columns: awsRegionalColumns([]*plugin.Column{
+		Columns: []*plugin.Column{
 			{
 				Name:        "name",
 				Description: "The name of the Web ACL. You cannot change the name of a Web ACL after you create it.",
@@ -35,7 +35,7 @@ func tableAwsWafv2WebAcl(_ context.Context) *plugin.Table {
 			{
 				Name:        "arn",
 				Description: "The Amazon Resource Name (ARN) of the entity.",
-				Type:        proto.ColumnType_TIMESTAMP,
+				Type:        proto.ColumnType_STRING,
 				Transform:   transform.FromField("ARN"),
 			},
 			{
@@ -44,19 +44,19 @@ func tableAwsWafv2WebAcl(_ context.Context) *plugin.Table {
 				Type:        proto.ColumnType_STRING,
 			},
 			{
+				Name:        "scope",
+				Description: "Specifies the scope of the Web ACL. Possibles values are: 'REGIONAL' and 'CLOUDFRONT'.",
+				Type:        proto.ColumnType_STRING,
+				Transform:   transform.From(webAclLocation),
+			},
+			{
 				Name:        "description",
 				Description: "A description of the Web ACL that helps with identification.",
 				Type:        proto.ColumnType_STRING,
 			},
 			{
-				Name:        "scope",
-				Description: "A description of the Web ACL that helps with identification.",
-				Type:        proto.ColumnType_STRING,
-				Transform:   transform.From(webAclLocation),
-			},
-			{
 				Name:        "capacity",
-				Description: "The web ACL capacity units (WCUs) currently being used by this web ACL.",
+				Description: "The Web ACL capacity units(WCUs) currently being used by this resource.",
 				Type:        proto.ColumnType_INT,
 				Hydrate:     getAwsWafv2WebAcl,
 			},
@@ -69,10 +69,11 @@ func tableAwsWafv2WebAcl(_ context.Context) *plugin.Table {
 				Name:        "managed_by_firewall_manager",
 				Description: "Indicates whether this web ACL is managed by AWS Firewall Manager.",
 				Type:        proto.ColumnType_BOOL,
+				Hydrate:     getAwsWafv2WebAcl,
 			},
 			{
 				Name:        "default_action",
-				Description: "DefaultAction is a required field.",
+				Description: "The action to perform if none of the Rules contained in the Web ACL match.",
 				Type:        proto.ColumnType_JSON,
 				Hydrate:     getAwsWafv2WebAcl,
 			},
@@ -100,34 +101,55 @@ func tableAwsWafv2WebAcl(_ context.Context) *plugin.Table {
 				Type:        proto.ColumnType_JSON,
 				Hydrate:     getAwsWafv2WebAcl,
 			},
-			// {
-			// 	Name:        "tags_src",
-			// 	Description: "A list of tags associated with the vault.",
-			// 	Type:        proto.ColumnType_JSON,
-			// 	Hydrate:     listTagsForAwsWafv2WebAcl,
-			// 	Transform:   transform.FromField("TagList"),
-			// },
+			{
+				Name:        "tags_src",
+				Description: "A list of tags associated with the resource.",
+				Type:        proto.ColumnType_JSON,
+				Hydrate:     listTagsForAwsWafv2WebAcl,
+				Transform:   transform.FromField("TagInfoForResource.TagList"),
+			},
 
-			// Standard columns
+			// steampipe standard columns
 			{
 				Name:        "title",
 				Description: resourceInterfaceDescription("title"),
 				Type:        proto.ColumnType_STRING,
 				Transform:   transform.FromField("Name"),
 			},
-			// {
-			// 	Name:        "tags",
-			// 	Description: resourceInterfaceDescription("tags"),
-			// 	Type:        proto.ColumnType_JSON,
-			// 	Transform:   transform.FromField("TagList").Transform(webAclTagListToTurbotTags),
-			// },
+			{
+				Name:        "tags",
+				Description: resourceInterfaceDescription("tags"),
+				Type:        proto.ColumnType_JSON,
+				Hydrate:     listTagsForAwsWafv2WebAcl,
+				Transform:   transform.FromField("TagInfoForResource.TagList").Transform(webAclTagListToTurbotTags),
+			},
 			{
 				Name:        "akas",
 				Description: resourceInterfaceDescription("akas"),
 				Type:        proto.ColumnType_JSON,
 				Transform:   transform.FromField("ARN").Transform(arnToAkas),
 			},
-		}),
+
+			// aws standard columns
+			{
+				Name:        "partition",
+				Description: "The AWS partition in which the resource is located (aws, aws-cn, or aws-us-gov).",
+				Type:        proto.ColumnType_STRING,
+				Hydrate:     getCommonColumns,
+			},
+			{
+				Name:        "region",
+				Description: "The AWS Region in which the resource is located.",
+				Type:        proto.ColumnType_STRING,
+				Transform:   transform.From(webAclRegion),
+			},
+			{
+				Name:        "account_id",
+				Description: "The AWS Account ID in which the resource is located.",
+				Type:        proto.ColumnType_STRING,
+				Hydrate:     getCommonColumns,
+			},
+		},
 	}
 }
 
@@ -142,93 +164,103 @@ func listAwsWafv2WebAcl(ctx context.Context, d *plugin.QueryData, _ *plugin.Hydr
 	}
 	plugin.Logger(ctx).Trace("listAwsWafv2WebAcl", "AWS_REGION", region)
 
-	if region != "us-east-1"{
-		return nil, nil
-	}
 	// Create session
 	svc, err := WAFv2Service(ctx, d, region)
 	if err != nil {
 		return nil, err
 	}
 
-	// List call
-	regionalParam := "REGIONAL"
-	param := &wafv2.ListWebACLsInput{
-		Scope: &regionalParam,
+	// List all regional web acls
+	pagesLeft := true
+	regionalWebAclParams := &wafv2.ListWebACLsInput{
+		Scope: aws.String("REGIONAL"),
 	}
-	for {
-		response, err := svc.ListWebACLs(param)
+	for pagesLeft {
+		response, err := svc.ListWebACLs(regionalWebAclParams)
 		if err != nil {
 			return nil, err
 		}
+
 		for _, reginalWebACLs := range response.WebACLs {
 			d.StreamListItem(ctx, reginalWebACLs)
 		}
-		if response.NextMarker == nil {
-			break
-		}
-	}
-	// globalParam := "CLOUDFRONT"
-	param2 := &wafv2.ListWebACLsInput{
-		Scope: aws.String("CLOUDFRONT"),
-	}
-	for {
-		response, err := svc.ListWebACLs(param2)
-		if err != nil {
-			return nil, err
-		}
-		for _, globalWebACLs := range response.WebACLs {
-			d.StreamListItem(ctx, globalWebACLs)
-		}
-		if response.NextMarker == nil {
-			break
+
+		if response.NextMarker != nil {
+			pagesLeft = true
+			regionalWebAclParams.NextMarker = response.NextMarker
+		} else {
+			pagesLeft = false
 		}
 	}
 
-	return nil, err
+	// List all global web acls
+	// To work with CloudFront, you must specify the Region US East (N. Virginia)
+	if region == "us-east-1" {
+		pagesLeft = true
+		globalWebAclParams := &wafv2.ListWebACLsInput{
+			Scope: aws.String("CLOUDFRONT"),
+		}
+		for pagesLeft {
+			response, err := svc.ListWebACLs(globalWebAclParams)
+			if err != nil {
+				return nil, err
+			}
+
+			for _, globalWebACLs := range response.WebACLs {
+				d.StreamListItem(ctx, globalWebACLs)
+			}
+
+			if response.NextMarker != nil {
+				pagesLeft = true
+				globalWebAclParams.NextMarker = response.NextMarker
+			} else {
+				pagesLeft = false
+			}
+		}
+	}
+
+	return nil, nil
 }
 
 //// HYDRATE FUNCTIONS
 
 func getAwsWafv2WebAcl(ctx context.Context, d *plugin.QueryData, h *plugin.HydrateData) (interface{}, error) {
-	logger := plugin.Logger(ctx)
-	logger.Trace("getAwsWafv2WebAcl")
+	plugin.Logger(ctx).Trace("getAwsWafv2WebAcl")
 
+	// TODO put me in helper function
 	var region string
 	matrixRegion := plugin.GetMatrixItem(ctx)[matrixKeyRegion]
 	if matrixRegion != nil {
 		region = matrixRegion.(string)
 	}
-	var id string
+
+	var id, name, scope string
 	if h.Item != nil {
-		id = webAclId(h.Item)
+		data := webAclData(h.Item)
+		id = data["ID"]
+		name = data["Name"]
+		locationType := strings.Split(strings.Split(string(data["Arn"]), ":")[5], "/")[0]
+
+		if locationType == "regional" {
+			scope = "REGIONAL"
+		} else {
+			scope = "CLOUDFRONT"
+		}
 	} else {
 		id = d.KeyColumnQuals["id"].GetStringValue()
-	}
-	var name string
-	if h.Item != nil {
-		name = webAclName(h.Item)
-	} else {
 		name = d.KeyColumnQuals["name"].GetStringValue()
-	}
-	var scope string
-	if h.Item != nil {
-		data := h.Item.(*wafv2.WebACLSummary)
-		scope = strings.Split(string(*data.ARN), ":")[5]
-	} else {
 		scope = d.KeyColumnQuals["scope"].GetStringValue()
-	}
-
-	if scope == "regional" {
-		scope = "REGIONAL"
-	} else {
-		scope = "CLOUDFRONT"
 	}
 
 	// Create Session
 	svc, err := WAFv2Service(ctx, d, region)
 	if err != nil {
 		return nil, err
+	}
+
+	// To work with CloudFront, you must specify the Region US East (N. Virginia)
+	if strings.ToLower(scope) == "cloudfront" && region != "us-east-1" {
+		return nil, nil
 	}
 
 	params := &wafv2.GetWebACLInput{
@@ -239,7 +271,7 @@ func getAwsWafv2WebAcl(ctx context.Context, d *plugin.QueryData, h *plugin.Hydra
 
 	op, err := svc.GetWebACL(params)
 	if err != nil {
-		logger.Debug("GetWebACL", "ERROR", err)
+		plugin.Logger(ctx).Debug("GetWebACL", "ERROR", err)
 		return nil, err
 	}
 
@@ -247,8 +279,7 @@ func getAwsWafv2WebAcl(ctx context.Context, d *plugin.QueryData, h *plugin.Hydra
 }
 
 func listTagsForAwsWafv2WebAcl(ctx context.Context, d *plugin.QueryData, h *plugin.HydrateData) (interface{}, error) {
-	logger := plugin.Logger(ctx)
-	logger.Trace("listTagsForAwsWafv2WebAcl")
+	plugin.Logger(ctx).Trace("listTagsForAwsWafv2WebAcl")
 
 	// TODO put me in helper function
 	var region string
@@ -256,8 +287,13 @@ func listTagsForAwsWafv2WebAcl(ctx context.Context, d *plugin.QueryData, h *plug
 	if matrixRegion != nil {
 		region = matrixRegion.(string)
 	}
+	data := webAclData(h.Item)
+	locationType := strings.Split(strings.Split(string(data["Arn"]), ":")[5], "/")[0]
 
-	data := h.Item.(*wafv2.WebACLSummary)
+	// To work with CloudFront, you must specify the Region US East (N. Virginia)
+	if locationType == "global" && region != "us-east-1" {
+		return nil, nil
+	}
 
 	// Create session
 	svc, err := WAFv2Service(ctx, d, region)
@@ -267,7 +303,7 @@ func listTagsForAwsWafv2WebAcl(ctx context.Context, d *plugin.QueryData, h *plug
 
 	// Build param
 	param := &wafv2.ListTagsForResourceInput{
-		ResourceARN: data.ARN,
+		ResourceARN: aws.String(data["Arn"]),
 	}
 
 	webAclTags, err := svc.ListTagsForResource(param)
@@ -280,27 +316,27 @@ func listTagsForAwsWafv2WebAcl(ctx context.Context, d *plugin.QueryData, h *plug
 //// TRANSFORM FUNCTIONS
 
 func webAclLocation(_ context.Context, d *transform.TransformData) (interface{}, error) {
-	data := d.HydrateItem.(*wafv2.WebACLSummary)
-	loc := strings.Split(string(*data.ARN), ":")[5]
+	data := webAclData(d.HydrateItem)
+	loc := strings.Split(strings.Split(string(data["Arn"]), ":")[5], "/")[0]
 	if loc == "regional" {
 		return "REGIONAL", nil
 	}
-	return "GLOBAL", nil
+	return "CLOUDFRONT", nil
 }
 
 func webAclTagListToTurbotTags(ctx context.Context, d *transform.TransformData) (interface{}, error) {
 	plugin.Logger(ctx).Trace("webAclTagListToTurbotTags")
-	webAclTag := d.HydrateItem.(*wafv2.TagInfoForResource)
+	data := d.HydrateItem.(*wafv2.ListTagsForResourceOutput)
 
-	if webAclTag.TagList == nil {
+	if data.TagInfoForResource.TagList == nil || len(data.TagInfoForResource.TagList) < 1 {
 		return nil, nil
 	}
 
 	// Mapping the resource tags inside turbotTags
 	var turbotTagsMap map[string]string
-	if webAclTag.TagList != nil {
+	if data.TagInfoForResource.TagList != nil {
 		turbotTagsMap = map[string]string{}
-		for _, i := range webAclTag.TagList {
+		for _, i := range data.TagInfoForResource.TagList {
 			turbotTagsMap[*i.Key] = *i.Value
 		}
 	}
@@ -308,22 +344,33 @@ func webAclTagListToTurbotTags(ctx context.Context, d *transform.TransformData) 
 	return turbotTagsMap, nil
 }
 
-func webAclId(item interface{}) string {
-	switch item.(type) {
-	case *wafv2.WebACL:
-		return *item.(*wafv2.WebACL).Id
-	case *wafv2.WebACLSummary:
-		return *item.(*wafv2.WebACLSummary).Id
+func webAclRegion(ctx context.Context, d *transform.TransformData) (interface{}, error) {
+	data := webAclData(d.HydrateItem)
+	loc := strings.Split(strings.Split(string(data["Arn"]), ":")[5], "/")[0]
+
+	var region string
+	matrixRegion := plugin.GetMatrixItem(ctx)[matrixKeyRegion]
+	if matrixRegion != nil {
+		region = matrixRegion.(string)
 	}
-	return ""
+
+	if loc == "global" {
+		return "global", nil
+	}
+	return region, nil
 }
 
-func webAclName(item interface{}) string {
-	switch item.(type) {
+func webAclData(item interface{}) map[string]string {
+	data := map[string]string{}
+	switch item := item.(type) {
 	case *wafv2.WebACL:
-		return *item.(*wafv2.WebACL).Name
+		data["ID"] = *item.Id
+		data["Arn"] = *item.ARN
+		data["Name"] = *item.Name
 	case *wafv2.WebACLSummary:
-		return *item.(*wafv2.WebACLSummary).Name
+		data["ID"] = *item.Id
+		data["Arn"] = *item.ARN
+		data["Name"] = *item.Name
 	}
-	return ""
+	return data
 }
