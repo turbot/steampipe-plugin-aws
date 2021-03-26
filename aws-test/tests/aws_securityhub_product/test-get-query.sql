43568da7a8a1f66049a1547255c832093078d029
rajeshbal65 <rajesh.bal@turbot.com>
2021-03-26 17:05:43 +0530

    update

--- aws-test/tests/aws_securityhub_product/test-get-query.sql
+++ aws-test/tests/aws_securityhub_product/test-get-query.sql
@@ -1,3 +1,3 @@
 select name, product_arn, company_name, description, categories, activation_url
 from aws_securityhub_product
-where product_arn = 'arn:aws:securityhub:us-east-1:324264561773:product/guardicore/aws-infection-monkey';
+where product_arn = 'arn:aws:securityhub:{{ output.aws_region.value }}::product/aws/guardduty';
