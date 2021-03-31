97ada10edc165e50d79dc41bec3313c94ac8e0ca
Anisa Das <anisa@turbot.com>
2021-03-31 14:28:38 +0530

    remove extra space

--- aws-test/tests/aws_redshift_parameter_group/test-list-query.sql
+++ aws-test/tests/aws_redshift_parameter_group/test-list-query.sql
@@ -1,3 +1,3 @@
-select name , partition, region, tags, title
+select name, partition, region, tags, title
 from aws.aws_redshift_parameter_group
 where akas::text = '["{{ output.resource_aka.value }}"]'
