7b0eddb236713de1591e0e73bd5b749c2109e7c4
Anisa Das <anisa@turbot.com>
2021-04-05 15:18:13 +0530

    Add table aws_waf_rule. Closes #260

---
+++ aws/table_aws_waf_rule.go
@@ -0,0 +1,225 @@
+package aws
+
+import (
+	"context"
+
+	"github.com/aws/aws-sdk-go/aws"
+	"github.com/aws/aws-sdk-go/service/waf"
+
+	"github.com/turbot/steampipe-plugin-sdk/grpc/proto"
+	"github.com/turbot/steampipe-plugin-sdk/plugin"
+	"github.com/turbot/steampipe-plugin-sdk/plugin/transform"
+)
+
+func tableAwsWAFRule(_ context.Context) *plugin.Table {
+	return &plugin.Table{
+		Name:        "aws_waf_rule",
+		Description: "AWS WAF Rule",
+		Get: &plugin.GetConfig{
+			KeyColumns:        plugin.SingleColumn("rule_id"),
+			ShouldIgnoreError: isNotFoundError([]string{"WAFNonexistentItemException"}),
+			Hydrate:           getAwsWAFRule,
+		},
+		List: &plugin.ListConfig{
+			Hydrate:       listAwsWAFRules,
+		},
+		Columns: awsColumns([]*plugin.Column{
+			{
+				Name:        "name",
+				Description: "The name for the rule.",
+				Type:        proto.ColumnType_STRING,
+			},
+			{
+				Name:        "rule_id",
+				Description: "The ID of the Rule.",
+				Type:        proto.ColumnType_STRING,
+			},
+			{
+				Name:        "metric_name",
+				Description: "The name of the metric for the Rule.",
+				Type:        proto.ColumnType_STRING,
+				Hydrate:     getAwsWAFRule,
+			},
+			{
+				Name:        "predicates",
+				Description: "The Predicates object contains one Predicate element for each ByteMatchSet,IPSet, or SqlInjectionMatchSet object that you want to include in a Rule.",
+				Type:        proto.ColumnType_JSON,
+				Hydrate:     getAwsWAFRule,
+			},
+			{
+				Name:        "tags_src",
+				Description: "A list of tags assigned to the Rule.",
+				Type:        proto.ColumnType_JSON,
+				Hydrate:     getAwsWAFRuleTag,
+				Transform:   transform.FromField("TagInfoForResource.TagList"),
+			},
+			// Standard columns for all tables
+			{
+				Name:        "title",
+				Description: resourceInterfaceDescription("title"),
+				Type:        proto.ColumnType_STRING,
+				Transform:   transform.FromField("Name"),
+			},
+			{
+				Name:        "tags",
+				Description: resourceInterfaceDescription("tags"),
+				Type:        proto.ColumnType_JSON,
+				Hydrate:     getAwsWAFRuleTag,
+				Transform:   transform.FromField("TagInfoForResource.TagList").Transform(tagListToTurbotTags),
+			},
+			{
+				Name:        "akas",
+				Description: resourceInterfaceDescription("akas"),
+				Type:        proto.ColumnType_JSON,
+				Hydrate:     getAwsWAFRuleAkas,
+				Transform:   transform.FromValue(),
+			},
+		}),
+	}
+}
+
+//// LIST FUNCTION
+
+func listAwsWAFRules(ctx context.Context, d *plugin.QueryData, _ *plugin.HydrateData) (interface{}, error) {
+	
+	plugin.Logger(ctx).Trace("listAwsWAFRules")
+
+	// Create session
+	svc, err := WAFService(ctx, d)
+	if err != nil {
+		return nil, err
+	}
+
+	// List call
+	params := &waf.ListRulesInput{}
+	pagesLeft := true
+	for pagesLeft {
+			response, err := svc.ListRules(params)
+			if err != nil {
+				return nil, err
+			}
+			for _, rule := range response.Rules {
+				d.StreamListItem(ctx, rule)
+			}
+			if response.NextMarker != nil {
+				pagesLeft = true
+				params.NextMarker = response.NextMarker
+			} else {
+				pagesLeft = false
+			}
+			
+	}
+
+	return nil, nil
+}
+
+//// HYDRATE FUNCTIONS
+
+func getAwsWAFRule(ctx context.Context, d *plugin.QueryData, h *plugin.HydrateData) (interface{}, error) {
+	logger := plugin.Logger(ctx)
+	logger.Trace("getAwsWAFRule")
+
+	// Create Session
+	svc, err := WAFService(ctx, d)
+	if err != nil {
+		return nil, err
+	}
+
+	var id string
+	if h.Item != nil {
+		id = ruleData(h.Item)
+	} else {
+		id = d.KeyColumnQuals["rule_id"].GetStringValue()
+	}
+
+	// Build the params
+	param := &waf.GetRuleInput{
+		RuleId:    aws.String(id),
+	}
+
+	// Get call
+	data, err := svc.GetRule(param)
+	if err != nil {
+		return nil, err
+	}
+
+	return data.Rule, nil
+}
+
+//// TRANSFORM FUNCTIONS
+
+func getAwsWAFRuleTag(ctx context.Context, d *plugin.QueryData, h *plugin.HydrateData) (interface{}, error) {
+	plugin.Logger(ctx).Trace("getAwsWAFRuleTag")
+
+	id := ruleData(h.Item)
+	
+	commonAwsColumns, err := getCommonColumns(ctx, d, h)
+	if err != nil {
+		return nil, err
+	}
+	commonColumnData := commonAwsColumns.(*awsCommonColumnData)
+
+	// Create Session
+	svc, err := WAFService(ctx, d)
+	if err != nil {
+		return nil, err
+	}
+
+	aka := "arn:" + commonColumnData.Partition + ":waf::" + commonColumnData.AccountId + ":rule" + "/" + id
+
+	params := &waf.ListTagsForResourceInput{
+		ResourceARN: &aka,
+	}
+
+	op, err := svc.ListTagsForResource(params)
+	if err != nil {
+		return nil, err
+	}
+
+	return op, nil
+}
+
+func getAwsWAFRuleAkas(ctx context.Context, d *plugin.QueryData, h *plugin.HydrateData) (interface{}, error) {
+	plugin.Logger(ctx).Trace("getAwsWAFRuleAkas")
+
+	id := ruleData(h.Item)
+
+	c, err := getCommonColumns(ctx, d, h)
+	if err != nil {
+		return nil, err
+	}
+	commonColumnData := c.(*awsCommonColumnData)
+	aka := "arn:" + commonColumnData.Partition + ":waf::" + commonColumnData.AccountId + ":rule" + "/" + id 
+
+	return []string{aka}, nil
+}
+
+func tagListToTurbotTags(ctx context.Context, d *transform.TransformData) (interface{}, error) {
+	plugin.Logger(ctx).Trace("tagListToTurbotTags")
+	tagList := d.HydrateItem.(*waf.ListTagsForResourceOutput)
+
+	if tagList.TagInfoForResource.TagList == nil {
+		return nil, nil
+	}
+
+	// Mapping the resource tags inside turbotTags
+	var turbotTagsMap map[string]string
+	if tagList != nil {
+		turbotTagsMap = map[string]string{}
+		for _, i := range tagList.TagInfoForResource.TagList  {
+			turbotTagsMap[*i.Key] = *i.Value
+		}
+	}
+
+	return turbotTagsMap, nil
+}
+
+func ruleData(item interface{}) string {
+	switch item.(type) {
+	case *waf.RuleSummary:
+		return *item.(*waf.RuleSummary).RuleId
+	case *waf.Rule:
+		return *item.(*waf.Rule).RuleId
+	}
+	return ""
+}
---
+++ docs/tables/aws_waf_rule.md
@@ -0,0 +1,31 @@
+# Table: aws_waf_rule
+
+Amazon WAF Rule defines how to inspect web requests and what to do when a web request matches the inspection criteria.
+
+## Example
+
+### Basic info
+
+```sql
+select
+  name,
+  rule_id,
+  metric_name
+from
+  aws_waf_rule;
+```
+
+
+### Get details of predicate element associated with rule
+
+```sql
+select
+  name,
+  rule_id,
+  p ->> 'DataId' as data_id,
+  p ->> 'Negated' as negated,
+  p ->> 'Type' as type
+from
+  aws_waf_rule,
+  jsonb_array_elements(predicates) as p;
+```
