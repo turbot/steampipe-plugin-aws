f1f83d9517cdcc81314fd2a7d07bdeb74dddeef7
Anisa Das <anisa@turbot.com>
2021-04-05 17:13:34 +0530

    Changed in service file

--- aws/service.go
+++ aws/service.go
@@ -750,7 +750,7 @@ func EventBridgeService(ctx context.Context, d *plugin.QueryData, region string)
 	return svc, nil
 }
 
-// GuardDutyService returns the service connection for AWS ELB GuardDuty service
+// GuardDutyService returns the service connection for AWS GuardDuty service
 func GuardDutyService(ctx context.Context, d *plugin.QueryData, region string) (*guardduty.GuardDuty, error) {
 	if region == "" {
 		return nil, fmt.Errorf("region must be passed GuardDutyService")
