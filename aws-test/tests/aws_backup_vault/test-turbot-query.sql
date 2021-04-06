5555362d5441d0b95e502b1903dbceb3aa67ab78
souravTurbot <78197905+bigdatasourav@users.noreply.github.com>
2021-04-07 01:56:55 +0530

    Add table aws_backup_vault closes #160 (#163)
    This commit also updates steampipe-plugin-sdk to v0.2.7 closes #294

---
+++ aws-test/tests/aws_backup_vault/test-turbot-query.sql
@@ -0,0 +1,3 @@
+select title, akas
+from aws.aws_backup_vault
+where name = '{{ output.id.value }}';
---
+++ aws-test/tests/aws_backup_vault/variables.tf
@@ -0,0 +1,90 @@
+variable "resource_name" {
+  type        = string
+  default     = "turbot-test-20200125-create-update"
+  description = "Name of the resource used throughout the test."
+}
+
+variable "aws_profile" {
+  type        = string
+  default     = "integration-tests"
+  description = "AWS credentials profile used for the test. Default is to use the default profile."
+}
+
+variable "aws_region" {
+  type        = string
+  default     = "us-east-1"
+  description = "AWS region used for the test. Does not work with default region in config, so must be defined here."
+}
+
+variable "aws_region_alternate" {
+  type        = string
+  default     = "us-east-2"
+  description = "Alternate AWS region used for tests that require two regions (e.g. DynamoDB global tables)."
+}
+
+provider "aws" {
+  profile = var.aws_profile
+  region  = var.aws_region
+}
+
+provider "aws" {
+  alias   = "alternate"
+  profile = var.aws_profile
+  region  = var.aws_region_alternate
+}
+
+data "aws_partition" "current" {}
+data "aws_caller_identity" "current" {}
+data "aws_region" "primary" {}
+data "aws_region" "alternate" {
+  provider = aws.alternate
+}
+
+data "null_data_source" "resource" {
+  inputs = {
+    scope = "arn:${data.aws_partition.current.partition}:::${data.aws_caller_identity.current.account_id}"
+  }
+}
+
+resource "aws_backup_vault" "named_test_resource" {
+  name = var.resource_name
+}
+
+resource "aws_backup_vault_policy" "named_test_resource" {
+  backup_vault_name = aws_backup_vault.named_test_resource.name
+
+  policy = <<POLICY
+{
+  "Version": "2012-10-17",
+  "Id": "default",
+  "Statement": [
+    {
+      "Sid": "default",
+      "Effect": "Allow",
+      "Principal": {
+        "AWS": "*"
+      },
+      "Action": [
+        "backup:DescribeBackupVault",
+        "backup:DeleteBackupVault",
+        "backup:PutBackupVaultAccessPolicy",
+        "backup:DeleteBackupVaultAccessPolicy",
+        "backup:GetBackupVaultAccessPolicy",
+        "backup:StartBackupJob",
+        "backup:GetBackupVaultNotifications",
+        "backup:PutBackupVaultNotifications"
+      ],
+      "Resource": "${aws_backup_vault.named_test_resource.arn}"
+    }
+  ]
+}
+POLICY
+}
+
+output "id" {
+  value = aws_backup_vault.named_test_resource.id
+}
+
+output "resource_aka" {
+  value = aws_backup_vault.named_test_resource.arn
+}
