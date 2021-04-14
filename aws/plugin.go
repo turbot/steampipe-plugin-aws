15b4242bea27d8ad9d985439ca2aedf3ef1fb72d
Sayan Saha <sayan@turbot.com>
2021-04-14 23:32:44 +0530

    Add table aws_inspector_assessment_template. closes #244 (#248)

--- aws/plugin.go
+++ aws/plugin.go
@@ -100,6 +100,7 @@ func Plugin(ctx context.Context) *plugin.Plugin {
 			"aws_iam_user":                           tableAwsIamUser(ctx),
 			"aws_iam_virtual_mfa_device":             tableAwsIamVirtualMfaDevice(ctx),
 			"aws_inspector_assessment_target":        tableAwsInspectorAssessmentTarget(ctx),
+			"aws_inspector_assessment_template":      tableAwsInspectorAssessmentTemplate(ctx),
 			"aws_kinesis_consumer":                   tableAwsKinesisConsumer(ctx),
 			"aws_kinesis_firehose_delivery_stream":   tableAwsKinesisFirehoseDeliveryStream(ctx),
 			"aws_kinesis_stream":                     tableAwsKinesisStream(ctx),
