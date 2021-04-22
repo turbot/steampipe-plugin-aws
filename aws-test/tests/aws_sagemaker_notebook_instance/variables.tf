3ad35b636a8259ea9392fe9e60b40252e4f3c414
Priyanka585464 <39396993+Priyanka585464@users.noreply.github.com>
2021-04-23 02:01:37 +0530

    Add table aws_sagemaker_notebook_instance. closes #200 (#324)

---
+++ aws-test/tests/aws_sagemaker_notebook_instance/variables.tf
@@ -0,0 +1,97 @@
+variable "resource_name" {
+  type        = string
+  default     = "turbot-test-20200125-create-update"
+  description = "Name of the resource used throughout the test."
+}
+
+variable "aws_profile" {
+  type        = string
+  default     = "default"
+  description = "AWS credentials profile used for the test. Default is to use the default profile."
+}
+
+variable "aws_region" {
+  type        = string
+  default     = "us-east-1"
+  description = "AWS region used for the test. Does not work with default region in config, so must be defined here."
+}
+
+variable "aws_region_alternate" {
+  type        = string
+  default     = "us-east-2"
+  description = "Alternate AWS region used for tests that require two regions (e.g. DynamoDB global tables)."
+}
+
+provider "aws" {
+  profile = var.aws_profile
+  region  = var.aws_region
+}
+
+provider "aws" {
+  alias   = "alternate"
+  profile = var.aws_profile
+  region  = var.aws_region_alternate
+}
+
+data "aws_partition" "current" {}
+data "aws_caller_identity" "current" {}
+data "aws_region" "primary" {}
+data "aws_region" "alternate" {
+  provider = aws.alternate
+}
+
+data "null_data_source" "resource" {
+  inputs = {
+    scope = "arn:${data.aws_partition.current.partition}:::${data.aws_caller_identity.current.account_id}"
+  }
+}
+
+resource "aws_iam_role" "my_role" {
+  name = var.resource_name
+  assume_role_policy = jsonencode({
+    Version = "2012-10-17"
+    Statement = [
+      {
+        Action = "sts:AssumeRole"
+        Effect = "Allow"
+        Sid    = ""
+        Principal = {
+          Service = "sagemaker.amazonaws.com"
+        }
+      },
+    ]
+  })
+}
+
+resource "aws_sagemaker_notebook_instance" "named_test_resource" {
+  name = var.resource_name
+  role_arn = aws_iam_role.my_role.arn
+  instance_type = "ml.t2.medium"
+  tags = {
+    name = var.resource_name
+  }
+}
+
+output "account_id" {
+  value = data.aws_caller_identity.current.account_id
+}
+
+output "aws_region" {
+  value = data.aws_region.primary.name
+}
+
+output "aws_partition" {
+  value = data.aws_partition.current.partition
+}
+
+output "resource_aka" {
+  value = aws_sagemaker_notebook_instance.named_test_resource.arn
+}
+
+output "resource_id" {
+  value = aws_sagemaker_notebook_instance.named_test_resource.id
+}
+
+output "resource_name" {
+  value = var.resource_name
+}
