aa5e2aa8ff88c8799648cdc1f0fde1234074a385
Subhajit Mondal <38218418+Subhajit97@users.noreply.github.com>
2021-05-04 22:54:13 +0530

    Add 'arn' column to aws_ec2_instance table. Closes #366 (#367)

--- aws-test/tests/aws_ec2_instance/test-get-query.sql
+++ aws-test/tests/aws_ec2_instance/test-get-query.sql
@@ -1,3 +1,3 @@
-select instance_id, instance_type, monitoring_state, cpu_options_core_count, cpu_options_threads_per_core, ebs_optimized, hypervisor, image_id, tags_src
+select instance_id, arn, instance_type, monitoring_state, cpu_options_core_count, cpu_options_threads_per_core, ebs_optimized, hypervisor, image_id, tags_src, akas
 from aws.aws_ec2_instance
 where instance_id = '{{ output.resource_id.value }}'
