4107dc2c482c1e2b110f070a2f8028540ee3f981
Subhajit Mondal <38218418+Subhajit97@users.noreply.github.com>
2021-05-05 19:56:14 +0530

    Add 'arn' column to aws_vpc_security_group table. Closes #376 (#377)

--- aws-test/tests/aws_vpc_security_group/test-get-query.sql
+++ aws-test/tests/aws_vpc_security_group/test-get-query.sql
@@ -1,3 +1,3 @@
-select group_name, group_id, description, vpc_id, owner_id, tags_src
+select group_name, arn, group_id, description, vpc_id, owner_id, tags_src
 from aws.aws_vpc_security_group
 where group_id = '{{ output.resource_id.value }}'
