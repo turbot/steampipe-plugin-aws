7f091b555f2e7bc0ee38d8edfb98ea230fbb9dbf
Rajesh Bal <46851617+rajeshbal65@users.noreply.github.com>
2021-06-03 01:35:33 +0530

    Add table aws_auditmanager_control. Closes #402 (#425)

---
+++ aws/table_aws_auditmanager_control.go
@@ -0,0 +1,211 @@
+package aws
+
+import (
+	"context"
+
+	"github.com/aws/aws-sdk-go/aws"
+	"github.com/aws/aws-sdk-go/service/auditmanager"
+	"github.com/turbot/steampipe-plugin-sdk/grpc/proto"
+	"github.com/turbot/steampipe-plugin-sdk/plugin"
+	"github.com/turbot/steampipe-plugin-sdk/plugin/transform"
+)
+
+//// TABLE DEFINITION
+
+func tableAwsAuditManagerControl(_ context.Context) *plugin.Table {
+	return &plugin.Table{
+		Name:        "aws_auditmanager_control",
+		Description: "AWS Audit Manager Control",
+		Get: &plugin.GetConfig{
+			KeyColumns:        plugin.SingleColumn("id"),
+			Hydrate:           getAuditManagerControl,
+			ShouldIgnoreError: isNotFoundError([]string{"ResourceNotFoundException", "ValidationException", "InvalidParameter"}),
+		},
+		List: &plugin.ListConfig{
+			Hydrate: listAuditManagerControls,
+		},
+		GetMatrixItem: BuildRegionList,
+		Columns: awsRegionalColumns([]*plugin.Column{
+			{
+				Name:        "name",
+				Description: "The name of the specified control.",
+				Type:        proto.ColumnType_STRING,
+			},
+			{
+				Name:        "arn",
+				Description: "The Amazon Resource Name (ARN) of the specified control.",
+				Type:        proto.ColumnType_STRING,
+			},
+			{
+				Name:        "id",
+				Description: "An unique identifier for the specified control.",
+				Type:        proto.ColumnType_STRING,
+			},
+			{
+				Name:        "type",
+				Description: "The type of control, such as custom or standard.",
+				Type:        proto.ColumnType_STRING,
+				Hydrate:     getAuditManagerControl,
+			},
+			{
+				Name:        "created_at",
+				Description: "Specifies when the control was created.",
+				Type:        proto.ColumnType_TIMESTAMP,
+			},
+			{
+				Name:        "created_by",
+				Description: "The IAM user or role that created the control.",
+				Type:        proto.ColumnType_STRING,
+				Hydrate:     getAuditManagerControl,
+			},
+			{
+				Name:        "action_plan_title",
+				Description: "The title of the action plan for remediating the control.",
+				Type:        proto.ColumnType_STRING,
+				Hydrate:     getAuditManagerControl,
+			},
+			{
+				Name:        "action_plan_instructions",
+				Description: "The recommended actions to carry out if the control is not fulfilled.",
+				Type:        proto.ColumnType_STRING,
+				Hydrate:     getAuditManagerControl,
+			},
+			{
+				Name:        "control_sources",
+				Description: "The data source that determines from where AWS Audit Manager collects evidence for the control.",
+				Type:        proto.ColumnType_STRING,
+			},
+			{
+				Name:        "description",
+				Description: "The description of the specified control.",
+				Type:        proto.ColumnType_STRING,
+				Hydrate:     getAuditManagerControl,
+			},
+			{
+				Name:        "last_updated_at",
+				Description: "Specifies when the control was most recently updated.",
+				Type:        proto.ColumnType_TIMESTAMP,
+			},
+			{
+				Name:        "last_updated_by",
+				Description: "The IAM user or role that most recently updated the control.",
+				Type:        proto.ColumnType_STRING,
+				Hydrate:     getAuditManagerControl,
+			},
+			{
+				Name:        "testing_information",
+				Description: "The steps to follow to determine if the control has been satisfied.",
+				Type:        proto.ColumnType_STRING,
+				Hydrate:     getAuditManagerControl,
+			},
+			{
+				Name:        "control_mapping_sources",
+				Description: "The data mapping sources for the specified control.",
+				Type:        proto.ColumnType_JSON,
+				Hydrate:     getAuditManagerControl,
+			},
+
+			// Steampipe standard columns
+			{
+				Name:        "title",
+				Description: resourceInterfaceDescription("title"),
+				Type:        proto.ColumnType_STRING,
+				Transform:   transform.FromField("Name"),
+			},
+			{
+				Name:        "tags",
+				Description: resourceInterfaceDescription("tags"),
+				Type:        proto.ColumnType_JSON,
+				Hydrate:     getAuditManagerControl,
+			},
+			{
+				Name:        "akas",
+				Description: resourceInterfaceDescription("akas"),
+				Type:        proto.ColumnType_JSON,
+				Transform:   transform.FromField("Arn").Transform(arnToAkas),
+			},
+		}),
+	}
+}
+
+//// LIST FUNCTION
+
+func listAuditManagerControls(ctx context.Context, d *plugin.QueryData, h *plugin.HydrateData) (interface{}, error) {
+	var region string
+	matrixRegion := plugin.GetMatrixItem(ctx)[matrixKeyRegion]
+	if matrixRegion != nil {
+		region = matrixRegion.(string)
+	}
+	plugin.Logger(ctx).Trace("listAuditManagerControls", "AWS_REGION", region)
+
+	// Create Session
+	svc, err := AuditManagerService(ctx, d, region)
+	if err != nil {
+		return nil, err
+	}
+
+	// List all standard controls
+	err = svc.ListControlsPages(
+		&auditmanager.ListControlsInput{
+			ControlType: aws.String("Standard"),
+		},
+		func(page *auditmanager.ListControlsOutput, lastPage bool) bool {
+			for _, items := range page.ControlMetadataList {
+				d.StreamListItem(ctx, items)
+			}
+			return !lastPage
+		},
+	)
+
+	// List all custom controls
+	err = svc.ListControlsPages(
+		&auditmanager.ListControlsInput{
+			ControlType: aws.String("Custom"),
+		},
+		func(page *auditmanager.ListControlsOutput, lastPage bool) bool {
+			for _, items := range page.ControlMetadataList {
+				d.StreamListItem(ctx, items)
+			}
+			return !lastPage
+		},
+	)
+
+	return nil, err
+}
+
+//// HYDRATE FUNCTIONS
+
+func getAuditManagerControl(ctx context.Context, d *plugin.QueryData, h *plugin.HydrateData) (interface{}, error) {
+	plugin.Logger(ctx).Trace("getAuditManagerControl")
+
+	var region string
+	matrixRegion := plugin.GetMatrixItem(ctx)[matrixKeyRegion]
+	if matrixRegion != nil {
+		region = matrixRegion.(string)
+	}
+
+	// Create Session
+	svc, err := AuditManagerService(ctx, d, region)
+	if err != nil {
+		return nil, err
+	}
+
+	var id string
+	if h.Item != nil {
+		id = *h.Item.(*auditmanager.ControlMetadata).Id
+	} else {
+		id = d.KeyColumnQuals["id"].GetStringValue()
+	}
+
+	params := &auditmanager.GetControlInput{
+		ControlId: aws.String(id),
+	}
+
+	op, err := svc.GetControl(params)
+	if err != nil {
+		plugin.Logger(ctx).Debug("getAuditManagerControl", "ERROR", err)
+		return nil, err
+	}
+
+	return op.Control, nil
+}
