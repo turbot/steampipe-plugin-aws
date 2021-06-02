b9656d8436aef8283f2ef108209183e5915ba00b
Anisa Das <59417312+anisadas@users.noreply.github.com>
2021-06-02 21:35:40 +0530

    Add 'arn' column to aws_redshift_cluster table. Closes #452 (#462)

--- aws-test/tests/aws_redshift_cluster/test-hydrate-query.sql
+++ aws-test/tests/aws_redshift_cluster/test-hydrate-query.sql
@@ -1,3 +1,3 @@
-select cluster_identifier, akas, allow_version_upgrade
+select cluster_identifier, akas, arn, allow_version_upgrade
 from aws.aws_redshift_cluster
 where cluster_identifier = '{{ output.resource_name.value }}';
