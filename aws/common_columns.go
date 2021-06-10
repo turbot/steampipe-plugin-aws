59d4500acaa56c04ba199696c130ea1b2975650d
LalitTurbot <lalit@turbot.com>
2021-06-10 11:53:52 +0530

    update

--- aws/common_columns.go
+++ aws/common_columns.go
@@ -110,16 +110,12 @@ func getCommonColumns(ctx context.Context, d *plugin.QueryData, h *plugin.Hydrat
 
 	cacheKey := "commonColumnData" + region
 	var commonColumnData *awsCommonColumnData
-	// if cachedData, ok := d.ConnectionManager.Cache.Get(cacheKey); ok {
-	// try to get the data from the cache - call GetP to wait for any pending fetches of the same data
-	if cachedData, ok := d.ConnectionManager.Cache.GetP(cacheKey); ok {
+	if cachedData, ok := d.ConnectionManager.Cache.Get(cacheKey); ok {
 		commonColumnData = cachedData.(*awsCommonColumnData)
 		plugin.Logger(ctx).Warn("getCommonColumns", "From Cache", commonColumnData)
 	} else {
 		callerIdentity, err := getCallerIdentity(ctx, d, h)
 		if err != nil {
-			// let the cache know that we have failed to fetch this item
-			d.ConnectionManager.Cache.ClearPendingItem(cacheKey)
 			return nil, err
 		}
 
@@ -131,10 +127,7 @@ func getCommonColumns(ctx context.Context, d *plugin.QueryData, h *plugin.Hydrat
 		}
 
 		// save to extension cache
-		// d.ConnectionManager.Cache.Set(cacheKey, commonColumnData)
-		// save to the cache
-		// SetP also clears any pending fetches
-		d.ConnectionManager.Cache.SetP(cacheKey, commonColumnData)
+		d.ConnectionManager.Cache.Set(cacheKey, commonColumnData)
 		plugin.Logger(ctx).Warn("getCommonColumns", "NOT From Cache", commonColumnData)
 	}
 
@@ -147,33 +140,24 @@ func getCallerIdentity(ctx context.Context, d *plugin.QueryData, h *plugin.Hydra
 	cacheKey := "GetCallerIdentity"
 
 	// if found in cache, return the result
-	// if cachedData, ok := d.ConnectionManager.Cache.Get(cacheKey); ok {
-	// try to get the data from the cache - call GetP to wait for any pending fetches of the same data
-	if cachedData, ok := d.ConnectionManager.Cache.GetP(cacheKey); ok {
-		plugin.Logger(ctx).Warn("getCallerIdentity", "From Cache", getCallerIdentity)
+	if cachedData, ok := d.ConnectionManager.Cache.Get(cacheKey); ok {
 		return cachedData.(*sts.GetCallerIdentityOutput), nil
 	}
 
 	// get the service connection for the service
 	stsSvc, err := StsService(ctx, d)
 	if err != nil {
-		// let the cache know that we have failed to fetch this item
-		d.ConnectionManager.Cache.ClearPendingItem(cacheKey)
 		return nil, err
 	}
 
 	callerIdentity, err := stsSvc.GetCallerIdentity(&sts.GetCallerIdentityInput{})
 	if err != nil {
 		// let the cache know that we have failed to fetch this item
-		d.ConnectionManager.Cache.ClearPendingItem(cacheKey)
 		return nil, err
 	}
 
 	// save to extension cache
-	// d.ConnectionManager.Cache.Set(cacheKey, callerIdentity)
-	// save to the cache
-	// SetP also clears any pending fetches
-	d.ConnectionManager.Cache.SetP(cacheKey, callerIdentity)
+	d.ConnectionManager.Cache.Set(cacheKey, callerIdentity)
 	plugin.Logger(ctx).Warn("getCallerIdentity", "Not From Cache", getCallerIdentity)
 
 	return callerIdentity, nil
--- go.mod
+++ go.mod
@@ -7,7 +7,5 @@ require (
 	github.com/gocarina/gocsv v0.0.0-20201208093247-67c824bc04d4
 	github.com/golang/protobuf v1.4.3
 	github.com/turbot/go-kit v0.1.1
-	github.com/turbot/steampipe-plugin-sdk v0.3.0-rc.0.0.20210608170910-83f1bac3df08
+	github.com/turbot/steampipe-plugin-sdk v0.2.10
 )
-
-// replace github.com/turbot/steampipe-plugin-sdk => github.com/turbot/steampipe-plugin-sdk v0.3.0-rc.0.0.20210608170910-83f1bac3df08
