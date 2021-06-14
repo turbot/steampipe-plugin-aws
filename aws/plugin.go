06850078e90bdcfba9bda75cf56520618baaf086
Subhajit Mondal <38218418+Subhajit97@users.noreply.github.com>
2021-06-15 03:50:04 +0530

    Add table aws_ec2_ssl_policy. Closes #361 (#362)

--- aws/plugin.go
+++ aws/plugin.go
@@ -100,6 +100,7 @@ func Plugin(ctx context.Context) *plugin.Plugin {
 			"aws_ec2_network_interface":                         tableAwsEc2NetworkInterface(ctx),
 			"aws_ec2_network_load_balancer":                     tableAwsEc2NetworkLoadBalancer(ctx),
 			"aws_ec2_regional_settings":                         tableAwsEc2RegionalSettings(ctx),
+			"aws_ec2_ssl_policy":                                tableAwsEc2SslPolicy(ctx),
 			"aws_ec2_target_group":                              tableAwsEc2TargetGroup(ctx),
 			"aws_ec2_transit_gateway":                           tableAwsEc2TransitGateway(ctx),
 			"aws_ec2_transit_gateway_route_table":               tableAwsEc2TransitGatewayRouteTable(ctx),
