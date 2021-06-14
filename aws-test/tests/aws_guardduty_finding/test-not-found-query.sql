519a805f12338d6c009aaf4feec2d6f9d64e1dd2
bigdatasourav <bigdatasourav@gmail.com>
2021-06-14 13:14:01 +0530

    update

--- aws-test/tests/aws_guardduty_finding/test-not-found-query.sql
+++
@@ -1,3 +0,0 @@
-select id, name
-from aws.aws_guardduty_finding
-where id = '{{ output.resource_id.value }}::dummy';
--- aws-test/tests/aws_guardduty_finding/test-turbot-query.sql
+++
@@ -1,3 +0,0 @@
-select account_id, akas, title
-from aws.aws_guardduty_finding
-where akas::text = '["{{ output.resource_aka.value }}"]';
