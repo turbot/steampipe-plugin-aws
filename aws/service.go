a8ff0e7d8598e05554f38660b80c27e06f0206b4
Priyanka585464 <39396993+Priyanka585464@users.noreply.github.com>
2021-06-22 02:49:10 +0530

    Add table aws_route53_domain. closes #59 (#360)

--- aws/service.go
+++ aws/service.go
@@ -55,6 +55,7 @@ import (
 	"github.com/aws/aws-sdk-go/service/rds"
 	"github.com/aws/aws-sdk-go/service/redshift"
 	"github.com/aws/aws-sdk-go/service/route53"
+	"github.com/aws/aws-sdk-go/service/route53domains"
 	"github.com/aws/aws-sdk-go/service/route53resolver"
 	"github.com/aws/aws-sdk-go/service/s3"
 	"github.com/aws/aws-sdk-go/service/s3control"
@@ -1036,6 +1037,27 @@ func RedshiftService(ctx context.Context, d *plugin.QueryData, region string) (*
 	svc := redshift.New(sess)
 	d.ConnectionManager.Cache.Set(serviceCacheKey, svc)
 
+	return svc, nil
+}
+
+// Route53DomainsService returns the service connection for AWS route53 domains service
+func Route53DomainsService(ctx context.Context, d *plugin.QueryData, region string) (*route53domains.Route53Domains, error) {
+	if region == "" {
+		return nil, fmt.Errorf("region must be passed Route53Domains")
+	}
+	// have we already created and cached the service?
+	serviceCacheKey := fmt.Sprintf("route53domain-%s", region)
+	if cachedData, ok := d.ConnectionManager.Cache.Get(serviceCacheKey); ok {
+		return cachedData.(*route53domains.Route53Domains), nil
+	}
+	// so it was not in cache - create service
+	sess, err := getSession(ctx, d, region)
+	if err != nil {
+		return nil, err
+
+	}
+	svc := route53domains.New(sess)
+	d.ConnectionManager.Cache.Set(serviceCacheKey, svc)
 	return svc, nil
 }
 
