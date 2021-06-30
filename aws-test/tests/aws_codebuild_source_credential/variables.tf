5678e183886cfe3305a7fa2dd65050b68b9a51c8
Subhajit Mondal <38218418+Subhajit97@users.noreply.github.com>
2021-06-30 19:05:51 +0530

    Add table aws_codebuild_source_credential. Closes #508 (#511)

---
+++ aws-test/tests/aws_codebuild_source_credential/variables.tf
@@ -0,0 +1,56 @@
+
+variable "aws_profile" {
+  type        = string
+  default     = "default"
+  description = "AWS credentials profile used for the test. Default is to use the default profile."
+}
+
+variable "aws_region" {
+  type        = string
+  default     = "us-east-1"
+  description = "AWS region used for the test. Does not work with default region in config, so must be defined here."
+}
+
+variable "aws_region_alternate" {
+  type        = string
+  default     = "us-east-2"
+  description = "Alternate AWS region used for tests that require two regions."
+}
+
+provider "aws" {
+  profile = var.aws_profile
+  region  = var.aws_region
+}
+
+provider "aws" {
+  alias   = "alternate"
+  profile = var.aws_profile
+  region  = var.aws_region_alternate
+}
+
+data "aws_partition" "current" {}
+data "aws_caller_identity" "current" {}
+data "aws_region" "primary" {}
+data "aws_region" "alternate" {
+  provider = aws.alternate
+}
+
+data "null_data_source" "resource" {
+  inputs = {
+    scope = "arn:${data.aws_partition.current.partition}:::${data.aws_caller_identity.current.account_id}"
+  }
+}
+
+resource "aws_codebuild_source_credential" "named_test_resource" {
+  auth_type   = "PERSONAL_ACCESS_TOKEN"
+  server_type = "GITHUB"
+  token       = "example"
+}
+
+output "arn" {
+  value = aws_codebuild_source_credential.named_test_resource.arn
+}
+
+output "region" {
+  value = var.aws_region
+}
