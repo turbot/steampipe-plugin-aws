dd7dbd7d653b82adc1e5d0e43d15e3a9ddc0566c
Sanket Raut <sankerau@amazon.com>
2021-08-23 22:51:24 +0530

    Add table aws_lambda_function_metric_invocations_daily

--- aws/plugin.go
+++ aws/plugin.go
@@ -178,6 +178,7 @@ func Plugin(ctx context.Context) *plugin.Plugin {
 			"aws_lambda_alias":                                             tableAwsLambdaAlias(ctx),
 			"aws_lambda_function":                                          tableAwsLambdaFunction(ctx),
 			"aws_lambda_version":                                           tableAwsLambdaVersion(ctx),
+			"aws_lambda_function_metric_invocations_daily":                 tableAwsLambdaFunctionMetricInvocationsDaily(ctx),
 			"aws_macie2_classification_job":                                tableAwsMacie2ClassificationJob(ctx),
 			"aws_rds_db_cluster":                                           tableAwsRDSDBCluster(ctx),
 			"aws_rds_db_cluster_parameter_group":                           tableAwsRDSDBClusterParameterGroup(ctx),
---
+++ docs/tables/aws_lambda_function_metric_invocations_daily.md
@@ -0,0 +1,40 @@
+# Table: aws_lambda_function_metric_invocations_daily
+
+Amazon CloudWatch Metrics provide data about the performance of your systems.  The `aws_lambda_function_metric_invocations_daily` table provides metric statistics at 24 hour intervals for the last year.
+
+
+## Examples
+
+### Basic info
+
+```sql
+select
+  name,
+  timestamp,
+  sum
+from
+  aws_lambda_function_metric_invocations_daily
+order by
+  name,
+  timestamp;
+```
+
+
+
+### Lambda function daily invocations over 10 in last 3 days
+
+```sql
+select
+  name,
+  timestamp,
+  round(sum::numeric,2) as sum_invocations,
+  sample_count
+from
+  aws_lambda_function_metric_invocations_daily
+where 
+    date_part('day', now() - timestamp) <=3
+and sum > 10
+order by
+  name,
+  timestamp;
+```
