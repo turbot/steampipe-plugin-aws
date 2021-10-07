turbot/steampipe-plugin-aws

commit 49220a708ebae0bc16882a9765f6a4e20824ed1a

Add new optional key quals, filter support, context cancellation handling, and limit support for `aws_ec2_instance`, `aws_iam_policy`, `aws_rds_db_cluster_snapshot` tables (#638)
This commit also adds the `is_attached` column to the `aws_iam_policy` table and updates the steampipe-plugin-sdk to v1.6.1.

aws/table_aws_ec2_instance.go
@@ -28,6 +28,26 @@ func tableAwsEc2Instance(_ context.Context) *plugin.Table {
 		},
 		List: &plugin.ListConfig{
 			Hydrate: listEc2Instance,
+			KeyColumns: []*plugin.KeyColumn{
+				{Name: "hypervisor", Require: plugin.Optional},
+				{Name: "iam_instance_profile_arn", Require: plugin.Optional},
+				{Name: "image_id", Require: plugin.Optional},
+				{Name: "instance_lifecycle", Require: plugin.Optional},
+				{Name: "instance_state", Require: plugin.Optional},
+				{Name: "instance_type", Require: plugin.Optional},
+				{Name: "monitoring_state", Require: plugin.Optional},
+				{Name: "outpost_arn", Require: plugin.Optional},
+				{Name: "placement_availability_zone", Require: plugin.Optional},
+				{Name: "placement_group_name", Require: plugin.Optional},
+				{Name: "public_dns_name", Require: plugin.Optional},
+				{Name: "ram_disk_id", Require: plugin.Optional},
+				{Name: "root_device_name", Require: plugin.Optional},
+				{Name: "root_device_type", Require: plugin.Optional},
+				{Name: "subnet_id", Require: plugin.Optional},
+				{Name: "placement_tenancy", Require: plugin.Optional},
+				{Name: "virtualization_type", Require: plugin.Optional},
+				{Name: "vpc_id", Require: plugin.Optional},
+			},
 		},
 		GetMatrixItem: BuildRegionList,
 		Columns: awsRegionalColumns([]*plugin.Column{
@@ -327,19 +347,48 @@ func listEc2Instance(ctx context.Context, d *plugin.QueryData, _ *plugin.Hydrate
 		return nil, err
 	}
 
+	input := ec2.DescribeInstancesInput{
+		MaxResults: types.Int64(1000),
+	}
+	filters := buildEc2InstanceFilter(d.KeyColumnQuals)
+
+	if len(filters) != 0 {
+		input.Filters = filters
+	}
+
+	// If the requested number of items is less than the paging max limit
+	// set the limit to that instead
+	limit := d.QueryContext.Limit
+	if d.QueryContext.Limit != nil {
+		if *limit < *input.MaxResults {
+			// select * from aws_ec2_instance limit 1
+			// Error: InvalidParameterValue: Value ( 1 ) for parameter maxResults is invalid. Expecting a value greater than 5.
+			// 		status code: 400, request id: a84912d9-f5fd-403f-8e37-7f7b3f6faba6
+			if *limit < 5 {
+				input.MaxResults = types.Int64(5)
+			} else {
+				input.MaxResults = limit
+			}
+		}
+	}
+
 	// List call
-	err = svc.DescribeInstancesPages(
-		&ec2.DescribeInstancesInput{},
-		func(page *ec2.DescribeInstancesOutput, isLast bool) bool {
-			if page.Reservations != nil && len(page.Reservations) > 0 {
-				for _, reservation := range page.Reservations {
-					for _, instance := range reservation.Instances {
-						d.StreamListItem(ctx, instance)
+	err = svc.DescribeInstancesPages(&input, func(page *ec2.DescribeInstancesOutput, isLast bool) bool {
+		if page.Reservations != nil && len(page.Reservations) > 0 {
+			for _, reservation := range page.Reservations {
+				for _, instance := range reservation.Instances {
+					d.StreamListItem(ctx, instance)
+					// Check if context has been cancelled or if the limit has been hit (if specified)
+					// if there is a limit, it will return the number of rows required to reach this limit
+					if d.QueryStatus.RowsRemaining(ctx) == 0 {
+						return true
 					}
 				}
 			}
-			return !isLast
-		},
+		}
+
+		return !isLast
+	},
 	)
 
 	if err != nil {
@@ -609,4 +658,56 @@ func ec2InstanceStateChangeTime(_ context.Context, d *transform.TransformData) (
 		}
 	}
 	return data.LaunchTime, nil
+}
+
+//// UTILITY FUNCTIONS
+
+// build ec2 instance list call input filter
+func buildEc2InstanceFilter(equalQuals plugin.KeyColumnEqualsQualMap) []*ec2.Filter {
+	filters := make([]*ec2.Filter, 0)
+
+	filterQuals := map[string]string{
+		"hypervisor":                  "hypervisor",
+		"iam_instance_profile_arn":    "iam-instance-profile.arn",
+		"image_id":                    "image-id",
+		"instance_lifecycle":          "instance-lifecycle",
+		"instance_state":              "instance-state-name",
+		"instance_type":               "instance-type",
+		"monitoring_state":            "monitoring-state",
+		"outpost_arn":                 "outpost-arn",
+		"placement_availability_zone": "availability-zone",
+		"placement_group_name":        "placement-group-name",
+		"public_dns_name":             "dns-name",
+		"ram_disk_id":                 "ramdisk-id",
+		"root_device_name":            "root-device-name",
+		"root_device_type":            "root-device-type",
+		"subnet_id":                   "subnet-id",
+		"placement_tenancy":           "tenancy",
+		"virtualization_type":         "virtualization-type",
+		"vpc_id":                      "vpc-id",
+	}
+
+	for columnName, filterName := range filterQuals {
+		if equalQuals[columnName] != nil {
+			filter := ec2.Filter{
+				Name: types.String(filterName),
+			}
+			value := equalQuals[columnName]
+			if value.GetStringValue() != "" {
+				filter.Values = []*string{types.String(equalQuals[columnName].GetStringValue())}
+			} else if value.GetListValue() != nil {
+				filter.Values = getListValues(value.GetListValue())
+			}
+			filters = append(filters, &filter)
+		}
+	}
+	return filters
+}
+
+func getListValues(listValue *proto.QualValueList) []*string {
+	values := make([]*string, 0)
+	for _, value := range listValue.Values {
+		values = append(values, types.String(value.GetStringValue()))
+	}
+	return values
 }

aws/table_aws_iam_policy.go
@@ -4,6 +4,7 @@ import (
 	"context"
 	"strings"
 
+	"github.com/turbot/go-kit/types"
 	"github.com/turbot/steampipe-plugin-sdk/grpc/proto"
 	"github.com/turbot/steampipe-plugin-sdk/plugin/transform"
 
@@ -23,6 +24,11 @@ func tableAwsIamPolicy(_ context.Context) *plugin.Table {
 		},
 		List: &plugin.ListConfig{
 			Hydrate: listIamPolicies,
+			KeyColumns: plugin.KeyColumnSlice{
+				{Name: "is_aws_managed", Require: plugin.Optional, Operators: []string{"<>", "="}},
+				{Name: "is_attached", Require: plugin.Optional, Operators: []string{"<>", "="}},
+				{Name: "path", Require: plugin.Optional},
+			},
 		},
 		Columns: awsColumns([]*plugin.Column{
 			{
@@ -72,6 +78,12 @@ func tableAwsIamPolicy(_ context.Context) *plugin.Table {
 				Name:        "attachment_count",
 				Description: "The number of entities (users, groups, and roles) that the policy is attached to.",
 				Type:        proto.ColumnType_INT,
+			},
+			{
+				Name:        "is_attached",
+				Description: "Specifies whether the policy is attached to at least one IAM user, group, or role.",
+				Type:        proto.ColumnType_BOOL,
+				Transform:   transform.FromField("AttachmentCount").Transform(attachementCountToBool),
 			},
 			{
 				Name:        "default_version_id",
@@ -138,14 +150,31 @@ func listIamPolicies(ctx context.Context, d *plugin.QueryData, _ *plugin.Hydrate
 		return nil, err
 	}
 
-	err = svc.ListPoliciesPages(
-		&iam.ListPoliciesInput{},
-		func(page *iam.ListPoliciesOutput, lastPage bool) bool {
-			for _, policy := range page.Policies {
-				d.StreamListItem(ctx, policy)
+	input := buildIamPolicyFilter(d.KeyColumnQuals, d.Quals)
+	input.MaxItems = types.Int64(100)
+
+	// If the requested number of items is less than the paging max limit
+	// set the limit to that instead
+	limit := d.QueryContext.Limit
+	if d.QueryContext.Limit != nil {
+		if *limit < *input.MaxItems {
+			input.MaxItems = limit
+		}
+	}
+
+	// List call
+	err = svc.ListPoliciesPages(&input, func(page *iam.ListPoliciesOutput, lastPage bool) bool {
+		for _, policy := range page.Policies {
+			d.StreamListItem(ctx, policy)
+			// Check if context has been cancelled or if the limit has been hit (if specified)
+			// if there is a limit, it will return the number of rows required to reach this limit
+			if d.QueryStatus.RowsRemaining(ctx) == 0 {
+				return true
 			}
-			return !lastPage
-		},
+		}
+
+		return !lastPage
+	},
 	)
 	return nil, err
 }
@@ -229,7 +258,7 @@ func isPolicyAwsManaged(ctx context.Context, d *plugin.QueryData, h *plugin.Hydr
 	return false, nil
 }
 
-//// Transform Function
+//// TRANSFORM FUNCTIONS
 
 func iamPolicyTurbotTags(_ context.Context, d *transform.TransformData) (interface{}, error) {
 	policy := d.HydrateItem.(*iam.Policy)
@@ -244,4 +273,83 @@ func iamPolicyTurbotTags(_ context.Context, d *transform.TransformData) (interfa
 	}
 
 	return &turbotTagsMap, nil
+}
+
+func attachementCountToBool(_ context.Context, d *transform.TransformData) (interface{}, error) {
+	attachementCount := types.Int64Value((d.Value.(*int64)))
+	if attachementCount == 0 {
+		return false, nil
+	}
+	return true, nil
+}
+
+//// UTILITY FUNCTIONS
+
+func buildIamPolicyFilter(equalQuals plugin.KeyColumnEqualsQualMap, quals plugin.KeyColumnQualMap) iam.ListPoliciesInput {
+	input := iam.ListPoliciesInput{}
+
+	filterQuals := FilterQuals{
+		FilterQual{"is_aws_managed", "Bool", "Scope"},
+		FilterQual{"is_attached", "Bool", "OnlyAttached"},
+		FilterQual{"path", "String", "PathPrefix"},
+	}
+
+	// EqualsQualMap handling
+	for _, filterQual := range filterQuals {
+		if equalQuals[filterQual.ColumnName] != nil {
+			switch filterQual.ColumnType {
+			case "String":
+				input.PathPrefix = types.String(equalQuals[filterQual.ColumnName].GetStringValue())
+			case "Bool":
+				if filterQual.ColumnName == "is_aws_managed" {
+					input.SetScope("Local")
+					if equalQuals[filterQual.ColumnName].GetBoolValue() {
+						input.SetScope("AWS")
+					}
+				}
+				if filterQual.ColumnName == "is_attached" {
+					input.SetOnlyAttached(false)
+					if equalQuals[filterQual.ColumnName].GetBoolValue() {
+						input.SetOnlyAttached(true)
+					}
+				}
+			}
+		}
+	}
+
+	boolNEQuals := []string{
+		"is_aws_managed",
+		"is_attached",
+	}
+	// Non-Equals Qual Map handling
+	for _, qual := range boolNEQuals {
+		if quals[qual] != nil {
+			for _, q := range quals[qual].Quals {
+				value := q.Value.GetBoolValue()
+				if q.Operator == "<>" {
+					if qual == "is_aws_managed" {
+						input.SetScope("Local")
+						if !value {
+							input.SetScope("AWS")
+						}
+					}
+					if qual == "is_attached" {
+						input.SetOnlyAttached(false)
+						if !value {
+							input.SetOnlyAttached(true)
+						}
+					}
+				}
+			}
+		}
+	}
+	return input
+}
+
+type FilterQuals []FilterQual
+
+type FilterQual struct {
+	ColumnName   string
+	ColumnType   string
+	PropertyName string
 }

aws/table_aws_rds_db_cluster_snapshot.go
@@ -3,6 +3,7 @@ package aws
 import (
 	"context"
 
+	"github.com/turbot/go-kit/types"
 	"github.com/turbot/steampipe-plugin-sdk/grpc/proto"
 	"github.com/turbot/steampipe-plugin-sdk/plugin/transform"
 
@@ -24,6 +25,12 @@ func tableAwsRDSDBClusterSnapshot(_ context.Context) *plugin.Table {
 		},
 		List: &plugin.ListConfig{
 			Hydrate: listRDSDBClusterSnapshots,
+			KeyColumns: []*plugin.KeyColumn{
+				{Name: "db_cluster_identifier", Require: plugin.Optional},
+				{Name: "db_cluster_snapshot_identifier", Require: plugin.Optional},
+				{Name: "engine", Require: plugin.Optional},
+				{Name: "type", Require: plugin.Optional},
+			},
 		},
 		GetMatrixItem: BuildRegionList,
 		Columns: awsRegionalColumns([]*plugin.Column{
@@ -183,15 +190,44 @@ func listRDSDBClusterSnapshots(ctx context.Context, d *plugin.QueryData, _ *plug
 		return nil, err
 	}
 
-	// List call
-	err = svc.DescribeDBClusterSnapshotsPages(
-		&rds.DescribeDBClusterSnapshotsInput{},
-		func(page *rds.DescribeDBClusterSnapshotsOutput, isLast bool) bool {
-			for _, dbClusterSnapshot := range page.DBClusterSnapshots {
-				d.StreamListItem(ctx, dbClusterSnapshot)
+	input := rds.DescribeDBClusterSnapshotsInput{
+		MaxRecords: types.Int64(100),
+	}
+
+	// If the requested number of items is less than the paging max limit
+	// set the limit to that instead
+	limit := d.QueryContext.Limit
+	if d.QueryContext.Limit != nil {
+		// select * from aws_rds_db_cluster_snapshot limit 3
+		// Error: InvalidParameterValue: Invalid value 3 for MaxRecords. Must be between 20 and 100
+		// 	status code: 400, request id: c39eead1-96e0-49c8-a927-aa9a3131836d
+		if *limit < *input.MaxRecords {
+			if *limit < 20 {
+				input.MaxRecords = types.Int64(100)
+			} else {
+				input.MaxRecords = limit
 			}
-			return !isLast
-		},
+		}
+	}
+	filters := buildRdsDbClusterSnapshotFilter(d.KeyColumnQuals)
+
+	if len(filters) != 0 {
+		input.SetFilters(filters)
+	}
+
+	// List call
+	err = svc.DescribeDBClusterSnapshotsPages(&input, func(page *rds.DescribeDBClusterSnapshotsOutput, isLast bool) bool {
+		for _, dbClusterSnapshot := range page.DBClusterSnapshots {
+			d.StreamListItem(ctx, dbClusterSnapshot)
+			// Check if context has been cancelled or if the limit has been hit (if specified)
+			// if there is a limit, it will return the number of rows required to reach this limit
+			if d.QueryStatus.RowsRemaining(ctx) == 0 {
+				return true
+			}
+		}
+
+		return !isLast
+	},
 	)
 	return nil, err
 }
@@ -258,4 +294,33 @@ func getRDSDBClusterSnapshotTurbotTags(_ context.Context, d *transform.Transform
 		return turbotTagsMap, nil
 	}
 	return nil, nil
+}
+
+//// UTILITY FUNCTIONS
+
+// build snapshots list call input filter
+func buildRdsDbClusterSnapshotFilter(equalQuals plugin.KeyColumnEqualsQualMap) []*rds.Filter {
+	filters := make([]*rds.Filter, 0)
+	filterQuals := map[string]string{
+		"db_cluster_identifier":          "db-cluster-id",
+		"db_cluster_snapshot_identifier": "db-cluster-snapshot-id",
+		"engine":                         "engine",
+		"type":                           "snapshot-type",
+	}
+
+	for columnName, filterName := range filterQuals {
+		if equalQuals[columnName] != nil {
+			filter := rds.Filter{
+				Name: types.String(filterName),
+			}
+			value := equalQuals[columnName]
+			if value.GetStringValue() != "" {
+				filter.Values = []*string{types.String(equalQuals[columnName].GetStringValue())}
+			} else if value.GetListValue() != nil {
+				filter.Values = getListValues(value.GetListValue())
+			}
+			filters = append(filters, &filter)
+		}
+	}
+	return filters
 }

go.mod
@@ -8,5 +8,5 @@ require (
 	github.com/golang/protobuf v1.4.3
 	github.com/ttacon/chalk v0.0.0-20160626202418-22c06c80ed31
 	github.com/turbot/go-kit v0.2.2-0.20210628165333-268ba0a30be3
-	github.com/turbot/steampipe-plugin-sdk v1.5.1
+	github.com/turbot/steampipe-plugin-sdk v1.6.1
 )

go.sum
@@ -134,8 +134,8 @@ github.com/ttacon/chalk v0.0.0-20160626202418-22c06c80ed31 h1:OXcKh35JaYsGMRzpvF
 github.com/ttacon/chalk v0.0.0-20160626202418-22c06c80ed31/go.mod h1:onvgF043R+lC5RZ8IT9rBXDaEDnpnw/Cl+HFiw+v/7Q=
 github.com/turbot/go-kit v0.2.2-0.20210628165333-268ba0a30be3 h1:UAfWYp+K7oESlqomRus4k+h/dSPXU17tEcarbRdtBwQ=
 github.com/turbot/go-kit v0.2.2-0.20210628165333-268ba0a30be3/go.mod h1:SBdPRngbEfYubiR81iAVtO43oPkg1+ASr+XxvgbH7/k=
-github.com/turbot/steampipe-plugin-sdk v1.5.1 h1:xEYRbD0F2xlDLcL9vP6WgOr83x2lWvnxEmPXkahaacM=
-github.com/turbot/steampipe-plugin-sdk v1.5.1/go.mod h1:zM68yGM+wjkjDPz8yUTx6078GDTVWkrI26EC56XIspw=
+github.com/turbot/steampipe-plugin-sdk v1.6.1 h1:zXs0jiGyYc0vq3YwPDtw78jCZbirjSPEeTcZEDvoPJk=
+github.com/turbot/steampipe-plugin-sdk v1.6.1/go.mod h1:zM68yGM+wjkjDPz8yUTx6078GDTVWkrI26EC56XIspw=
 github.com/vmihailenco/msgpack v3.3.3+incompatible/go.mod h1:fy3FlTQTDXWkZ7Bh6AcGMlsjHatGryHQYUTf1ShIgkk=
 github.com/vmihailenco/msgpack/v4 v4.3.12/go.mod h1:gborTTJjAo/GWTqqRjrLCn9pgNN+NXzzngzBKDPIqw4=
 github.com/vmihailenco/tagparser v0.1.1/go.mod h1:OeAg3pn3UbLjkWt+rN9oFYB6u/cQgqMEUPoW2WPyhdI=

commit a776cebaa8e360d389d4eef7be04abb6a6107121

Fix: paging error introduced in PR #638

aws/table_aws_ec2_instance.go
@@ -381,7 +381,7 @@ func listEc2Instance(ctx context.Context, d *plugin.QueryData, _ *plugin.Hydrate
 					// Check if context has been cancelled or if the limit has been hit (if specified)
 					// if there is a limit, it will return the number of rows required to reach this limit
 					if d.QueryStatus.RowsRemaining(ctx) == 0 {
-						return true
+						return false
 					}
 				}
 			}

aws/table_aws_iam_policy.go
@@ -169,7 +169,7 @@ func listIamPolicies(ctx context.Context, d *plugin.QueryData, _ *plugin.Hydrate
 			// Check if context has been cancelled or if the limit has been hit (if specified)
 			// if there is a limit, it will return the number of rows required to reach this limit
 			if d.QueryStatus.RowsRemaining(ctx) == 0 {
-				return true
+				return false
 			}
 		}
 

aws/table_aws_rds_db_cluster_snapshot.go
@@ -222,7 +222,7 @@ func listRDSDBClusterSnapshots(ctx context.Context, d *plugin.QueryData, _ *plug
 			// Check if context has been cancelled or if the limit has been hit (if specified)
 			// if there is a limit, it will return the number of rows required to reach this limit
 			if d.QueryStatus.RowsRemaining(ctx) == 0 {
-				return true
+				return false
 			}
 		}
 
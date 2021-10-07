a776cebaa8e360d389d4eef7be04abb6a6107121
LalitTurbot <lalit@turbot.com>
2021-10-07 20:06:30 +0530

    Fix: paging error introduced in PR #638

--- aws/table_aws_rds_db_cluster_snapshot.go
+++ aws/table_aws_rds_db_cluster_snapshot.go
@@ -222,7 +222,7 @@ func listRDSDBClusterSnapshots(ctx context.Context, d *plugin.QueryData, _ *plug
 			// Check if context has been cancelled or if the limit has been hit (if specified)
 			// if there is a limit, it will return the number of rows required to reach this limit
 			if d.QueryStatus.RowsRemaining(ctx) == 0 {
-				return true
+				return false
 			}
 		}
 
