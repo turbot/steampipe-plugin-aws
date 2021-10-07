8afb3ea0541a0ae0da88a8f3f8db9fb883f13463
Lalit Bhardwaj <37527306+LalitTurbot@users.noreply.github.com>
2021-10-07 20:19:53 +0530

    Fix pagination rows remaining check in EC2, IAM, and RDS tables (#678)

--- aws/table_aws_iam_policy.go
+++ aws/table_aws_iam_policy.go
@@ -169,7 +169,7 @@ func listIamPolicies(ctx context.Context, d *plugin.QueryData, _ *plugin.Hydrate
 			// Check if context has been cancelled or if the limit has been hit (if specified)
 			// if there is a limit, it will return the number of rows required to reach this limit
 			if d.QueryStatus.RowsRemaining(ctx) == 0 {
-				return true
+				return false
 			}
 		}
 
