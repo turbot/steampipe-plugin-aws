ba9de80564d7a8b25429e05b7908bc72f1a1d906
Sanket Raut <sankeyraut@users.noreply.github.com>
2021-11-18 12:20:33 +0530

    Update aws/table_aws_elasticache_redis_metric_enginecpuutilization_hourly.go
    Co-authored-by: souravTurbot <78197905+bigdatasourav@users.noreply.github.com>

--- aws/table_aws_elasticache_redis_metric_enginecpuutilization_hourly.go
+++ aws/table_aws_elasticache_redis_metric_enginecpuutilization_hourly.go
@@ -22,7 +22,7 @@ func tableAwsElasticacheRedisEngineCPUUtilizationHourly(_ context.Context) *plug
 		Columns: awsRegionalColumns(cwMetricColumns(
 			[]*plugin.Column{
 				{
-					Name:        "cacheclusterid",
+					Name:        "cache_cluster_id",
 					Description: "The cache cluster id",
 					Type:        proto.ColumnType_STRING,
 					Transform:   transform.FromField("DimensionValue"),
