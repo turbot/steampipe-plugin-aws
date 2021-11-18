95fac05166368e9d4eda8bdea31b9c699e760708
Sanket Raut <sankeyraut@users.noreply.github.com>
2021-11-18 12:21:15 +0530

    Update aws/table_aws_elasticache_redis_metric_currconnections_hourly.go
    Co-authored-by: souravTurbot <78197905+bigdatasourav@users.noreply.github.com>

--- aws/table_aws_elasticache_redis_metric_currconnections_hourly.go
+++ aws/table_aws_elasticache_redis_metric_currconnections_hourly.go
@@ -22,7 +22,7 @@ func tableAwsElasticacheRedisMetricCurrConnectionsHourly(_ context.Context) *plu
 		Columns: awsRegionalColumns(cwMetricColumns(
 			[]*plugin.Column{
 				{
-					Name:        "cacheclusterid",
+					Name:        "cache_cluster_id",
 					Description: "The cache cluster id",
 					Type:        proto.ColumnType_STRING,
 					Transform:   transform.FromField("DimensionValue"),
