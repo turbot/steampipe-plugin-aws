881a0fdc074af0dfebdc738eca5d8bcd2a83dfc4
souravTurbot <78197905+bigdatasourav@users.noreply.github.com>
2021-11-23 11:10:46 +0530

    Fix GET request for table aws_lambda_alias. Fixes #777 (#781)

--- aws-test/tests/aws_lambda_alias/test-list-call-query.sql
+++ aws-test/tests/aws_lambda_alias/test-list-call-query.sql
@@ -1,3 +1,3 @@
 select alias_arn, description, function_name, function_version, name, akas, title
 from aws.aws_lambda_alias
-where akas::text = '["{{ output.resource_aka.value }}"]'
+where akas::text = '["{{ output.resource_aka.value }}"]';
