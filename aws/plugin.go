94f753e728754716e7c9dc750991757e106b75f6
ParthaI <47887552+ParthaI@users.noreply.github.com>
2022-02-14 17:16:37 +0530

    Add table aws_cloudwatch_metric closes #872 (#880)

--- aws/plugin.go
+++ aws/plugin.go
@@ -66,6 +66,7 @@ func Plugin(ctx context.Context) *plugin.Plugin {
 			"aws_cloudwatch_log_metric_filter":                             tableAwsCloudwatchLogMetricFilter(ctx),
 			"aws_cloudwatch_log_resource_policy":                           tableAwsCloudwatchLogResourcePolicy(ctx),
 			"aws_cloudwatch_log_stream":                                    tableAwsCloudwatchLogStream(ctx),
+			"aws_cloudwatch_metric":                                        tableAwsCloudWatchMetric(ctx),
 			"aws_codebuild_project":                                        tableAwsCodeBuildProject(ctx),
 			"aws_codebuild_source_credential":                              tableAwsCodeBuildSourceCredential(ctx),
 			"aws_codecommit_repository":                                    tableAwsCodeCommitRepository(ctx),
