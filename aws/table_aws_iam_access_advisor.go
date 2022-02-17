3229e740beeeae114ddd5c9a446ab0d3e543b814
bigdatasourav <bigdatasourav@gmail.com>
2022-02-17 12:56:03 +0530

    Add check for account id

--- aws/table_aws_iam_access_advisor.go
+++ aws/table_aws_iam_access_advisor.go
@@ -2,6 +2,7 @@ package aws
 
 import (
 	"context"
+	"strings"
 	"time"
 
 	"github.com/aws/aws-sdk-go/aws"
@@ -84,15 +85,27 @@ func tableAwsIamAccessAdvisor(_ context.Context) *plugin.Table {
 
 //// LIST FUNCTION
 
-func listAccessAdvisor(ctx context.Context, d *plugin.QueryData, _ *plugin.HydrateData) (interface{}, error) {
+func listAccessAdvisor(ctx context.Context, d *plugin.QueryData, h *plugin.HydrateData) (interface{}, error) {
 	logger := plugin.Logger(ctx)
-	logger.Trace("listAccessAdvisor")
+	logger.Error("listAccessAdvisor")
 
 	// To simplify the table we always get ACTION_LEVEL.  ACTION_LEVEL is a superset of
 	// SERVICE_LEVEL, and currently only s# supports action level actions anyway, so the
 	// performance impact is minimal
 	granularity := "ACTION_LEVEL"
 	principalArn := d.KeyColumnQuals["principal_arn"].GetStringValue()
+
+	getCommonColumnsCached := plugin.HydrateFunc(getCommonColumns).WithCache()
+	commonData, err := getCommonColumnsCached(ctx, d, h)
+	if err != nil {
+		return nil, err
+	}
+	commonColumnData := commonData.(*awsCommonColumnData)
+
+	// check if principalArn is empty or if the account id in the principalArn is not same with the account
+	if principalArn == "" || strings.Split(principalArn, ":")[4] != commonColumnData.AccountId {
+		return nil, nil
+	}
 
 	// Create Session
 	svc, err := IAMService(ctx, d)
@@ -153,7 +166,7 @@ func listAccessAdvisor(ctx context.Context, d *plugin.QueryData, _ *plugin.Hydra
 				TotalAuthenticatedEntities: serviceLastAccessed.TotalAuthenticatedEntities,
 				TrackedActionsLastAccessed: serviceLastAccessed.TrackedActionsLastAccessed,
 			})
-			
+
 			// Context may get cancelled due to manual cancellation or if the limit has been reached
 			if d.QueryStatus.RowsRemaining(ctx) == 0 {
 				break
