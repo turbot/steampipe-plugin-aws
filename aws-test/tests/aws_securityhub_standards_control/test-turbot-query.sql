ff7c635aeb6b6c46f33a3c2709a92f6c16af3176
souravTurbot <78197905+bigdatasourav@users.noreply.github.com>
2022-05-18 16:22:30 +0530

    Update column name standards_control_arn to arn in aws_securityhub_standards_control table. Closes #1026 (#1027)

--- aws-test/tests/aws_securityhub_standards_control/test-turbot-query.sql
+++ aws-test/tests/aws_securityhub_standards_control/test-turbot-query.sql
@@ -1,3 +1,3 @@
 select akas, region
 from aws_securityhub_standards_control
-where standards_control_arn = '{{ output.standards_control_arn.value }}';
+where arn = '{{ output.standards_control_arn.value }}';
