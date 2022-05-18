19155d9a3129a98323a6034cec41f53dd399850d
ParthaI <47887552+ParthaI@users.noreply.github.com>
2022-05-18 18:32:38 +0530

    Added the arn column

--- aws/table_aws_securityhub_finding.go
+++ aws/table_aws_securityhub_finding.go
@@ -45,6 +45,12 @@ func tableAwsSecurityHubFinding(_ context.Context) *plugin.Table {
 				Type:        proto.ColumnType_STRING,
 			},
 			{
+				Name:        "arn",
+				Description: "The Amazon Resource Name (ARN) for the finding.",
+				Type:        proto.ColumnType_STRING,
+				Transform:   transform.FromField("Id")
+			},
+			{
 				Name:        "company_name",
 				Description: "The name of the company for the product that generated the finding.",
 				Type:        proto.ColumnType_STRING,
