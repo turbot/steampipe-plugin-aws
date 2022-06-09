5dd8c3705b09bf11ef499ea73d73569aebbfb43c
ParthaI <partha@turbot.com>
2022-06-09 11:41:52 +0530

    Updates the position of context cancel logic

--- aws/table_aws_ssm_inventory.go
+++ aws/table_aws_ssm_inventory.go
@@ -115,13 +115,13 @@ func listAwsSSMInventories(ctx context.Context, d *plugin.QueryData, _ *plugin.H
 							TypeName:      v.TypeName,
 							Content:       v.Content,
 						})
+						// Context may get cancelled due to manual cancellation or if the limit has been reached
+						if d.QueryStatus.RowsRemaining(ctx) == 0 {
+							return false
+						}
 					}
 				}
 
-				// Context may get cancelled due to manual cancellation or if the limit has been reached
-				if d.QueryStatus.RowsRemaining(ctx) == 0 {
-					return false
-				}
 			}
 			return !isLast
 		},
