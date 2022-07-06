542cfbdd2df357a2ff143d362e761931781d64b1
LalitTurbot <lalit@turbot.com>
2022-07-06 13:21:23 +0530

    update aws_resource_policy_analysis columns description

--- aws/public_access.go
+++ aws/public_access.go
@@ -7,7 +7,6 @@ import (
 	"strings"
 
 	"github.com/aws/aws-sdk-go/aws/arn"
-
 	"github.com/turbot/go-kit/helpers"
 )
 
@@ -24,21 +23,18 @@ type PolicyEvaluation struct {
 }
 
 func (policy *Policy) EvaluatePolicy(sourceAccountID string) (*PolicyEvaluation, error) {
-	//TODO - bring source account information for getting public, private or shared level access info
 	re := regexp.MustCompile(`[0-9]{12}`)
 
 	var policyEvaluation PolicyEvaluation
 	if !re.Match([]byte(sourceAccountID)) {
 		return &policyEvaluation, fmt.Errorf("%s is not a valid. Please enter a valid account id", sourceAccountID)
 	}
-	// var public bool
+
 	if policy.Statements == nil {
 		return &policyEvaluation, nil
 	}
 
-	var publicActions, allowedOrgs, allowedAccounts, allowedServices, allowedFederatedIdentities, allowedPrincipals, publicStatementIds []string
-	deniedActions := []string{}
-	// deniedAccounts := []string{}
+	var publicActions, deniedActions, allowedOrgs, allowedAccounts, allowedServices, allowedFederatedIdentities, allowedPrincipals, publicStatementIds []string
 
 	// Only for allow statements
 	for index, stmt := range policy.Statements {
@@ -87,7 +83,6 @@ func (policy *Policy) EvaluatePolicy(sourceAccountID string) (*PolicyEvaluation,
 		} else if item == "*" || re.Match([]byte(item)) {
 			accountIds = append(accountIds, item)
 		}
-		// TODO - Should we add principals which doesn't have account ids
 	}
 	policyEvaluation.AllowedPrincipalAccountIds = append(allowedAccounts, accountIds...)
 
@@ -145,6 +140,7 @@ func (policy *Policy) EvaluatePolicy(sourceAccountID string) (*PolicyEvaluation,
 		policyEvaluation.AllowedPrincipalAccountIds = helpers.RemoveFromStringSlice(policyEvaluation.AllowedPrincipalAccountIds, []string{"*", sourceAccountID}...)
 	}
 
+	// Sort the slice items in object
 	sort.Strings(helpers.StringSliceDistinct(policyEvaluation.AllowedOrganizationIds))
 	sort.Strings(helpers.StringSliceDistinct(policyEvaluation.AllowedPrincipalFederatedIdentities))
 	sort.Strings(policyEvaluation.AllowedPrincipalAccountIds)
@@ -214,18 +210,17 @@ func (stmt *Statement) EvaluateStatement() (bool, PolicyEvaluation) {
 		internalPublicPrincipalOperator := true
 
 		// Code to detect public
+		// TODO: Handle ForAnyValue and ForAllValues in conditions
 		for operatorKey, operatorValue := range stmt.Condition {
 			hasIfExistsSuffix := CheckIfExistsSuffix(operatorKey)
 			operatorKey = strings.ReplaceAll(operatorKey, "IfExists", "")
-			// hasForAnyValue := strings.HasPrefix(operatorKey, "ForAnyValue:")
-			// hasForAllValues := strings.HasPrefix(operatorKey, "ForAllValues:")
 			operatorKey = strings.ReplaceAll(operatorKey, "ForAnyValue:", "")
 			operatorKey = strings.ReplaceAll(operatorKey, "ForAllValues:", "")
 
 			var typeOfOperator string = "Unknown"
 			typeOfOperator, operatorKey = getOperatorType(operatorKey)
 
-			hasNotInOperator := CheckNotInOperator(operatorKey)
+			hasNotInOperator := strings.Contains(operatorKey, "Not")
 			hasLikeOperator := strings.Contains(operatorKey, "Like")
 
 			if conditionOperatorValueMap, ok := operatorValue.(map[string]interface{}); ok {
@@ -439,7 +434,7 @@ func (stmt *Statement) EvaluateStatement() (bool, PolicyEvaluation) {
 			var typeOfOperator string = "Unknown"
 			typeOfOperator, operatorKey = getOperatorType(operatorKey)
 
-			hasNotInOperator := CheckNotInOperator(operatorKey)
+			hasNotInOperator := strings.Contains(operatorKey, "Not")
 			hasLikeOperator := strings.Contains(operatorKey, "Like")
 
 			if conditionOperatorValueMap, ok := operatorValue.(map[string]interface{}); ok {
@@ -509,7 +504,6 @@ func (stmt *Statement) EvaluateStatement() (bool, PolicyEvaluation) {
 }
 
 type DeniedStmtEvaluation struct {
-	Actions                            []string `json:"actions"`
 	DeniedOrganizationIds              []string `json:"denied_organization_ids"`
 	DeniedPrincipals                   []string `json:"denied_principals"`
 	DeniedPrincipalAccountIds          []string `json:"denied_principal_account_ids"`
@@ -519,29 +513,18 @@ type DeniedStmtEvaluation struct {
 
 // In a way "Effect" = "Deny" never allows grants but only explicitely denies the rights
 func (stmt *Statement) DenyStatementEvaluation() DeniedStmtEvaluation {
-	deniedEvaluation := DeniedStmtEvaluation{
-		Actions: stmt.Action,
-	}
+	deniedEvaluation := DeniedStmtEvaluation{}
 	if stmt.NotPrincipal != nil {
 		// makes policy unsolvable as it denies access to only principals mentioned in `NotPrincipal` but allows access to everyone else.
 		return deniedEvaluation
 	}
 
-	var awsPrincipals, deniedPrincipals, deniedOrgPaths, deniedServices, deniedFederatedPrincipal []string
+	var awsPrincipals, deniedPrincipals, deniedOrgPaths []string
 	if stmt.Principal != nil {
 		if data, ok := stmt.Principal["AWS"]; ok {
 			awsPrincipals = data.([]string)
 		}
-		if data, ok := stmt.Principal["Service"]; ok {
-			deniedServices = data.([]string)
-		}
-		if data, ok := stmt.Principal["Federated"]; ok {
-			deniedFederatedPrincipal = data.([]string)
-		}
 	}
-
-	deniedEvaluation.DeniedPrincipalServices = deniedServices
-	deniedEvaluation.DeniedPrincipalFederatedIdentities = deniedFederatedPrincipal
 
 	hasPublicPrincipal := false
 
@@ -562,7 +545,7 @@ func (stmt *Statement) DenyStatementEvaluation() DeniedStmtEvaluation {
 
 			var typeOfOperator string = "Unknown"
 			typeOfOperator, operatorKey = getOperatorType(operatorKey)
-			hasNotInOperator := CheckNotInOperator(operatorKey)
+			hasNotInOperator := strings.Contains(operatorKey, "Not")
 			hasLikeOperator := strings.Contains(operatorKey, "Like")
 
 			if conditionOperatorValueMap, ok := operatorValue.(map[string]interface{}); ok {
@@ -673,55 +656,10 @@ func (stmt *Statement) DenyStatementEvaluation() DeniedStmtEvaluation {
 	deniedEvaluation.DeniedOrganizationIds = deniedOrgPaths
 
 	return deniedEvaluation
-	// TODO: Instead of returning false should return an analysis to negate the allowed actions and principals from other allowed statements in the policy - more useful for the case of per principal analysis
 }
 
-/*
-https://docs.aws.amazon.com/IAM/latest/UserGuide/reference_policies_multi-value-conditions.html
-
-// ForAllValues – Use with multivalued condition keys. Tests whether the value of every member of the request set is a subset of the condition key set. The condition returns true if every key value in the request matches at least one value in the policy. It also returns true if there are no keys in the request, or if the key values resolve to a null data set, such as an empty string. Do not use ForAllValues with an Allow effect because it can be overly permissive.
-
-ForAnyValue – Use with multivalued condition keys. Tests whether at least one member of the set of request values matches at least one member of the set of condition key values. The condition returns true if any one of the key values in the request matches any one of the condition values in the policy. For no matching key or a null dataset, the condition returns false.
-
-The the difference between single-valued and multivalued condition keys depends on the number of values in the request context, not the number of values in the policy condition.
-*/
 func CheckIfExistsSuffix(key string) bool {
 	return strings.HasSuffix(key, "IfExists")
-}
-func CheckNotInOperator(operator string) bool {
-	return strings.Contains(operator, "Not")
-}
-
-func removeNotFromOperator(operatorKey string) string {
-	return strings.ReplaceAll(operatorKey, "Not", "")
-}
-
-func intersection(s1, s2 []string) (inter []string) {
-	hash := make(map[string]bool)
-	for _, e := range s1 {
-		hash[e] = true
-	}
-	for _, e := range s2 {
-		// If elements present in the hashmap then append intersection list.
-		if hash[e] {
-			inter = append(inter, e)
-		}
-	}
-	//Remove dups from slice.
-	inter = removeDups(inter)
-	return
-}
-
-//Remove dups from slice.
-func removeDups(elements []string) (nodups []string) {
-	encountered := make(map[string]bool)
-	for _, element := range elements {
-		if !encountered[element] {
-			nodups = append(nodups, element)
-			encountered[element] = true
-		}
-	}
-	return
 }
 
 func GetAccessLevelsFromActions(permissionsData ParliamentPermissions, actions []string) []string {
@@ -753,6 +691,7 @@ func GetAccessLevelsFromActions(permissionsData ParliamentPermissions, actions [
 	return helpers.StringSliceDistinct(accessLevels)
 }
 
+// getOperatorType:: to get the type of operator sting, arn, bool, etc..
 func getOperatorType(key string) (operatorType string, newKey string) {
 	var typeOfOperator string = "Unknown"
 	switch key {
