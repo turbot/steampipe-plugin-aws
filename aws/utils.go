f0050934d5ebb40664c8ffe374502d3b37049c6f
Binaek Sarkar <binaek@turbot.com>
2022-07-13 15:20:17 +0530

    util

--- aws/utils.go
+++ aws/utils.go
@@ -18,7 +18,7 @@ import (
 	"github.com/turbot/steampipe-plugin-sdk/v3/plugin/transform"
 )
 
-var stringPointer = func(s string) *string { return &s }
+func PointerOf[T any](v T) *T { return &v }
 
 func ec2TagsToMap(tags []*ec2.Tag) (*map[string]string, error) {
 	var turbotTagsMap map[string]string
