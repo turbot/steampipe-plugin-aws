1f503182517c28bb1c2fc125522caa0bb7f47f25
LalitTurbot <lalit@turbot.com>
2022-07-26 15:44:41 +0530

    migrate aws_iam_policy_attachment table to aws-sdk-go-v2

--- aws/table_aws_iam_policy.go
+++ aws/table_aws_iam_policy.go
@@ -162,6 +162,7 @@ func listIamPolicies(ctx context.Context, d *plugin.QueryData, _ *plugin.Hydrate
 			maxItems = limit
 		}
 	}
+
 	paginator := iam.NewListPoliciesPaginator(svc, &params, func(o *iam.ListPoliciesPaginatorOptions) {
 		o.Limit = maxItems
 		o.StopOnDuplicateToken = true
