92f396578ccb5f7049c09c058a784c02130d827f
ParthaI <partha@turbot.com>
2022-08-04 18:53:14 +0530

    migrated aws_vpc_eip

--- aws/table_aws_vpc_eip.go
+++ aws/table_aws_vpc_eip.go
@@ -3,8 +3,8 @@ package aws
 import (
 	"context"
 
-	"github.com/aws/aws-sdk-go/aws"
-	"github.com/aws/aws-sdk-go/service/ec2"
+	"github.com/aws/aws-sdk-go-v2/service/ec2"
+	"github.com/aws/aws-sdk-go-v2/service/ec2/types"
 	"github.com/turbot/steampipe-plugin-sdk/v3/grpc/proto"
 	"github.com/turbot/steampipe-plugin-sdk/v3/plugin"
 	"github.com/turbot/steampipe-plugin-sdk/v3/plugin/transform"
@@ -17,7 +17,7 @@ func tableAwsVpcEip(_ context.Context) *plugin.Table {
 		Get: &plugin.GetConfig{
 			KeyColumns: plugin.SingleColumn("allocation_id"),
 			IgnoreConfig: &plugin.IgnoreConfig{
-				ShouldIgnoreErrorFunc: isNotFoundError([]string{"InvalidAllocationID.NotFound", "InvalidAllocationID.Malformed"}),
+				ShouldIgnoreErrorFunc: isNotFoundErrorV2([]string{"InvalidAllocationID.NotFound", "InvalidAllocationID.Malformed"}),
 			},
 			Hydrate: getVpcEip,
 		},
@@ -142,12 +142,11 @@ func tableAwsVpcEip(_ context.Context) *plugin.Table {
 //// LIST FUNCTION
 
 func listVpcEips(ctx context.Context, d *plugin.QueryData, _ *plugin.HydrateData) (interface{}, error) {
-	region := d.KeyColumnQualString(matrixKeyRegion)
-	plugin.Logger(ctx).Trace("listVpcEips", "AWS_REGION", region)
 
 	// Create session
-	svc, err := Ec2Service(ctx, d, region)
+	svc, err := Ec2Client(ctx, d)
 	if err != nil {
+		plugin.Logger(ctx).Error("aws_vpc_eip.listVpcEips", "connection_error", err)
 		return nil, err
 	}
 
@@ -170,7 +169,10 @@ func listVpcEips(ctx context.Context, d *plugin.QueryData, _ *plugin.HydrateData
 	}
 
 	// List call
-	resp, err := svc.DescribeAddresses(input)
+	resp, err := svc.DescribeAddresses(ctx, input)
+	if err != nil {
+		plugin.Logger(ctx).Error("aws_vpc_eip.listVpcEips", "api_error", err)
+	}
 	for _, address := range resp.Addresses {
 		d.StreamListItem(ctx, address)
 
@@ -186,26 +188,25 @@ func listVpcEips(ctx context.Context, d *plugin.QueryData, _ *plugin.HydrateData
 //// HYDRATE FUNCTIONS
 
 func getVpcEip(ctx context.Context, d *plugin.QueryData, _ *plugin.HydrateData) (interface{}, error) {
-	plugin.Logger(ctx).Trace("getVpcEip")
 
-	region := d.KeyColumnQualString(matrixKeyRegion)
 	allocationID := d.KeyColumnQuals["allocation_id"].GetStringValue()
 
 	// get service
-	svc, err := Ec2Service(ctx, d, region)
+	svc, err := Ec2Client(ctx, d)
 	if err != nil {
+		plugin.Logger(ctx).Error("aws_vpc_eip.getVpcEip", "connection_error", err)
 		return nil, err
 	}
 
 	// Build the params
 	params := &ec2.DescribeAddressesInput{
-		AllocationIds: []*string{aws.String(allocationID)},
+		AllocationIds: []string{allocationID},
 	}
 
 	// Get call
-	op, err := svc.DescribeAddresses(params)
+	op, err := svc.DescribeAddresses(ctx, params)
 	if err != nil {
-		plugin.Logger(ctx).Debug("getVpcEip__", "ERROR", err)
+		plugin.Logger(ctx).Error("aws_vpc_eip.getVpcEip", "api_error", err)
 		return nil, err
 	}
 
@@ -218,7 +219,7 @@ func getVpcEip(ctx context.Context, d *plugin.QueryData, _ *plugin.HydrateData) 
 func getVpcEipARN(ctx context.Context, d *plugin.QueryData, h *plugin.HydrateData) (interface{}, error) {
 	plugin.Logger(ctx).Trace("getVpcEipARN")
 	region := d.KeyColumnQualString(matrixKeyRegion)
-	eip := h.Item.(*ec2.Address)
+	eip := h.Item.(types.Address)
 	getCommonColumnsCached := plugin.HydrateFunc(getCommonColumns).WithCache()
 	commonData, err := getCommonColumnsCached(ctx, d, h)
 	if err != nil {
@@ -235,6 +236,16 @@ func getVpcEipARN(ctx context.Context, d *plugin.QueryData, h *plugin.HydrateDat
 //// TRANSFORM FUNCTIONS
 
 func getVpcEipTurbotTags(_ context.Context, d *transform.TransformData) (interface{}, error) {
-	eip := d.HydrateItem.(*ec2.Address)
-	return ec2TagsToMap(eip.Tags)
+	tags := d.HydrateItem.(types.Address).Tags
+	var turbotTagsMap map[string]string
+	if tags == nil {
+		return nil, nil
+	}
+
+	turbotTagsMap = map[string]string{}
+	for _, i := range tags {
+		turbotTagsMap[*i.Key] = *i.Value
+	}
+
+	return &turbotTagsMap, nil
 }
