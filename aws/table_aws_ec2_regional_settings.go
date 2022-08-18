f759455c312c42d9acf735432d5bdef778bef988
ParthaI <partha@turbot.com>
2022-08-18 19:08:00 +0530

    Migrated aws_ec2_regional_settings

--- aws/table_aws_ec2_regional_settings.go
+++ aws/table_aws_ec2_regional_settings.go
@@ -2,9 +2,9 @@ package aws
 
 import (
 	"context"
+	"strings"
 
-	"github.com/aws/aws-sdk-go/aws/awserr"
-	"github.com/aws/aws-sdk-go/service/ec2"
+	"github.com/aws/aws-sdk-go-v2/service/ec2"
 
 	"github.com/turbot/steampipe-plugin-sdk/v3/grpc/proto"
 	"github.com/turbot/steampipe-plugin-sdk/v3/plugin"
@@ -52,7 +52,6 @@ func tableAwsEc2RegionalSettings(_ context.Context) *plugin.Table {
 
 func listEc2RegionalSettings(ctx context.Context, d *plugin.QueryData, _ *plugin.HydrateData) (interface{}, error) {
 	region := d.KeyColumnQualString(matrixKeyRegion)
-	plugin.Logger(ctx).Trace("listEc2RegionalSettings", "AWS_REGION", region)
 
 	d.StreamListItem(ctx, region)
 	return nil, nil
@@ -61,24 +60,19 @@ func listEc2RegionalSettings(ctx context.Context, d *plugin.QueryData, _ *plugin
 //// HYDRATE FUNCTIONS
 
 func getDefaultEBSVolumeEncryption(ctx context.Context, d *plugin.QueryData, _ *plugin.HydrateData) (interface{}, error) {
-	plugin.Logger(ctx).Trace("getDefaultEBSVolumeEncryption")
-
-	region := d.KeyColumnQualString(matrixKeyRegion)
-	plugin.Logger(ctx).Trace("listEc2RegionalSettings", "AWS_REGION", region)
 
 	// Create session
-	svc, err := Ec2Service(ctx, d, region)
+	svc, err := Ec2Client(ctx, d)
 	if err != nil {
+		plugin.Logger(ctx).Error("aws_ec2_regional_settings.getDefaultEBSVolumeEncryption", "connection_error", err)
 		return nil, err
 	}
 	params := &ec2.GetEbsEncryptionByDefaultInput{}
-	defaultEncryption, err := svc.GetEbsEncryptionByDefault(params)
+	defaultEncryption, err := svc.GetEbsEncryptionByDefault(ctx, params)
 	if err != nil {
-		if a, ok := err.(awserr.Error); ok {
-			// Returning false for disabled regions
-			if a.Code() == "AuthFailure" {
-				return false, nil
-			}
+		plugin.Logger(ctx).Error("aws_ec2_regional_settings.getDefaultEBSVolumeEncryption", "api_error", err)
+		if strings.Contains(err.Error(), "AuthFailure") {
+			return false, nil
 		}
 		return nil, err
 	}
@@ -86,25 +80,21 @@ func getDefaultEBSVolumeEncryption(ctx context.Context, d *plugin.QueryData, _ *
 }
 
 func getDefaultEBSVolumeEncryptionKey(ctx context.Context, d *plugin.QueryData, _ *plugin.HydrateData) (interface{}, error) {
-	plugin.Logger(ctx).Trace("getDefaultEBSVolumeEncryptionKey")
-
-	region := d.KeyColumnQualString(matrixKeyRegion)
-	plugin.Logger(ctx).Trace("listEc2RegionalSettings", "AWS_REGION", region)
 
 	// Create session
-	svc, err := Ec2Service(ctx, d, region)
+	svc, err := Ec2Client(ctx, d)
 	if err != nil {
+		plugin.Logger(ctx).Error("aws_ec2_regional_settings.getDefaultEBSVolumeEncryptionKey", "connection_error", err)
 		return nil, err
 	}
 	params := &ec2.GetEbsDefaultKmsKeyIdInput{}
-	defaultEncryptionKey, err := svc.GetEbsDefaultKmsKeyId(params)
+	defaultEncryptionKey, err := svc.GetEbsDefaultKmsKeyId(ctx, params)
 	if err != nil {
-		if a, ok := err.(awserr.Error); ok {
-			// Returning default ebs key alias for disabled regions
-			if a.Code() == "AuthFailure" {
-				return "alias/aws/ebs", nil
-			}
+		plugin.Logger(ctx).Error("aws_ec2_regional_settings.getDefaultEBSVolumeEncryptionKey", "api_error", err)
+		if strings.Contains(err.Error(), "AuthFailure") {
+			return "alias/aws/ebs", nil
 		}
+
 		return nil, err
 	}
 	return defaultEncryptionKey.KmsKeyId, nil
