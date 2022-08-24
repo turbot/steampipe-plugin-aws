ccaf25b300122e61333b5a637ff01023034ff21e
misraved <ved@turbot.com>
2022-08-24 18:11:08 +0530

    Fix lint errors

--- aws/service_v2.go
+++ aws/service_v2.go
@@ -147,21 +147,21 @@ func getClient(ctx context.Context, d *plugin.QueryData, region string) (*aws.Co
 
 // Get a session for the region defined in query data, but only after checking it's
 // a supported region for the given serviceID.
-func getClientForQuerySupportedRegion(ctx context.Context, d *plugin.QueryData, serviceID string) (*aws.Config, error) {
-	region := d.KeyColumnQualString(matrixKeyRegion)
-	if region == "" {
-		return nil, fmt.Errorf("getSessionForQueryRegion called without a region in QueryData")
-	}
-	validRegions := SupportedRegionsForService(ctx, d, serviceID)
-	if !helpers.StringSliceContains(validRegions, region) {
-		// We choose to ignore unsupported regions rather than returning an error
-		// for them - it's a better user experience. So, return a nil session rather
-		// than an error. The caller must handle this case.
-		return nil, nil
-	}
-	// Supported region, so get and return the session
-	return getClient(ctx, d, region)
-}
+// func getClientForQuerySupportedRegion(ctx context.Context, d *plugin.QueryData, serviceID string) (*aws.Config, error) {
+// 	region := d.KeyColumnQualString(matrixKeyRegion)
+// 	if region == "" {
+// 		return nil, fmt.Errorf("getSessionForQueryRegion called without a region in QueryData")
+// 	}
+// 	validRegions := SupportedRegionsForService(ctx, d, serviceID)
+// 	if !helpers.StringSliceContains(validRegions, region) {
+// 		// We choose to ignore unsupported regions rather than returning an error
+// 		// for them - it's a better user experience. So, return a nil session rather
+// 		// than an error. The caller must handle this case.
+// 		return nil, nil
+// 	}
+// 	// Supported region, so get and return the session
+// 	return getClient(ctx, d, region)
+// }
 
 // Helper function to get the session for a region set in query data
 func getClientForQueryRegion(ctx context.Context, d *plugin.QueryData) (*aws.Config, error) {
