c3635dac3680fdeaa7bee244d75a91ca654e73c8
ParthaI <partha@turbot.com>
2022-08-25 23:06:10 +0530

    Migrated aws_ec2_application_load_balancer_metric_request_count

--- aws/table_aws_ec2_application_load_balancer_metric_request_count.go
+++ aws/table_aws_ec2_application_load_balancer_metric_request_count.go
@@ -4,7 +4,7 @@ import (
 	"context"
 	"strings"
 
-	"github.com/aws/aws-sdk-go/service/elbv2"
+	elbv2 "github.com/aws/aws-sdk-go-v2/service/elasticloadbalancingv2/types"
 	"github.com/turbot/steampipe-plugin-sdk/v3/grpc/proto"
 	"github.com/turbot/steampipe-plugin-sdk/v3/plugin"
 	"github.com/turbot/steampipe-plugin-sdk/v3/plugin/transform"
@@ -33,7 +33,7 @@ func tableAwsEc2ApplicationLoadBalancerMetricRequestCount(_ context.Context) *pl
 }
 
 func listEc2ApplicationLoadBalancerMetricRequestCount(ctx context.Context, d *plugin.QueryData, h *plugin.HydrateData) (interface{}, error) {
-	loadBalancer := h.Item.(*elbv2.LoadBalancer)
+	loadBalancer := h.Item.(elbv2.LoadBalancer)
 	arn := strings.SplitN(*loadBalancer.LoadBalancerArn, "/", 2)[1]
 	return listCWMetricStatistics(ctx, d, "5_MIN", "AWS/ApplicationELB", "RequestCount", "LoadBalancer", arn)
 }
