1f47b828d2a984ac283184757a8573727a2556cd
LalitTurbot <lalit@turbot.com>
2022-08-25 20:17:40 +0530

    update audit manager client for supported regions

--- aws/service_v2.go
+++ aws/service_v2.go
@@ -24,6 +24,7 @@ import (
 	"github.com/aws/aws-sdk-go-v2/service/s3"
 	"github.com/aws/aws-sdk-go-v2/service/sns"
 
+	"github.com/turbot/go-kit/helpers"
 	"github.com/turbot/steampipe-plugin-sdk/v4/plugin"
 )
 
@@ -70,7 +71,7 @@ func APIGatewayV2Client(ctx context.Context, d *plugin.QueryData) (*apigatewayv2
 }
 
 func AuditManagerClient(ctx context.Context, d *plugin.QueryData) (*auditmanager.Client, error) {
-	cfg, err := getClientForQueryRegion(ctx, d)
+	cfg, err := getClientForQuerySupportedRegion(ctx, d, "auditmanager")
 	if err != nil {
 		return nil, err
 	}
@@ -110,14 +111,11 @@ func S3Client(ctx context.Context, d *plugin.QueryData, region string) (*s3.Clie
 }
 
 func SNSClient(ctx context.Context, d *plugin.QueryData) (*sns.Client, error) {
-	start := time.Now()
 	cfg, err := getClientForQueryRegion(ctx, d)
 	if err != nil {
 		return nil, err
 	}
-	result := sns.NewFromConfig(*cfg)
-	plugin.Logger(ctx).Warn(fmt.Sprintf("SNSClient exec time: %v", time.Since(start)))
-	return result, nil
+	return sns.NewFromConfig(*cfg), nil
 }
 
 func getClient(ctx context.Context, d *plugin.QueryData, region string) (*aws.Config, error) {
@@ -174,21 +172,21 @@ func getClient(ctx context.Context, d *plugin.QueryData, region string) (*aws.Co
 
 // Get a session for the region defined in query data, but only after checking it's
 // a supported region for the given serviceID.
-// func getClientForQuerySupportedRegion(ctx context.Context, d *plugin.QueryData, serviceID string) (*aws.Config, error) {
-// 	region := d.KeyColumnQualString(matrixKeyRegion)
-// 	if region == "" {
-// 		return nil, fmt.Errorf("getSessionForQueryRegion called without a region in QueryData")
-// 	}
-// 	validRegions := SupportedRegionsForService(ctx, d, serviceID)
-// 	if !helpers.StringSliceContains(validRegions, region) {
-// 		// We choose to ignore unsupported regions rather than returning an error
-// 		// for them - it's a better user experience. So, return a nil session rather
-// 		// than an error. The caller must handle this case.
-// 		return nil, nil
-// 	}
-// 	// Supported region, so get and return the session
-// 	return getClient(ctx, d, region)
-// }
+func getClientForQuerySupportedRegion(ctx context.Context, d *plugin.QueryData, serviceID string) (*aws.Config, error) {
+	region := d.KeyColumnQualString(matrixKeyRegion)
+	if region == "" {
+		return nil, fmt.Errorf("getSessionForQueryRegion called without a region in QueryData")
+	}
+	validRegions := SupportedRegionsForService(ctx, d, serviceID)
+	if !helpers.StringSliceContains(validRegions, region) {
+		// We choose to ignore unsupported regions rather than returning an error
+		// for them - it's a better user experience. So, return a nil session rather
+		// than an error. The caller must handle this case.
+		return nil, nil
+	}
+	// Supported region, so get and return the session
+	return getClient(ctx, d, region)
+}
 
 // Helper function to get the session for a region set in query data
 func getClientForQueryRegion(ctx context.Context, d *plugin.QueryData) (*aws.Config, error) {
