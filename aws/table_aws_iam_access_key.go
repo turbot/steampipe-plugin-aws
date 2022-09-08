f0bdbb8585b9a8f6f338d6457702ff94269be135
misraved <ved@turbot.com>
2022-09-08 23:48:40 +0530

    Update logging data

--- aws/table_aws_iam_access_key.go
+++ aws/table_aws_iam_access_key.go
@@ -156,7 +156,7 @@ func getIamAccessKeyLastUsed(ctx context.Context, d *plugin.QueryData, h *plugin
 	// Create Session
 	svc, err := IAMClient(ctx, d)
 	if err != nil {
-	  plugin.Logger(ctx).Error("aws_iam_access_key.listUserAccessKeys", "client_error", err)
+	  plugin.Logger(ctx).Error("aws_iam_access_key.getIamAccessKeyLastUsed", "client_error", err)
 	  return nil, err
 	}
 
