a25693a77d5a6d6a9c9954e8275bddfa57c0455b
rajmohanty17 <raj@turbot.com>
2022-09-29 17:59:41 +0530

    handling dimension in custom struct

--- aws/table_aws_route53_health_check.go
+++ aws/table_aws_route53_health_check.go
@@ -2,15 +2,14 @@ package aws
 
 import (
 	"context"
-	"fmt"
 
-	"github.com/aws/aws-sdk-go/aws"
-	"github.com/aws/aws-sdk-go/service/route53"
-	"github.com/turbot/go-kit/types"
-	"github.com/turbot/steampipe-plugin-sdk/v4/plugin"
-	"github.com/turbot/steampipe-plugin-sdk/v4/plugin/transform"
+	"github.com/aws/aws-sdk-go-v2/aws"
+	"github.com/aws/aws-sdk-go-v2/service/route53"
+	"github.com/aws/aws-sdk-go-v2/service/route53/types"
 
 	"github.com/turbot/steampipe-plugin-sdk/v4/grpc/proto"
+	"github.com/turbot/steampipe-plugin-sdk/v4/plugin"
+	"github.com/turbot/steampipe-plugin-sdk/v4/plugin/transform"
 )
 
 func tableAwsRoute53HealthCheck(_ context.Context) *plugin.Table {
@@ -105,57 +104,129 @@ func tableAwsRoute53HealthCheck(_ context.Context) *plugin.Table {
 	}
 }
 
+// A complex type that contains information about the CloudWatch alarm that Amazon
+// Route 53 is monitoring for this health check.
+type CloudWatchAlarmConfiguration struct {
+
+	// For the metric that the CloudWatch alarm is associated with, the arithmetic
+	// operation that is used for the comparison.
+	//
+	// This member is required.
+	ComparisonOperator types.ComparisonOperator
+
+	// For the metric that the CloudWatch alarm is associated with, the number of
+	// periods that the metric is compared to the threshold.
+	//
+	// This member is required.
+	EvaluationPeriods *int32
+
+	// The name of the CloudWatch metric that the alarm is associated with.
+	//
+	// This member is required.
+	MetricName *string
+
+	// The namespace of the metric that the alarm is associated with. For more
+	// information, see Amazon CloudWatch Namespaces, Dimensions, and Metrics Reference
+	// (https://docs.aws.amazon.com/AmazonCloudWatch/latest/DeveloperGuide/CW_Support_For_AWS.html)
+	// in the Amazon CloudWatch User Guide.
+	//
+	// This member is required.
+	Namespace *string
+
+	// For the metric that the CloudWatch alarm is associated with, the duration of one
+	// evaluation period in seconds.
+	//
+	// This member is required.
+	Period *int32
+
+	// For the metric that the CloudWatch alarm is associated with, the statistic that
+	// is applied to the metric.
+	//
+	// This member is required.
+	Statistic types.Statistic
+
+	// For the metric that the CloudWatch alarm is associated with, the value the
+	// metric is compared with.
+	//
+	// This member is required.
+	Threshold *float64
+
+	// For the metric that the CloudWatch alarm is associated with, a complex type that
+	// contains information about the dimensions for the metric. For information, see
+	// Amazon CloudWatch Namespaces, Dimensions, and Metrics Reference
+	// (https://docs.aws.amazon.com/AmazonCloudWatch/latest/DeveloperGuide/CW_Support_For_AWS.html)
+	// in the Amazon CloudWatch User Guide.
+	Dimensions []types.Dimension
+
+}
+
 //// LIST FUNCTION
 
 func listHealthChecks(ctx context.Context, d *plugin.QueryData, _ *plugin.HydrateData) (interface{}, error) {
 	// Create session
-	svc, err := Route53Service(ctx, d)
+	svc, err := Route53Client(ctx, d)
 	if err != nil {
-		plugin.Logger(ctx).Error("aws_route53_health_check.listHealthChecks", "service_creation_error", err)
+		plugin.Logger(ctx).Error("aws_route53_health_check.listHealthChecks", "client_error", err)
 		return nil, err
 	}
 
-	input := &route53.ListHealthChecksInput{
-		MaxItems: aws.String("100"),
-	}
+	maxItems := int32(100)
+	input := route53.ListHealthChecksInput{}
 
 	// Reduce the basic request limit down if the user has only requested a small number of rows
-	limit := d.QueryContext.Limit
 	if d.QueryContext.Limit != nil {
-		if *limit < 100 {
-			if *limit < 1 {
-				input.MaxItems = aws.String("1")
+		limit := int32(*d.QueryContext.Limit)
+		if limit < maxItems {
+			if limit < 1 {
+				maxItems = int32(1)
 			} else {
-				input.MaxItems = aws.String(fmt.Sprint(*limit))
+				maxItems = int32(limit)
 			}
 		}
 	}
 
-	err = svc.ListHealthChecksPages(
-		input,
-		func(page *route53.ListHealthChecksOutput, isLast bool) bool {
-			for _, healthCheck := range page.HealthChecks {
-				d.StreamListItem(ctx, healthCheck)
+	input.MaxItems = aws.Int32(maxItems)
+	paginator := route53.NewListHealthChecksPaginator(svc, &input, func(o *route53.ListHealthChecksPaginatorOptions) {
+		o.Limit = maxItems
+		o.StopOnDuplicateToken = true
+	})
 
-				// Context may get cancelled due to manual cancellation or if the limit has been reached
-				if d.QueryStatus.RowsRemaining(ctx) == 0 {
-					return false
-				}
+	for paginator.HasMorePages() {
+		output, err := paginator.NextPage(ctx)
+		if err != nil {
+			plugin.Logger(ctx).Error("aws_route53_health_check.listHealthChecks", "api_error", err)
+			return nil, err
+		}
+
+		for _, healthCheck := range output.HealthChecks {
+
+			dimensions := healthCheck.CloudWatchAlarmConfiguration.Dimensions
+
+		if len(dimensions) == 0 {
+		return CloudWatchAlarmConfiguration{op.BucketName, op.LastFailureMessage, op.LastFailureTime, op.LastSuccessfulDeliveryTime, nil, op.LogExports, op.LoggingEnabled, op.S3KeyPrefix}, nil
+	}
+
+	return LoggingStatus{op.BucketName, op.LastFailureMessage, op.LastFailureTime, op.LastSuccessfulDeliveryTime, aws.String(logDestinationType), op.LogExports, op.LoggingEnabled, op.S3KeyPrefix}, nil
+
+			d.StreamListItem(ctx, healthCheck)
+
+			// Context may get cancelled due to manual cancellation or if the limit has been reached
+			if d.QueryStatus.RowsRemaining(ctx) == 0 {
+				return nil, nil
 			}
-			return !isLast
-		},
-	)
+		}
+	}
 
-	return nil, err
+	return nil, nil
 }
 
 //// HYDRATE FUNCTIONS
 
 func getHealthCheck(ctx context.Context, d *plugin.QueryData, _ *plugin.HydrateData) (interface{}, error) {
 	// Create session
-	svc, err := Route53Service(ctx, d)
+	svc, err := Route53Client(ctx, d)
 	if err != nil {
-		plugin.Logger(ctx).Error("aws_route53_health_check.getHealthCheck", "service_creation_error", err)
+		plugin.Logger(ctx).Error("aws_route53_health_check.getHealthCheck", "client_error", err)
 		return nil, err
 	}
 	id := d.KeyColumnQuals["id"].GetStringValue()
@@ -169,8 +240,8 @@ func getHealthCheck(ctx context.Context, d *plugin.QueryData, _ *plugin.HydrateD
 		HealthCheckId: aws.String(id),
 	}
 
-	// execute list call
-	item, err := svc.GetHealthCheck(params)
+	// Get call
+	item, err := svc.GetHealthCheck(ctx, params)
 	if err != nil {
 		plugin.Logger(ctx).Error("aws_route53_health_check.getHealthCheck", "api_error", err)
 		return nil, err
@@ -180,12 +251,12 @@ func getHealthCheck(ctx context.Context, d *plugin.QueryData, _ *plugin.HydrateD
 }
 
 func getHealthCheckStatus(ctx context.Context, d *plugin.QueryData, h *plugin.HydrateData) (interface{}, error) {
-	healthCheck := h.Item.(*route53.HealthCheck)
+	healthCheck := h.Item.(types.HealthCheck)
 
 	// Create session
-	svc, err := Route53Service(ctx, d)
+	svc, err := Route53Client(ctx, d)
 	if err != nil {
-		plugin.Logger(ctx).Error("aws_route53_health_check.getHealthCheckStatus", "service_creation_error", err)
+		plugin.Logger(ctx).Error("aws_route53_health_check.getHealthCheckStatus", "client_error", err)
 		return nil, err
 	}
 
@@ -193,8 +264,8 @@ func getHealthCheckStatus(ctx context.Context, d *plugin.QueryData, h *plugin.Hy
 		HealthCheckId: aws.String(*healthCheck.Id),
 	}
 
-	// execute list call
-	item, err := svc.GetHealthCheckStatus(params)
+	// execute get call
+	item, err := svc.GetHealthCheckStatus(ctx, params)
 	if err != nil {
 		plugin.Logger(ctx).Error("aws_route53_health_check.getHealthCheckStatus", "api_error", err)
 		return nil, err
@@ -204,32 +275,32 @@ func getHealthCheckStatus(ctx context.Context, d *plugin.QueryData, h *plugin.Hy
 }
 
 func getHealthCheckTags(ctx context.Context, d *plugin.QueryData, h *plugin.HydrateData) (interface{}, error) {
-	healthCheck := h.Item.(*route53.HealthCheck)
+	healthCheck := h.Item.(types.HealthCheck)
 
 	// Create session
-	svc, err := Route53Service(ctx, d)
+	svc, err := Route53Client(ctx, d)
 	if err != nil {
-		plugin.Logger(ctx).Error("aws_route53_health_check.getHealthCheckTags", "service_creation_error", err)
+		plugin.Logger(ctx).Error("aws_route53_health_check.getHealthCheckTags", "client_error", err)
 		return nil, err
 	}
 
 	params := &route53.ListTagsForResourceInput{
 		ResourceId:   healthCheck.Id,
-		ResourceType: types.String("healthcheck"),
+		ResourceType: "healthcheck",
 	}
 
 	// execute list call
-	resp, err := svc.ListTagsForResource(params)
+	resp, err := svc.ListTagsForResource(ctx, params)
 	if err != nil {
-		plugin.Logger(ctx).Error("aws_route53_health_check.getHealthCheckTags", "api_error", err)
-		return nil, err
-	}
+	plugin.Logger(ctx).Error("aws_route53_health_check.getHealthCheckTags", "api_error", err)
+	return nil, err
+}
 
 	return resp, nil
 }
 
 func getRoute53HealthCheckTurbotAkas(ctx context.Context, d *plugin.QueryData, h *plugin.HydrateData) (interface{}, error) {
-	healthCheck := h.Item.(*route53.HealthCheck)
+	healthCheck := h.Item.(types.HealthCheck)
 	getCommonColumnsCached := plugin.HydrateFunc(getCommonColumns).WithCache()
 	commonData, err := getCommonColumnsCached(ctx, d, h)
 	if err != nil {
@@ -246,11 +317,11 @@ func getRoute53HealthCheckTurbotAkas(ctx context.Context, d *plugin.QueryData, h
 //// TRANSFORM FUNCTIONS
 
 func route53HealthCheckTurbotTags(ctx context.Context, d *transform.TransformData) (interface{}, error) {
-	tags := d.Value.([]*route53.Tag)
+	tags := d.Value.([]types.Tag)
 
 	// Mapping the resource tags inside turbotTags
 	var turbotTagsMap map[string]string
-	if tags != nil {
+	if len(tags) > 0 {
 		turbotTagsMap = map[string]string{}
 		for _, i := range tags {
 			turbotTagsMap[*i.Key] = *i.Value
