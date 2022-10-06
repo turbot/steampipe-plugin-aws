8fd3077cf6469cf5220de3b475f03a61b0bef994
LalitTurbot <lalit@turbot.com>
2022-10-06 15:54:55 +0530

    rebase dependencies

--- aws/service_v2.go
+++ aws/service_v2.go
@@ -48,8 +48,8 @@ import (
 	"github.com/aws/aws-sdk-go-v2/service/s3"
 	"github.com/aws/aws-sdk-go-v2/service/s3control"
 	"github.com/aws/aws-sdk-go-v2/service/sns"
-	"github.com/aws/aws-sdk-go-v2/service/waf"
 	"github.com/aws/aws-sdk-go-v2/service/sqs"
+	"github.com/aws/aws-sdk-go-v2/service/waf"
 	"github.com/aws/aws-sdk-go/aws/endpoints"
 
 	"github.com/turbot/go-kit/helpers"
