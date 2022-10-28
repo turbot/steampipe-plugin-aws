c4f7efe37e3af4b6f42d527b33910a4ec15b3021
LalitTurbot <lalit@turbot.com>
2022-10-28 11:44:03 +0530

    Migrate aws_ses_domain_identity table to v2

--- aws/table_aws_ses_domain_identity.go
+++ aws/table_aws_ses_domain_identity.go
@@ -3,9 +3,8 @@ package aws
 import (
 	"context"
 
-	"github.com/aws/aws-sdk-go/aws"
-	"github.com/aws/aws-sdk-go/service/ses"
-	"github.com/turbot/go-kit/types"
+	"github.com/aws/aws-sdk-go-v2/service/ses"
+	"github.com/aws/aws-sdk-go-v2/service/ses/types"
 	"github.com/turbot/steampipe-plugin-sdk/v4/grpc/proto"
 	"github.com/turbot/steampipe-plugin-sdk/v4/plugin"
 	"github.com/turbot/steampipe-plugin-sdk/v4/plugin/transform"
@@ -76,46 +75,53 @@ func tableAwsSESDomainIdentity(_ context.Context) *plugin.Table {
 //// LIST FUNCTION
 
 func listSESDomainIdentities(ctx context.Context, d *plugin.QueryData, _ *plugin.HydrateData) (interface{}, error) {
-	logger := plugin.Logger(ctx)
-	logger.Trace("listSESDomainIdentities")
-
 	// Create Session
-	svc, err := SESService(ctx, d)
+	svc, err := SESClient(ctx, d)
 	if err != nil {
+		plugin.Logger(ctx).Error("aws_ses_domain_identity.listSESDomainIdentities", "connection_error", err)
 		return nil, err
 	}
 
-	input := &ses.ListIdentitiesInput{
-		MaxItems:     aws.Int64(1000),
-		IdentityType: aws.String(ses.IdentityTypeDomain),
-	}
-
+	maxItems := int32(1000)
 	// Limiting the results
-	limit := d.QueryContext.Limit
 	if d.QueryContext.Limit != nil {
-		if *limit < *input.MaxItems {
-			if *limit < 1 {
-				input.MaxItems = types.Int64(1)
+		limit := int32(*d.QueryContext.Limit)
+		if limit < maxItems {
+			if limit < 1 {
+				maxItems = 1
 			} else {
-				input.MaxItems = limit
+				maxItems = limit
 			}
 		}
 	}
 
-	// List call
-	err = svc.ListIdentitiesPages(
-		input,
-		func(page *ses.ListIdentitiesOutput, lastPage bool) bool {
-			for _, identity := range page.Identities {
-				d.StreamListItem(ctx, *identity)
+	input := &ses.ListIdentitiesInput{
+		MaxItems:     &maxItems,
+		IdentityType: types.IdentityTypeDomain,
+	}
 
-				// Context can be cancelled due to manual cancellation or the limit has been hit
-				if d.QueryStatus.RowsRemaining(ctx) == 0 {
-					return false
-				}
+	// List call
+	paginator := ses.NewListIdentitiesPaginator(svc, input, func(o *ses.ListIdentitiesPaginatorOptions) {
+		o.Limit = *input.MaxItems
+		o.StopOnDuplicateToken = true
+	})
+
+	for paginator.HasMorePages() {
+		output, err := paginator.NextPage(ctx)
+		if err != nil {
+			plugin.Logger(ctx).Error("aws_ses_domain_identity.listSESDomainIdentities", "api_error", err)
+			return nil, err
+		}
+
+		for _, identity := range output.Identities {
+			d.StreamListItem(ctx, identity)
+
+			// Context can be cancelled due to manual cancellation or the limit has been hit
+			if d.QueryStatus.RowsRemaining(ctx) == 0 {
+				return nil, nil
 			}
-			return !lastPage
-		},
-	)
-	return nil, err
+		}
+	}
+
+	return nil, nil
 }
