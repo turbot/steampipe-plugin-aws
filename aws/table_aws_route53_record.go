9963711469cf64714e17b6d3423b188b67d2cef9
Lalit Bhardwaj <lalit@turbot.com>
2022-11-04 16:43:13 +0530

    Update table_aws_route53_record.go

--- aws/table_aws_route53_record.go
+++ aws/table_aws_route53_record.go
@@ -190,7 +190,7 @@ func listRoute53Records(ctx context.Context, d *plugin.QueryData, _ *plugin.Hydr
 		}
 
 		for _, record := range op.ResourceRecordSets {
-			d.StreamListItem(ctx, &recordInfo{&hostedZoneID, record})
+			d.StreamListItem(ctx, &recordInfo{aws.String(hostedZoneID), record})
 
 			// Context may get cancelled due to manual cancellation or if the limit has been reached
 			if d.QueryStatus.RowsRemaining(ctx) == 0 {
