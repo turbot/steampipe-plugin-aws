70ed447e702c241f7e6a4c132022a7bcc8df4f44
ParthaI <partha@turbot.com>
2022-12-05 16:04:48 +0530

    Add table aws_vpc_verified_access_trust_provider closes #1469

---
+++ aws/table_aws_vpc_verified_access_trust_provider.go
@@ -0,0 +1,177 @@
+package aws
+
+import (
+	"context"
+
+	"github.com/aws/aws-sdk-go-v2/aws"
+	"github.com/aws/aws-sdk-go-v2/service/ec2"
+	"github.com/aws/aws-sdk-go-v2/service/ec2/types"
+	"github.com/turbot/steampipe-plugin-sdk/v4/grpc/proto"
+	"github.com/turbot/steampipe-plugin-sdk/v4/plugin"
+	"github.com/turbot/steampipe-plugin-sdk/v4/plugin/transform"
+)
+
+//// TABLE DEFINITION
+
+func tableAwsVpcVerifiedAccessTrustProvider(_ context.Context) *plugin.Table {
+	return &plugin.Table{
+		Name:        "aws_vpc_verified_access_trust_provider",
+		Description: "AWS VPC Verified Access Point",
+		List: &plugin.ListConfig{
+			Hydrate: listVpcVerifiedAccessTrustProvider,
+			IgnoreConfig: &plugin.IgnoreConfig{
+				ShouldIgnoreErrorFunc: shouldIgnoreErrors([]string{"InvalidParameterValue"}),
+			},
+			KeyColumns: []*plugin.KeyColumn{
+				{Name: "verified_access_trust_provider_id", Require: plugin.Optional},
+			},
+		},
+		GetMatrixItemFunc: BuildRegionList,
+		Columns: awsRegionalColumns([]*plugin.Column{
+			{
+				Name:        "verified_access_trust_provider_id",
+				Description: "The ID of the AWS Verified Access trust provider.",
+				Type:        proto.ColumnType_STRING,
+			},
+			{
+				Name:        "creation_time",
+				Description: "The creation time.",
+				Type:        proto.ColumnType_TIMESTAMP,
+			},
+			{
+				Name:        "description",
+				Description: "A description for the AWS Verified Access trust provider.",
+				Type:        proto.ColumnType_STRING,
+			},
+			{
+				Name:        "device_trust_provider_type",
+				Description: "The type of device-based trust provider.",
+				Type:        proto.ColumnType_STRING,
+			},
+			{
+				Name:        "last_updated_time",
+				Description: "The last updated time.",
+				Type:        proto.ColumnType_TIMESTAMP,
+			},
+			{
+				Name:        "policy_reference_name",
+				Description: "The identifier to be used when working with policy rules.",
+				Type:        proto.ColumnType_STRING,
+			},
+			{
+				Name:        "trust_provider_type",
+				Description: "The type of Verified Access trust provider.",
+				Type:        proto.ColumnType_STRING,
+			},
+			{
+				Name:        "user_trust_provider_type",
+				Description: "The type of user-based trust provider.",
+				Type:        proto.ColumnType_STRING,
+			},
+			{
+				Name:        "oidc_options",
+				Description: "The OpenID Connect details for an oidc-type, user-identity based trust provider.",
+				Type:        proto.ColumnType_JSON,
+			},
+			{
+				Name:        "tags_src",
+				Description: resourceInterfaceDescription("tags"),
+				Type:        proto.ColumnType_JSON,
+				Transform:   transform.FromField("Tags"),
+			},
+
+			// Steampipe standard columns
+			{
+				Name:        "title",
+				Description: resourceInterfaceDescription("title"),
+				Type:        proto.ColumnType_STRING,
+				Transform:   transform.FromField("VerifiedAccessTrustProviderId"),
+			},
+			{
+				Name:        "tags",
+				Description: resourceInterfaceDescription("tags"),
+				Type:        proto.ColumnType_JSON,
+				Transform:   transform.FromP(trustProviderTurbotData, "Tags"),
+			},
+		}),
+	}
+}
+
+//// LIST FUNCTION
+
+func listVpcVerifiedAccessTrustProvider(ctx context.Context, d *plugin.QueryData, _ *plugin.HydrateData) (interface{}, error) {
+
+	// Create session
+	svc, err := EC2Client(ctx, d)
+	if err != nil {
+		plugin.Logger(ctx).Error("aws_vpc_verified_access_trust_provider.listVpcVerifiedAccessTrustProvider", "connection_error", err)
+		return nil, err
+	}
+
+	// Limiting the results
+	maxLimit := int32(200)
+	if d.QueryContext.Limit != nil {
+		limit := int32(*d.QueryContext.Limit)
+		if limit < maxLimit {
+			if limit < 5 {
+				maxLimit = 5
+			} else {
+				maxLimit = limit
+			}
+		}
+	}
+
+	input := &ec2.DescribeVerifiedAccessTrustProvidersInput{
+		MaxResults: aws.Int32(maxLimit),
+	}
+
+	if d.KeyColumnQualString("verified_access_group_id") != "" {
+		input.VerifiedAccessTrustProviderIds = []string{d.KeyColumnQualString("verified_access_trust_provider_id")}
+	}
+
+	for {
+		// List call
+		resp, err := svc.DescribeVerifiedAccessTrustProviders(ctx, input)
+
+		if err != nil {
+			plugin.Logger(ctx).Error("aws_vpc_verified_access_trust_provider.listVpcVerifiedAccessTrustProvider", "api_error", err)
+			return nil, nil
+		}
+
+		for _, provider := range resp.VerifiedAccessTrustProviders {
+			d.StreamListItem(ctx, provider)
+
+			// Context may get cancelled due to manual cancellation or if the limit has been reached
+			if d.QueryStatus.RowsRemaining(ctx) == 0 {
+				return nil, nil
+			}
+		}
+		if resp.NextToken == nil {
+			break
+		} else {
+			input.NextToken = resp.NextToken
+		}
+	}
+
+	return nil, err
+}
+
+//// TRANSFORM FUNCTIONS
+
+func trustProviderTurbotData(_ context.Context, d *transform.TransformData) (interface{}, error) {
+	accessPoint := d.HydrateItem.(types.VerifiedAccessTrustProvider)
+
+	// Get the resource tags
+	var turbotTagsMap map[string]string
+	if accessPoint.Tags != nil {
+		turbotTagsMap = map[string]string{}
+		for _, i := range accessPoint.Tags {
+			turbotTagsMap[*i.Key] = *i.Value
+			// if *i.Key == "Name" {
+			// 	title = i.Value
+			// }
+		}
+	}
+
+	return turbotTagsMap, nil
+}
--- go.mod
+++ go.mod
@@ -4,7 +4,7 @@ go 1.19
 
 require (
 	github.com/aws/aws-sdk-go v1.44.150
-	github.com/aws/aws-sdk-go-v2 v1.17.1
+	github.com/aws/aws-sdk-go-v2 v1.17.2
 	github.com/aws/aws-sdk-go-v2/config v1.17.8
 	github.com/aws/aws-sdk-go-v2/credentials v1.12.21
 	github.com/aws/aws-sdk-go-v2/service/accessanalyzer v1.16.0
@@ -38,7 +38,7 @@ require (
 	github.com/aws/aws-sdk-go-v2/service/dlm v1.12.4
 	github.com/aws/aws-sdk-go-v2/service/docdb v1.19.11
 	github.com/aws/aws-sdk-go-v2/service/dynamodb v1.15.9
-	github.com/aws/aws-sdk-go-v2/service/ec2 v1.72.1
+	github.com/aws/aws-sdk-go-v2/service/ec2 v1.74.1
 	github.com/aws/aws-sdk-go-v2/service/ecr v1.17.16
 	github.com/aws/aws-sdk-go-v2/service/ecrpublic v1.13.15
 	github.com/aws/aws-sdk-go-v2/service/ecs v1.18.19
@@ -105,7 +105,7 @@ require (
 	github.com/aws/aws-sdk-go-v2/service/wafv2 v1.22.9
 	github.com/aws/aws-sdk-go-v2/service/wellarchitected v1.16.11
 	github.com/aws/aws-sdk-go-v2/service/workspaces v1.23.0
-	github.com/aws/smithy-go v1.13.4
+	github.com/aws/smithy-go v1.13.5
 	github.com/gocarina/gocsv v0.0.0-20201208093247-67c824bc04d4
 	github.com/golang/protobuf v1.5.2
 	github.com/turbot/go-kit v0.4.0
@@ -121,14 +121,14 @@ require (
 	github.com/apparentlymart/go-textseg/v13 v13.0.0 // indirect
 	github.com/aws/aws-sdk-go-v2/aws/protocol/eventstream v1.4.8 // indirect
 	github.com/aws/aws-sdk-go-v2/feature/ec2/imds v1.12.17 // indirect
-	github.com/aws/aws-sdk-go-v2/internal/configsources v1.1.25 // indirect
-	github.com/aws/aws-sdk-go-v2/internal/endpoints/v2 v2.4.19 // indirect
+	github.com/aws/aws-sdk-go-v2/internal/configsources v1.1.26 // indirect
+	github.com/aws/aws-sdk-go-v2/internal/endpoints/v2 v2.4.20 // indirect
 	github.com/aws/aws-sdk-go-v2/internal/ini v1.3.24 // indirect
 	github.com/aws/aws-sdk-go-v2/internal/v4a v1.0.14 // indirect
 	github.com/aws/aws-sdk-go-v2/service/internal/accept-encoding v1.9.3 // indirect
 	github.com/aws/aws-sdk-go-v2/service/internal/checksum v1.1.9 // indirect
 	github.com/aws/aws-sdk-go-v2/service/internal/endpoint-discovery v1.7.8 // indirect
-	github.com/aws/aws-sdk-go-v2/service/internal/presigned-url v1.9.19 // indirect
+	github.com/aws/aws-sdk-go-v2/service/internal/presigned-url v1.9.20 // indirect
 	github.com/aws/aws-sdk-go-v2/service/internal/s3shared v1.13.8 // indirect
 	github.com/aws/aws-sdk-go-v2/service/sso v1.11.23 // indirect
 	github.com/aws/aws-sdk-go-v2/service/ssooidc v1.13.6 // indirect
