a981372cf6be15ee5cd76fe35ee3659b0606265a
ParthaI <47887552+ParthaI@users.noreply.github.com>
2022-12-15 21:51:26 +0530

    Add table aws_vpc_verified_access_endpoint closes #1470 (#1491)
    Co-authored-by: Ved misra <47312748+misraved@users.noreply.github.com>

--- aws/plugin.go
+++ aws/plugin.go
@@ -394,6 +394,7 @@ func Plugin(ctx context.Context) *plugin.Plugin {
 			"aws_vpc_security_group":                                       tableAwsVpcSecurityGroup(ctx),
 			"aws_vpc_security_group_rule":                                  tableAwsVpcSecurityGroupRule(ctx),
 			"aws_vpc_subnet":                                               tableAwsVpcSubnet(ctx),
+			"aws_vpc_verified_access_endpoint":                             tableAwsVpcVerifiedAccessEndpoint(ctx),
 			"aws_vpc_verified_access_instance":                             tableAwsVpcVerifiedAccessInstance(ctx),
 			"aws_vpc_verified_access_trust_provider":                       tableAwsVpcVerifiedAccessTrustProvider(ctx),
 			"aws_vpc_vpn_connection":                                       tableAwsVpcVpnConnection(ctx),
