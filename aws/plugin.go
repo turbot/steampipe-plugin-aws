570d0a7c5420b2b86f1ef1f71cc40de47280a594
Subhajit Das <subhajit.das@turbot.com>
2022-12-22 17:03:32 +0530

    Add table aws_drs_job. closes #1458 (#1492)
    Co-authored-by: Ved misra <47312748+misraved@users.noreply.github.com>

--- aws/plugin.go
+++ aws/plugin.go
@@ -125,6 +125,7 @@ func Plugin(ctx context.Context) *plugin.Plugin {
 			"aws_dlm_lifecycle_policy":                                     tableAwsDLMLifecyclePolicy(ctx),
 			"aws_dms_replication_instance":                                 tableAwsDmsReplicationInstance(ctx),
 			"aws_docdb_cluster":                                            tableAwsDocDBCluster(ctx),
+			"aws_drs_job":                                                  tableAwsDRSJob(ctx),
 			"aws_drs_source_server":                                        tableAwsDRSSourceServer(ctx),
 			"aws_dynamodb_backup":                                          tableAwsDynamoDBBackup(ctx),
 			"aws_dynamodb_global_table":                                    tableAwsDynamoDBGlobalTable(ctx),
