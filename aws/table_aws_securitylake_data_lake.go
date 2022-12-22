69eafb09bc1534ada21c35ee9d4c26bafcc60cda
ParthaI <partha@turbot.com>
2022-12-22 21:25:37 +0530

    Add table aws_securitylake_data_lake closes #1441

---
+++ aws/table_aws_securitylake_data_lake.go
@@ -0,0 +1,106 @@
+package aws
+
+import (
+	"context"
+
+	"github.com/aws/aws-sdk-go-v2/service/securitylake"
+	"github.com/turbot/steampipe-plugin-sdk/v4/grpc/proto"
+	"github.com/turbot/steampipe-plugin-sdk/v4/plugin"
+	"github.com/turbot/steampipe-plugin-sdk/v4/plugin/transform"
+)
+
+//// TABLE DEFINITION
+
+func tableAwsSecurityLakeDataLake(_ context.Context) *plugin.Table {
+	return &plugin.Table{
+		Name:        "aws_securitylake_data_lake",
+		Description: "AWS Security Lake Data Lake",
+		List: &plugin.ListConfig{
+			Hydrate: getSecurityLakeDataLake,
+		},
+		GetMatrixItemFunc: BuildRegionList,
+		Columns: awsRegionalColumns([]*plugin.Column{
+			{
+				Name:        "encryption_key",
+				Description: "The type of encryption key used by Security Lake to encrypt the lake configuration.",
+				Type:        proto.ColumnType_STRING,
+			},
+			{
+				Name:        "replication_role_arn",
+				Description: "Replication settings for the Amazon S3 buckets. This parameter uses the IAM role created by you that is managed by Security Lake, to ensure the replication setting is correct.",
+				Type:        proto.ColumnType_STRING,
+			},
+			{
+				Name:        "s3_bucket_arn",
+				Description: "Amazon Resource Names (ARNs) uniquely identify Amazon Web Services resources.",
+				Type:        proto.ColumnType_STRING,
+			},
+			{
+				Name:        "status",
+				Description: "Retrieves the status of the configuration operation for an account in Amazon Security Lake.",
+				Type:        proto.ColumnType_STRING,
+			},
+			{
+				Name:        "replication_destination_regions",
+				Description: "Replication enables automatic, asynchronous copying of objects across Amazon S3 buckets.",
+				Type:        proto.ColumnType_JSON,
+			},
+			{
+				Name:        "retention_settings",
+				Description: "Retention settings for the destination Amazon S3 buckets.",
+				Type:        proto.ColumnType_JSON,
+			},
+
+			// Steampipe standard columns
+			{
+				Name:        "tags",
+				Description: resourceInterfaceDescription("tags"),
+				Type:        proto.ColumnType_STRING,
+				Transform:   transform.FromField("TagsMap"),
+			},
+		}),
+	}
+}
+
+//// LIST FUNCTION
+
+func getSecurityLakeDataLake(ctx context.Context, d *plugin.QueryData, _ *plugin.HydrateData) (interface{}, error) {
+
+	// Create Client
+	svc, err := SecurityLakeClient(ctx, d)
+	if err != nil {
+		plugin.Logger(ctx).Error("aws_securitylake_data_lake.listSecurityLakeDataLakes", "client_error", err)
+		return nil, err
+	}
+	if svc == nil {
+		// Unsupported region check
+		return nil, nil
+	}
+
+	// Limiting the results
+	maxLimit := int32(100)
+	if d.QueryContext.Limit != nil {
+		limit := int32(*d.QueryContext.Limit)
+		if limit < maxLimit {
+			if limit < 1 {
+				maxLimit = 1
+			} else {
+				maxLimit = limit
+			}
+		}
+	}
+
+	input := &securitylake.GetDatalakeInput{}
+
+	resp, err := svc.GetDatalake(ctx, input)
+	if err != nil {
+		plugin.Logger(ctx).Error("aws_securitylake_data_lake.listSecurityLakeDataLakes", "api_error", err)
+		return nil, err
+	}
+
+	for _, v := range resp.Configurations {
+		d.StreamLeafListItem(ctx, v)
+	}
+
+	return nil, nil
+}
---
+++ docs/tables/aws_securitylake_data_lake.md
@@ -0,0 +1,49 @@
+# Table: aws_securitylake_data_lake
+
+Amazon Security Lake automatically centralizes your security data from the cloud, on-premises, and custom sources into a data lake stored in your account.
+
+## Examples
+
+### Basic info
+
+```sql
+select
+  encryption_key,
+  replication_role_arn,
+  s3_bucket_arn,
+  status
+from
+  aws_securitylake_data_lake;
+```
+
+### Get S3 bucket details for each data lake
+
+```sql
+select
+  distinct b.name as bucket_name,
+  l.s3_bucket_arn,
+  b.creation_date,
+  b.bucket_policy_is_public,
+  b.versioning_enabled,
+  b.block_public_acls
+from
+  aws_securitylake_data_lake as l,
+  aws_s3_bucket as b
+where
+  l.s3_bucket_arn = b.arn;
+```
+
+## Get retention setting details of data lakes
+
+```sql
+select
+  l.encryption_key,
+  l.replication_role_arn,
+  l.s3_bucket_arn,
+  l.status,
+  r ->> 'RetentionPeriod' as retention_period,
+  r ->> 'StorageClass' as storage_class
+from
+  aws_securitylake_data_lake as l,
+  jsonb_array_elements(retention_settings) as r;
+```
