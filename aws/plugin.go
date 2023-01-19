f97e59c83ceab9ac3c2288c34dc34b2f7a214c3e
ParthaI <47887552+ParthaI@users.noreply.github.com>
2023-01-19 20:55:51 +0530

    Add table aws_health_affected_entity closes #1410 (#1525)
    Co-authored-by: Ved misra <47312748+misraved@users.noreply.github.com>

--- aws/plugin.go
+++ aws/plugin.go
@@ -242,6 +242,7 @@ func Plugin(ctx context.Context) *plugin.Plugin {
 			"aws_guardduty_member":                                         tableAwsGuardDutyMember(ctx),
 			"aws_guardduty_publishing_destination":                         tableAwsGuardDutyPublishingDestination(ctx),
 			"aws_guardduty_threat_intel_set":                               tableAwsGuardDutyThreatIntelSet(ctx),
+			"aws_health_affected_entity":                                   tableAwsHealthAffectedEntity(ctx),
 			"aws_health_event":                                             tableAwsHealthEvent(ctx),
 			"aws_iam_access_advisor":                                       tableAwsIamAccessAdvisor(ctx),
 			"aws_iam_access_key":                                           tableAwsIamAccessKey(ctx),
