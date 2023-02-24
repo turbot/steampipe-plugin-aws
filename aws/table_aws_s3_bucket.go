83b68eeb8929e977a05f7e2c774ff5ca91bf9fc8
Madhushree Ray <65847563+madhushreeray30@users.noreply.github.com>
2023-02-24 22:29:20 +0530

    Add column `website_configuration` in table `aws_s3_bucket` (#1618)
    Co-authored-by: Ved misra <47312748+misraved@users.noreply.github.com>

--- aws/table_aws_s3_bucket.go
+++ aws/table_aws_s3_bucket.go
@@ -76,6 +76,10 @@ func tableAwsS3Bucket(_ context.Context) *plugin.Table {
 				Func:    getS3BucketObjectOwnershipControl,
 				Depends: []plugin.HydrateFunc{getBucketLocation},
 			},
+			{
+				Func:    getBucketWebsite,
+				Depends: []plugin.HydrateFunc{getBucketLocation},
+			},
 		},
 		Columns: awsAccountColumns([]*plugin.Column{
 			{
@@ -213,6 +217,13 @@ func tableAwsS3Bucket(_ context.Context) *plugin.Table {
 				Type:        proto.ColumnType_JSON,
 				Hydrate:     getBucketReplication,
 				Transform:   transform.FromField("ReplicationConfiguration"),
+			},
+			{
+				Name:        "website_configuration",
+				Description: "The website configuration information of the bucket.",
+				Type:        proto.ColumnType_JSON,
+				Hydrate:     getBucketWebsite,
+				Transform:   transform.FromValue(),
 			},
 			{
 				Name:        "tags_src",
@@ -734,6 +745,34 @@ func getBucketTagging(ctx context.Context, d *plugin.QueryData, h *plugin.Hydrat
 	}
 
 	return bucketTags, nil
+}
+
+func getBucketWebsite(ctx context.Context, d *plugin.QueryData, h *plugin.HydrateData) (interface{}, error) {
+	// Bucket location will be nil if getBucketLocation returned an error but
+	// was ignored through ignore_error_codes config arg
+	if h.HydrateResults["getBucketLocation"] == nil {
+		return nil, nil
+	}
+
+	bucket := h.Item.(types.Bucket)
+	location := h.HydrateResults["getBucketLocation"].(*s3.GetBucketLocationOutput)
+
+	// Create client
+	svc, err := S3Client(ctx, d, string(location.LocationConstraint))
+	if err != nil {
+		plugin.Logger(ctx).Error("aws_s3_bucket.getBucketWebsite", "client_error", err)
+		return nil, err
+	}
+
+	params := &s3.GetBucketWebsiteInput{Bucket: bucket.Name}
+
+	bucketwebsites, _ := svc.GetBucketWebsite(ctx, params)
+	if err != nil {
+		plugin.Logger(ctx).Error("aws_s3_bucket.getBucketWebsite", "api_error", err)
+		return nil, err
+	}
+
+	return bucketwebsites, nil
 }
 
 func getBucketARN(ctx context.Context, d *plugin.QueryData, h *plugin.HydrateData) (interface{}, error) {
