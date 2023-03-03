b0168c49fd61690e281f977f940d16ebfaf7eb05
ParthaI <47887552+ParthaI@users.noreply.github.com>
2023-03-03 22:16:09 +0530

    Add table aws_servicecatalog_portfolio. closes #1635 (#1636)
    Co-authored-by: Ved misra <47312748+misraved@users.noreply.github.com>

--- aws/plugin.go
+++ aws/plugin.go
@@ -369,6 +369,7 @@ func Plugin(ctx context.Context) *plugin.Plugin {
 			"aws_securitylake_data_lake":                                   tableAwsSecurityLakeDataLake(ctx),
 			"aws_securitylake_subscriber":                                  tableAwsSecurityLakeSubscriber(ctx),
 			"aws_serverlessapplicationrepository_application":              tableAwsServerlessApplicationRepositoryApplication(ctx),
+			"aws_servicecatalog_portfolio":                                 tableAwsServicecatalogPortfolio(ctx),
 			"aws_servicequotas_default_service_quota":                      tableAwsServiceQuotasDefaultServiceQuota(ctx),
 			"aws_servicequotas_service_quota_change_request":               tableAwsServiceQuotasServiceQuotaChangeRequest(ctx),
 			"aws_servicequotas_service_quota":                              tableAwsServiceQuotasServiceQuota(ctx),
