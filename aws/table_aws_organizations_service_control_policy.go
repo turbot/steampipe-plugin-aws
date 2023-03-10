aa905fbaf934d331970f26202fdf042cba881ac1
Karan Popat <karan@turbot.com>
2023-03-10 13:02:53 +0530

    Remove comments

--- aws/table_aws_organizations_service_control_policy.go
+++ aws/table_aws_organizations_service_control_policy.go
@@ -24,23 +24,7 @@ func tableAwsOrganizationsServiceControlPolicy(_ context.Context) *plugin.Table 
 			Hydrate: getOrganizationsServiceControlPolicy,
 		},
 		List: &plugin.ListConfig{
-			Hydrate: listOrganizationsServiceControlPolicies,
-			/*
-				Specifies the type of policy that you want to include in the response. You must
-				specify one of the following values:
-
-				* AISERVICES_OPT_OUT_POLICY
-					(https://docs.aws.amazon.com/organizations/latest/userguide/orgs_manage_policies_ai-opt-out.html)
-
-				* BACKUP_POLICY
-					(https://docs.aws.amazon.com/organizations/latest/userguide/orgs_manage_policies_backup.html)
-
-				* SERVICE_CONTROL_POLICY
-					(https://docs.aws.amazon.com/organizations/latest/userguide/orgs_manage_policies_scp.html)
-
-				* TAG_POLICY
-					(https://docs.aws.amazon.com/organizations/latest/userguide/orgs_manage_policies_tag-policies.html)
-			*/
+			Hydrate:    listOrganizationsServiceControlPolicies,
 			KeyColumns: plugin.SingleColumn("type"),
 			IgnoreConfig: &plugin.IgnoreConfig{
 				ShouldIgnoreErrorFunc: shouldIgnoreErrors([]string{"InvalidInputException"}),
