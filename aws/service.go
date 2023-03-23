9f4295ca38bfd605d7fb1d1dc576af14871dc68c
Quentin Nambot <quentin.nambot@bedrockstreaming.com>
2023-03-23 16:25:20 +0100

    style: run go fmt

--- aws/service.go
+++ aws/service.go
@@ -22,7 +22,7 @@ import (
 	"github.com/aws/aws-sdk-go-v2/service/apigatewayv2"
 	"github.com/aws/aws-sdk-go-v2/service/appconfig"
 	"github.com/aws/aws-sdk-go-v2/service/applicationautoscaling"
-        "github.com/aws/aws-sdk-go-v2/service/athena"
+	"github.com/aws/aws-sdk-go-v2/service/athena"
 	"github.com/aws/aws-sdk-go-v2/service/auditmanager"
 	"github.com/aws/aws-sdk-go-v2/service/autoscaling"
 	"github.com/aws/aws-sdk-go-v2/service/backup"
@@ -272,11 +272,11 @@ func ApplicationAutoScalingClient(ctx context.Context, d *plugin.QueryData) (*ap
 }
 
 func AthenaClient(ctx context.Context, d *plugin.QueryData) (*athena.Client, error) {
-        cfg, err := getClientForQueryRegion(ctx, d)
-        if err != nil {
-                return nil, err
-        }
-        return athena.NewFromConfig(*cfg), nil
+	cfg, err := getClientForQueryRegion(ctx, d)
+	if err != nil {
+		return nil, err
+	}
+	return athena.NewFromConfig(*cfg), nil
 }
 
 func AuditManagerClient(ctx context.Context, d *plugin.QueryData) (*auditmanager.Client, error) {
--- aws/table_aws_athena_workgroup.go
+++ aws/table_aws_athena_workgroup.go
@@ -8,7 +8,7 @@ import (
 	"github.com/aws/aws-sdk-go-v2/service/athena"
 	"github.com/aws/aws-sdk-go-v2/service/athena/types"
 
-        athenav1 "github.com/aws/aws-sdk-go/service/athena"
+	athenav1 "github.com/aws/aws-sdk-go/service/athena"
 
 	"github.com/turbot/steampipe-plugin-sdk/v5/grpc/proto"
 	"github.com/turbot/steampipe-plugin-sdk/v5/plugin"
@@ -28,122 +28,122 @@ func tableAwsAthenaWorkGroup(_ context.Context) *plugin.Table {
 		},
 		GetMatrixItemFunc: SupportedRegionMatrix(athenav1.EndpointsID),
 		Columns: awsRegionalColumns([]*plugin.Column{
-                        {
-                                Name:        "name",
-                                Description: "The workgroup name.",
-                                Type:        proto.ColumnType_STRING,
-                        },
-                        {
-                                Name:        "description",
-                                Description: "The workgroup description.",
-                                Type:        proto.ColumnType_STRING,
-                        },
-                        {
-                                Name:        "creation_time",
-                                Description: "The date and time the workgroup was created.",
-                                Type:        proto.ColumnType_TIMESTAMP,
-                        },
-                        {
-                                Name:        "state",
-                                Description: "The state of the workgroup.",
-                                Type:        proto.ColumnType_STRING,
-                        },
-                        {
-                                Name:        "additional_configuration",
-                                Description: "Specifies a user defined JSON string that is passed to the notebook engine.",
-                                Type:        proto.ColumnType_JSON,
-                                Hydrate:     getAwsAthenaWorkGroup,
-                                Transform:   transform.FromField("Configuration.AdditionalConfiguration"),
-                        },
-                        {
-                                Name:        "bytes_scanned_cutoff_per_query",
-                                Description: "The upper data usage limit (cutoff) for the amount of bytes a single query in a workgroup is allowed to scan.",
-                                Type:        proto.ColumnType_INT,
-                                Hydrate:     getAwsAthenaWorkGroup,
-                                Transform:   transform.FromField("Configuration.BytesScannedCutoffPerQuery"),
-                        },
-                        {
-                                Name:        "customer_content_kms_key",
-                                Description: "Specifies the KMS key that is used to encrypt the user's data stores in Athena.",
-                                Type:        proto.ColumnType_STRING,
-                                Hydrate:     getAwsAthenaWorkGroup,
-                                Transform:   transform.FromField("Configuration.CustomerContentEncryptionConfiguration.KmsKey"),
-                        },
-                        {
-                                Name:        "enforce_workgroup_configuration",
-                                Description: "If set to \"true\", the settings for the workgroup override client-side settings.",
-                                Type:        proto.ColumnType_BOOL,
-                                Hydrate:     getAwsAthenaWorkGroup,
-                                Transform:   transform.FromField("Configuration.EnforceWorkGroupConfiguration"),
-                        },
-                        {
-                                Name:        "effective_engine_version",
-                                Description: "The engine version on which the query runs.",
-                                Type:        proto.ColumnType_STRING,
-                                Transform:   transform.FromField("Configuration.EngineVersion.EffectiveEngineVersion"),
-                        },
-                        {
-                                Name:        "selected_engine_version",
-                                Description: "The engine version requested by the user.",
-                                Type:        proto.ColumnType_STRING,
-                                Transform:   transform.FromField("Configuration.EngineVersion.SelectedEngineVersion"),
-                        },
-                        {
-                                Name:        "execution_role",
-                                Description: "Role used in a notebook session for accessing the user's resources.",
-                                Type:        proto.ColumnType_STRING,
-                                Hydrate:     getAwsAthenaWorkGroup,
-                                Transform:   transform.FromField("Configuration.ExecutionRole"),
-                        },
-                        {
-                                Name:        "publish_cloudwatch_metrics_enabled",
-                                Description: "Indicates that the Amazon CloudWatch metrics are enabled for the workgroup.",
-                                Type:        proto.ColumnType_BOOL,
-                                Hydrate:     getAwsAthenaWorkGroup,
-                                Transform:   transform.FromField("Configuration.PublishCloudWatchMetricsEnabled"),
-                        },
-                        {
-                                Name:        "requester_pays_enabled",
-                                Description: "If set to true, allows members assigned to a workgroup to reference Amazon S3 Requester Pays buckets in queries.",
-                                Type:        proto.ColumnType_BOOL,
-                                Hydrate:     getAwsAthenaWorkGroup,
-                                Transform:   transform.FromField("Configuration.RequesterPaysEnabled"),
-                        },
-                        {
-                                Name:        "s3_acl_option",
-                                Description: "The Amazon S3 canned ACL that Athena should specify when storing query results.",
-                                Type:        proto.ColumnType_STRING,
-                                Hydrate:     getAwsAthenaWorkGroup,
-                                Transform:   transform.FromField("Configuration.ResultConfiguration.AclConfiguration.S3AclOption"),
-                        },
-                        {
-                                Name:        "encryption_option",
-                                Description: "Indicates whether Amazon S3 server-side encryption with Amazon S3-managed keys (SSE_S3), server-side encryption with KMS-managed keys (SSE_KMS), or client-side encryption with KMS-managed keys (CSE_KMS) is used.",
-                                Type:        proto.ColumnType_STRING,
-                                Hydrate:     getAwsAthenaWorkGroup,
-                                Transform:   transform.FromField("Configuration.ResultConfiguration.EncryptionConfiguration.EncryptionOption"),
-                        },
-                        {
-                                Name:        "result_configuration_kms_key",
-                                Description: "For SSE_KMS and CSE_KMS, this is the KMS key ARN or ID.",
-                                Type:        proto.ColumnType_STRING,
-                                Hydrate:     getAwsAthenaWorkGroup,
-                                Transform:   transform.FromField("Configuration.ResultConfiguration.EncryptionConfiguration.KmsKey"),
-                        },
-                        {
-                                Name:        "expected_bucket_owner",
-                                Description: "The Amazon Web Services account ID that you expect to be the owner of the Amazon S3 bucket specified by ResultConfiguration$OutputLocation.",
-                                Type:        proto.ColumnType_STRING,
-                                Hydrate:     getAwsAthenaWorkGroup,
-                                Transform:   transform.FromField("Configuration.ResultConfiguration.ExpectedBucketOwner"),
-                        },
-                        {
-                                Name:        "output_location",
-                                Description: "The location in Amazon S3 where your query results are stored.",
-                                Type:        proto.ColumnType_STRING,
-                                Hydrate:     getAwsAthenaWorkGroup,
-                                Transform:   transform.FromField("Configuration.ResultConfiguration.OutputLocation"),
-                        },
+			{
+				Name:        "name",
+				Description: "The workgroup name.",
+				Type:        proto.ColumnType_STRING,
+			},
+			{
+				Name:        "description",
+				Description: "The workgroup description.",
+				Type:        proto.ColumnType_STRING,
+			},
+			{
+				Name:        "creation_time",
+				Description: "The date and time the workgroup was created.",
+				Type:        proto.ColumnType_TIMESTAMP,
+			},
+			{
+				Name:        "state",
+				Description: "The state of the workgroup.",
+				Type:        proto.ColumnType_STRING,
+			},
+			{
+				Name:        "additional_configuration",
+				Description: "Specifies a user defined JSON string that is passed to the notebook engine.",
+				Type:        proto.ColumnType_JSON,
+				Hydrate:     getAwsAthenaWorkGroup,
+				Transform:   transform.FromField("Configuration.AdditionalConfiguration"),
+			},
+			{
+				Name:        "bytes_scanned_cutoff_per_query",
+				Description: "The upper data usage limit (cutoff) for the amount of bytes a single query in a workgroup is allowed to scan.",
+				Type:        proto.ColumnType_INT,
+				Hydrate:     getAwsAthenaWorkGroup,
+				Transform:   transform.FromField("Configuration.BytesScannedCutoffPerQuery"),
+			},
+			{
+				Name:        "customer_content_kms_key",
+				Description: "Specifies the KMS key that is used to encrypt the user's data stores in Athena.",
+				Type:        proto.ColumnType_STRING,
+				Hydrate:     getAwsAthenaWorkGroup,
+				Transform:   transform.FromField("Configuration.CustomerContentEncryptionConfiguration.KmsKey"),
+			},
+			{
+				Name:        "enforce_workgroup_configuration",
+				Description: "If set to \"true\", the settings for the workgroup override client-side settings.",
+				Type:        proto.ColumnType_BOOL,
+				Hydrate:     getAwsAthenaWorkGroup,
+				Transform:   transform.FromField("Configuration.EnforceWorkGroupConfiguration"),
+			},
+			{
+				Name:        "effective_engine_version",
+				Description: "The engine version on which the query runs.",
+				Type:        proto.ColumnType_STRING,
+				Transform:   transform.FromField("Configuration.EngineVersion.EffectiveEngineVersion"),
+			},
+			{
+				Name:        "selected_engine_version",
+				Description: "The engine version requested by the user.",
+				Type:        proto.ColumnType_STRING,
+				Transform:   transform.FromField("Configuration.EngineVersion.SelectedEngineVersion"),
+			},
+			{
+				Name:        "execution_role",
+				Description: "Role used in a notebook session for accessing the user's resources.",
+				Type:        proto.ColumnType_STRING,
+				Hydrate:     getAwsAthenaWorkGroup,
+				Transform:   transform.FromField("Configuration.ExecutionRole"),
+			},
+			{
+				Name:        "publish_cloudwatch_metrics_enabled",
+				Description: "Indicates that the Amazon CloudWatch metrics are enabled for the workgroup.",
+				Type:        proto.ColumnType_BOOL,
+				Hydrate:     getAwsAthenaWorkGroup,
+				Transform:   transform.FromField("Configuration.PublishCloudWatchMetricsEnabled"),
+			},
+			{
+				Name:        "requester_pays_enabled",
+				Description: "If set to true, allows members assigned to a workgroup to reference Amazon S3 Requester Pays buckets in queries.",
+				Type:        proto.ColumnType_BOOL,
+				Hydrate:     getAwsAthenaWorkGroup,
+				Transform:   transform.FromField("Configuration.RequesterPaysEnabled"),
+			},
+			{
+				Name:        "s3_acl_option",
+				Description: "The Amazon S3 canned ACL that Athena should specify when storing query results.",
+				Type:        proto.ColumnType_STRING,
+				Hydrate:     getAwsAthenaWorkGroup,
+				Transform:   transform.FromField("Configuration.ResultConfiguration.AclConfiguration.S3AclOption"),
+			},
+			{
+				Name:        "encryption_option",
+				Description: "Indicates whether Amazon S3 server-side encryption with Amazon S3-managed keys (SSE_S3), server-side encryption with KMS-managed keys (SSE_KMS), or client-side encryption with KMS-managed keys (CSE_KMS) is used.",
+				Type:        proto.ColumnType_STRING,
+				Hydrate:     getAwsAthenaWorkGroup,
+				Transform:   transform.FromField("Configuration.ResultConfiguration.EncryptionConfiguration.EncryptionOption"),
+			},
+			{
+				Name:        "result_configuration_kms_key",
+				Description: "For SSE_KMS and CSE_KMS, this is the KMS key ARN or ID.",
+				Type:        proto.ColumnType_STRING,
+				Hydrate:     getAwsAthenaWorkGroup,
+				Transform:   transform.FromField("Configuration.ResultConfiguration.EncryptionConfiguration.KmsKey"),
+			},
+			{
+				Name:        "expected_bucket_owner",
+				Description: "The Amazon Web Services account ID that you expect to be the owner of the Amazon S3 bucket specified by ResultConfiguration$OutputLocation.",
+				Type:        proto.ColumnType_STRING,
+				Hydrate:     getAwsAthenaWorkGroup,
+				Transform:   transform.FromField("Configuration.ResultConfiguration.ExpectedBucketOwner"),
+			},
+			{
+				Name:        "output_location",
+				Description: "The location in Amazon S3 where your query results are stored.",
+				Type:        proto.ColumnType_STRING,
+				Hydrate:     getAwsAthenaWorkGroup,
+				Transform:   transform.FromField("Configuration.ResultConfiguration.OutputLocation"),
+			},
 		}),
 	}
 }
@@ -162,7 +162,7 @@ func listAwsAthenaWorkGroups(ctx context.Context, d *plugin.QueryData, _ *plugin
 		return nil, nil
 	}
 
-	maxResults := int32(50) // TODO
+	maxResults := int32(50)
 
 	// Reduce the basic request limit down if the user has only requested a small number of rows
 	if d.QueryContext.Limit != nil {
@@ -176,37 +176,37 @@ func listAwsAthenaWorkGroups(ctx context.Context, d *plugin.QueryData, _ *plugin
 		}
 	}
 
-        listWorkGroupsInput := athena.ListWorkGroupsInput{MaxResults: &maxResults}
-        workGroupsPaginator := athena.NewListWorkGroupsPaginator(svc, &listWorkGroupsInput, func(o *athena.ListWorkGroupsPaginatorOptions) {
-                o.Limit = maxResults
-                o.StopOnDuplicateToken = true
-        })
+	listWorkGroupsInput := athena.ListWorkGroupsInput{MaxResults: &maxResults}
+	workGroupsPaginator := athena.NewListWorkGroupsPaginator(svc, &listWorkGroupsInput, func(o *athena.ListWorkGroupsPaginatorOptions) {
+		o.Limit = maxResults
+		o.StopOnDuplicateToken = true
+	})
 
-        for workGroupsPaginator.HasMorePages() {
-                output, err := workGroupsPaginator.NextPage(ctx)
-                if err != nil {
-                        plugin.Logger(ctx).Error("aws_athena_query_execution.listAwsAthenaWorkGroups", "api_error", err)
-                        return nil, err
-                }
+	for workGroupsPaginator.HasMorePages() {
+		output, err := workGroupsPaginator.NextPage(ctx)
+		if err != nil {
+			plugin.Logger(ctx).Error("aws_athena_query_execution.listAwsAthenaWorkGroups", "api_error", err)
+			return nil, err
+		}
 
-                for _, workGroupSummary := range output.WorkGroups {
-                        configuration := types.WorkGroupConfiguration{EngineVersion: workGroupSummary.EngineVersion}
-                        workGroup := types.WorkGroup{
-                                Name: workGroupSummary.Name,
-                                CreationTime: workGroupSummary.CreationTime,
-                                Description: workGroupSummary.Description,
-                                State: workGroupSummary.State,
-                                Configuration: &configuration,
-                        }
+		for _, workGroupSummary := range output.WorkGroups {
+			configuration := types.WorkGroupConfiguration{EngineVersion: workGroupSummary.EngineVersion}
+			workGroup := types.WorkGroup{
+				Name:          workGroupSummary.Name,
+				CreationTime:  workGroupSummary.CreationTime,
+				Description:   workGroupSummary.Description,
+				State:         workGroupSummary.State,
+				Configuration: &configuration,
+			}
 
-                        d.StreamListItem(ctx, workGroup)
+			d.StreamListItem(ctx, workGroup)
 
-                        // Context may get cancelled due to manual cancellation or if the limit has been reached
-                        if d.RowsRemaining(ctx) == 0 {
-                                return nil, nil
-                        }
-                }
-        }
+			// Context may get cancelled due to manual cancellation or if the limit has been reached
+			if d.RowsRemaining(ctx) == 0 {
+				return nil, nil
+			}
+		}
+	}
 
 	return nil, nil
 }
