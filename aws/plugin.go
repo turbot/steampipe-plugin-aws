5ee329d7d8ab75c8a82bfb118d7b33598b265bc2
Ved misra <47312748+misraved@users.noreply.github.com>
2023-05-16 19:42:58 +0530

    Add table aws_ec2_client_vpn_endpoint closes #1717 (#1722)

--- aws/plugin.go
+++ aws/plugin.go
@@ -162,6 +162,7 @@ func Plugin(ctx context.Context) *plugin.Plugin {
 			"aws_ec2_autoscaling_group":                                    tableAwsEc2ASG(ctx),
 			"aws_ec2_capacity_reservation":                                 tableAwsEc2CapacityReservation(ctx),
 			"aws_ec2_classic_load_balancer":                                tableAwsEc2ClassicLoadBalancer(ctx),
+			"aws_ec2_client_vpn_endpoint":                                  tableAwsEC2ClientVPNEndpoint(ctx),
 			"aws_ec2_gateway_load_balancer":                                tableAwsEc2GatewayLoadBalancer(ctx),
 			"aws_ec2_instance_availability":                                tableAwsInstanceAvailability(ctx),
 			"aws_ec2_instance_metric_cpu_utilization_daily":                tableAwsEc2InstanceMetricCpuUtilizationDaily(ctx),
