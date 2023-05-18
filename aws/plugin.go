4eba2ff66a0d19cb411a25afdd5eb75c9c6092e6
ParthaI <47887552+ParthaI@users.noreply.github.com>
2023-05-18 17:58:33 +0530

    Add table aws_service_discovery_service #1730 (#1739)
    Co-authored-by: Ved misra <47312748+misraved@users.noreply.github.com>

--- aws/plugin.go
+++ aws/plugin.go
@@ -385,6 +385,7 @@ func Plugin(ctx context.Context) *plugin.Plugin {
 			"aws_servicecatalog_portfolio":                                 tableAwsServicecatalogPortfolio(ctx),
 			"aws_servicecatalog_product":                                   tableAwsServicecatalogProduct(ctx),
 			"aws_service_discovery_namespace":                              tableAwsServiceDiscoveryNamespace(ctx),
+			"aws_service_discovery_service":                                tableAwsServiceDiscoveryService(ctx),
 			"aws_servicequotas_default_service_quota":                      tableAwsServiceQuotasDefaultServiceQuota(ctx),
 			"aws_servicequotas_service_quota_change_request":               tableAwsServiceQuotasServiceQuotaChangeRequest(ctx),
 			"aws_servicequotas_service_quota":                              tableAwsServiceQuotasServiceQuota(ctx),
