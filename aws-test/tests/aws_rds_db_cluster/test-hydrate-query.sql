86223a85960cc3338645a9c38adac047d4239262
ParthaI <partha@turbot.com>
2023-06-13 18:49:14 +0530

    Removed the parameter which will keep updating depending upon the terraform provider version

--- aws-test/tests/aws_rds_db_cluster/test-hydrate-query.sql
+++ aws-test/tests/aws_rds_db_cluster/test-hydrate-query.sql
@@ -1,3 +1,3 @@
-select db_cluster_identifier, arn, resource_id, allocated_storage, copy_tags_to_snapshot, cross_account_clone, db_subnet_group, deletion_protection, endpoint, engine, engine_mode, engine_version, global_write_forwarding_requested, http_endpoint_enabled, iam_database_authentication_enabled, master_user_name, multi_az, port, storage_encrypted, tags_src
+select db_cluster_identifier, arn, resource_id, allocated_storage, copy_tags_to_snapshot, cross_account_clone, db_subnet_group, deletion_protection, endpoint, engine, engine_mode, global_write_forwarding_requested, http_endpoint_enabled, iam_database_authentication_enabled, master_user_name, multi_az, port, storage_encrypted, tags_src
 from aws.aws_rds_db_cluster
 where db_cluster_identifier = '{{ resourceName }}'
