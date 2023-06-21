3ff6006394d514be8d01c150b1d57eafbd9c7bef
ParthaI <partha@turbot.com>
2023-06-21 12:32:44 +0530

    Add table aws_s3_bucket_intelligent_tiering_configuration Closes #1786

--- aws/plugin.go
+++ aws/plugin.go
@@ -372,6 +372,7 @@ func Plugin(ctx context.Context) *plugin.Plugin {
 			"aws_s3_account_settings":                                      tableAwsS3AccountSettings(ctx),
 			"aws_s3_bucket":                                                tableAwsS3Bucket(ctx),
 			"aws_s3_object":                                                tableAwsS3Object(ctx),
+			"aws_s3_bucket_intelligent_tiering_configuration":              tableAwsS3BucketIntelligentTieringConfiguration(ctx),
 			"aws_s3_multi_region_access_point":                             tableAwsS3MultiRegionAccessPoint(ctx),
 			"aws_sagemaker_app":                                            tableAwsSageMakerApp(ctx),
 			"aws_sagemaker_domain":                                         tableAwsSageMakerDomain(ctx),
