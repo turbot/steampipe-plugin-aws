d36bd01cce3c25a1581587619c3908e4569d2158
Madhushree Ray <65847563+madhushreeray30@users.noreply.github.com>
2023-07-28 19:08:32 +0530

    Add table `aws_directory_service_log_subscription`. Closes #1824 (#1852)
    Co-authored-by: ParthaI <47887552+ParthaI@users.noreply.github.com>

--- aws/plugin.go
+++ aws/plugin.go
@@ -141,6 +141,7 @@ func Plugin(ctx context.Context) *plugin.Plugin {
 			"aws_dax_subnet_group":                                         tableAwsDaxSubnetGroup(ctx),
 			"aws_directory_service_certificate":                            tableAwsDirectoryServiceCertificate(ctx),
 			"aws_directory_service_directory":                              tableAwsDirectoryServiceDirectory(ctx),
+			"aws_directory_service_log_subscription":                       tableAwsDirectoryServiceLogSubscription(ctx),
 			"aws_dlm_lifecycle_policy":                                     tableAwsDLMLifecyclePolicy(ctx),
 			"aws_dms_replication_instance":                                 tableAwsDmsReplicationInstance(ctx),
 			"aws_docdb_cluster":                                            tableAwsDocDBCluster(ctx),
