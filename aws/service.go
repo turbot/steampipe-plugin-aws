012e1b13a47df88cbb555d58ad5822527f7f1956
misraved <ved@turbot.com>
2023-08-24 00:33:56 +0530

    FIx the message

--- aws/service.go
+++ aws/service.go
@@ -1705,11 +1705,11 @@ func getBaseClientForAccountUncached(ctx context.Context, d *plugin.QueryData, _
 		profile := aws.ToString(awsSpcConfig.Profile)
 		plugin.Logger(ctx).Trace("getBaseClientForAccountUncached", "connection_name", d.Connection.Name, "status", "profile_found", "profile", profile)
 
-		// Check connection config has a valid aws profile
+		// Check if connection config has a valid AWS profile
 		cfg, _ := config.LoadDefaultConfig(ctx, config.WithSharedConfigProfile(profile))
 		_, err := cfg.Credentials.Retrieve(ctx)
 		if err != nil {
-			return nil, fmt.Errorf("connection config has an invalid AWS profile '%v', Please update the configuration file", profile)
+			return nil, fmt.Errorf("connection config has an invalid AWS profile '%v'. Please update the configuration file.", profile)
 		}
 
 		configOptions = append(configOptions, config.WithSharedConfigProfile(profile))
