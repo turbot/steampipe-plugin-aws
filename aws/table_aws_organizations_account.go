2c9eb657a9da12f65a5a5264965f8cdc32988b1e
badideasforsale <80412585+badideasforsale@users.noreply.github.com>
2023-10-23 11:44:58 -0700

    Fixed a typo in the description of a column

--- aws/table_aws_organizations_account.go
+++ aws/table_aws_organizations_account.go
@@ -36,7 +36,7 @@ func tableAwsOrganizationsAccount(_ context.Context) *plugin.Table {
 		Columns: awsGlobalRegionColumns([]*plugin.Column{
 			{
 				Name:        "name",
-				Description: "The description of the permission set.",
+				Description: "The name of the member account.",
 				Type:        proto.ColumnType_STRING,
 			},
 			// This description has added text for better clarification on ID type
