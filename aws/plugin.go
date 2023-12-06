8549c2c55492668cacbec7a4d2060615ee1dc57f
ParthaI <partha@turbot.com>
2023-12-06 12:28:27 +0530

    Add table aws_iot_thing_type Closes #1994

--- aws/plugin.go
+++ aws/plugin.go
@@ -301,6 +301,7 @@ func Plugin(ctx context.Context) *plugin.Plugin {
 			"aws_inspector_assessment_template":                            tableAwsInspectorAssessmentTemplate(ctx),
 			"aws_inspector_exclusion":                                      tableAwsInspectorExclusion(ctx),
 			"aws_inspector_finding":                                        tableAwsInspectorFinding(ctx),
+			"aws_iot_thing_type":                                           tableAwsIotThingType(ctx),
 			"aws_kinesis_consumer":                                         tableAwsKinesisConsumer(ctx),
 			"aws_kinesis_firehose_delivery_stream":                         tableAwsKinesisFirehoseDeliveryStream(ctx),
 			"aws_kinesis_stream":                                           tableAwsKinesisStream(ctx),
---
+++ aws/table_aws_iot_thing_type.go
@@ -0,0 +1,251 @@
+package aws
+
+import (
+	"context"
+
+	"github.com/aws/aws-sdk-go-v2/aws"
+	"github.com/aws/aws-sdk-go-v2/service/iot"
+	"github.com/aws/aws-sdk-go-v2/service/iot/types"
+
+	iotv1 "github.com/aws/aws-sdk-go/service/iot"
+
+	"github.com/turbot/steampipe-plugin-sdk/v5/grpc/proto"
+	"github.com/turbot/steampipe-plugin-sdk/v5/plugin"
+	"github.com/turbot/steampipe-plugin-sdk/v5/plugin/transform"
+)
+
+//// TABLE DEFINITION
+
+func tableAwsIotThingType(_ context.Context) *plugin.Table {
+	return &plugin.Table{
+		Name:        "aws_iot_thing_type",
+		Description: "AWS Iot Thing Type",
+		Get: &plugin.GetConfig{
+			KeyColumns: plugin.SingleColumn("thing_type_name"),
+			Hydrate:    getIotThingType,
+			Tags:       map[string]string{"service": "iot", "action": "DescribeThingType"},
+			IgnoreConfig: &plugin.IgnoreConfig{
+				ShouldIgnoreErrorFunc: shouldIgnoreErrors([]string{"ResourceNotFoundException"}),
+			},
+		},
+		List: &plugin.ListConfig{
+			Hydrate: listIotThingTypes,
+			Tags: map[string]string{"service": "iot", "action": "ListThingTypes"},
+		},
+		GetMatrixItemFunc: SupportedRegionMatrix(iotv1.EndpointsID),
+		Columns: awsRegionalColumns([]*plugin.Column{
+			{
+				Name:        "thing_type_name",
+				Description: "The name of the thing type.",
+				Type:        proto.ColumnType_STRING,
+			},
+			{
+				Name:        "arn",
+				Description: "The Amazon Resource Name (ARN) for the thing type.",
+				Type:        proto.ColumnType_STRING,
+				Transform:   transform.FromField("ThingTypeArn"),
+			},
+			{
+				Name:        "thing_type_id",
+				Description: "The thing type ID.",
+				Type:        proto.ColumnType_STRING,
+				Hydrate:     getIotThingType,
+			},
+			{
+				Name:        "thing_type_description",
+				Description: "The description of the thing type.",
+				Type:        proto.ColumnType_STRING,
+				Transform:   transform.FromField("ThingTypeProperties.ThingTypeDescription"),
+			},
+			{
+				Name:        "creation_date",
+				Description: "The UNIX timestamp of when the thing type was created.",
+				Type:        proto.ColumnType_TIMESTAMP,
+				Transform:   transform.FromField("ThingTypeMetadata.CreationDate"),
+			},
+			{
+				Name:        "deprecated",
+				Description: "Whether the thing type is deprecated. If true, no new things could be associated with this type.",
+				Type:        proto.ColumnType_STRING,
+				Transform:   transform.FromField("ThingTypeMetadata.Deprecated"),
+			},
+			{
+				Name:        "deprecation_date",
+				Description: "The date and time when the thing type was deprecated.",
+				Type:        proto.ColumnType_TIMESTAMP,
+				Transform:   transform.FromField("ThingTypeMetadata.DeprecationDate"),
+			},
+			{
+				Name:        "searchable_attributes",
+				Description: "A list of searchable thing attribute names.",
+				Type:        proto.ColumnType_JSON,
+				Hydrate:     getIotThingType,
+				Transform:   transform.FromField("ThingTypeProperties.SearchableAttributes"),
+			},
+			{
+				Name:        "tags_src",
+				Description: "A list of tags currently associated with the thing type.",
+				Type:        proto.ColumnType_JSON,
+				Hydrate:     getIotThingTypeTags,
+				Transform:   transform.FromField("Tags"),
+			},
+
+			// Steampipe standard columns
+			{
+				Name:        "title",
+				Description: resourceInterfaceDescription("title"),
+				Type:        proto.ColumnType_STRING,
+				Transform:   transform.FromField("ThingTypeName"),
+			},
+			{
+				Name:        "tags",
+				Description: resourceInterfaceDescription("tags"),
+				Type:        proto.ColumnType_JSON,
+				Hydrate:     getIotThingTypeTags,
+				Transform:   transform.From(iotThingTypeTagListToTagsMap),
+			},
+			{
+				Name:        "akas",
+				Description: resourceInterfaceDescription("akas"),
+				Type:        proto.ColumnType_JSON,
+				Transform:   transform.FromField("ThingTypeArn").Transform(transform.EnsureStringArray),
+			},
+		}),
+	}
+}
+
+//// LIST FUNCTION
+
+func listIotThingTypes(ctx context.Context, d *plugin.QueryData, _ *plugin.HydrateData) (interface{}, error) {
+	// Create Session
+	svc, err := IOTClient(ctx, d)
+	if err != nil {
+		plugin.Logger(ctx).Error("aws_iot_thing_type.listIotThingTypes", "connection_error", err)
+		return nil, err
+	}
+	if svc == nil {
+		// Unsupported region, return no data
+		return nil, nil
+	}
+
+	// Limiting the results
+	maxLimit := int32(250)
+	if d.QueryContext.Limit != nil {
+		limit := int32(*d.QueryContext.Limit)
+		if limit < maxLimit {
+			maxLimit = limit
+		}
+	}
+
+	input := &iot.ListThingTypesInput{
+		MaxResults: aws.Int32(maxLimit),
+	}
+
+	paginator := iot.NewListThingTypesPaginator(svc, input, func(o *iot.ListThingTypesPaginatorOptions) {
+		o.Limit = maxLimit
+		o.StopOnDuplicateToken = true
+	})
+
+	// List call
+	for paginator.HasMorePages() {
+		// apply rate limiting
+		d.WaitForListRateLimit(ctx)
+
+		output, err := paginator.NextPage(ctx)
+		if err != nil {
+			plugin.Logger(ctx).Error("aws_iot_thing_type.listIotThingTypes", "api_error", err)
+			return nil, err
+		}
+
+		for _, item := range output.ThingTypes {
+			d.StreamListItem(ctx, item)
+
+			// Context can be cancelled due to manual cancellation or the limit has been hit
+			if d.RowsRemaining(ctx) == 0 {
+				return nil, nil
+			}
+		}
+	}
+
+	return nil, err
+}
+
+//// HYDRATE FUNCTIONS
+
+func getIotThingType(ctx context.Context, d *plugin.QueryData, h *plugin.HydrateData) (interface{}, error) {
+	typeName := ""
+	if h.Item != nil {
+		t := h.Item.(types.ThingTypeDefinition)
+		typeName = *t.ThingTypeName
+	} else {
+		typeName = d.EqualsQualString("thing_type_name")
+	}
+
+	if typeName == "" {
+		return nil, nil
+	}
+
+	// Create service
+	svc, err := IOTClient(ctx, d)
+	if err != nil {
+		plugin.Logger(ctx).Error("aws_iot_thing_type.getIotThingType", "connection_error", err)
+		return nil, err
+	}
+
+	params := &iot.DescribeThingTypeInput{
+		ThingTypeName: aws.String(typeName),
+	}
+
+	resp, err := svc.DescribeThingType(ctx, params)
+	if err != nil {
+		plugin.Logger(ctx).Error("aws_iot_thing_type.getIotThingType", "api_error", err)
+		return nil, err
+	}
+
+	return resp, nil
+}
+
+func getIotThingTypeTags(ctx context.Context, d *plugin.QueryData, h *plugin.HydrateData) (interface{}, error) {
+	typeArn := ""
+	switch item := h.Item.(type) {
+	case *iot.DescribeThingTypeOutput:
+		typeArn = *item.ThingTypeArn
+	case types.ThingTypeDefinition:
+		typeArn = *item.ThingTypeArn
+	}
+
+	// Create service
+	svc, err := IOTClient(ctx, d)
+	if err != nil {
+		plugin.Logger(ctx).Error("aws_iot_thing_type.getIotThingTypeTags", "connection_error", err)
+		return nil, err
+	}
+
+	params := &iot.ListTagsForResourceInput{
+		ResourceArn: aws.String(typeArn),
+	}
+
+	endpointTags, err := svc.ListTagsForResource(ctx, params)
+	if err != nil {
+		plugin.Logger(ctx).Error("aws_iot_thing_type.getIotThingTypeTags", "api_error", err)
+		return nil, err
+	}
+
+	return endpointTags, nil
+}
+
+//// TRANSFORM FUNCTIONS
+
+func iotThingTypeTagListToTagsMap(_ context.Context, d *transform.TransformData) (interface{}, error) {
+	data := d.HydrateItem.(*iot.ListTagsForResourceOutput)
+
+	// Mapping the resource tags inside turbotTags
+	if data.Tags != nil {
+		turbotTagsMap := map[string]string{}
+		for _, i := range data.Tags {
+			turbotTagsMap[*i.Key] = *i.Value
+		}
+		return turbotTagsMap, nil
+	}
+	return nil, nil
+}
