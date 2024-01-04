e60adf5bbaf8a0d9e7a4196382a0d547234e87f5
misraved <ved@turbot.com>
2024-01-04 12:06:51 +0530

    Update description

--- aws/table_aws_cloudwatch_log_stream.go
+++ aws/table_aws_cloudwatch_log_stream.go
@@ -88,7 +88,7 @@ func tableAwsCloudwatchLogStream(_ context.Context) *plugin.Table {
 			},
 			{
 				Name:        "order_by",
-				Description: "If the value is LogStreamName, the results are ordered by log stream name. If the value is LastEventTime, the results are ordered by the event time. The default value is LogStreamName.If you order the results by event time, you cannot specify the logStreamNamePrefix parameter. lastEventTimestamp represents the time of the most recent log event in the log stream in CloudWatch Logs.",
+				Description: "If the value is LogStreamName, the results are ordered by log stream name. If the value is LastEventTime, the results are ordered by the event time. The default value is LogStreamName. If you order the results by event time, you cannot specify the logStreamNamePrefix parameter. LastEventTimestamp represents the time of the most recent log event in the log stream in CloudWatch Logs.",
 				Type:        proto.ColumnType_STRING,
 				Transform:   transform.FromQual("order_by"),
 				Default:     "LogStreamName",
