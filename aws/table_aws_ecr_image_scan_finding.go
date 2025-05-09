0f82380d155d65c4fda899a8c7ecb5a5e776c61d
ParthaI <partha@turbot.com>
2025-05-09 13:22:41 +0530

    update

--- aws/table_aws_ecr_image_scan_finding.go
+++ aws/table_aws_ecr_image_scan_finding.go
@@ -35,8 +35,8 @@ func tableAwsEcrImageScanFinding(_ context.Context) *plugin.Table {
 			// image_digest as it's more common/friendly to use.
 			KeyColumns: []*plugin.KeyColumn{
 				{Name: "repository_name", Require: plugin.Required},
-				{Name: "image_tag", Require: plugin.AnyOf},
-				{Name: "image_digest", Require: plugin.AnyOf},
+				{Name: "image_tag", Require: plugin.Optional},
+				{Name: "image_digest", Require: plugin.Optional},
 			},
 		},
 		GetMatrixItemFunc: SupportedRegionMatrix(ecrv1.EndpointsID),
@@ -132,6 +132,10 @@ func listAwsEcrImageScanFindings(ctx context.Context, d *plugin.QueryData, h *pl
 	imageDigest := d.EqualsQuals["image_digest"]
 	repositoryName := d.EqualsQuals["repository_name"]
 
+	if imageTag == nil && imageDigest == nil {
+		return nil, errors.New("image_tag or image_digest must be provided")
+	}
+	
 	// Limiting the results
 	maxLimit := int32(1000)
 	if d.QueryContext.Limit != nil {
