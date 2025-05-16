d009484512eef8fed8312b7691f61f660a485b94
Muhammad Fuad Bin Abdullah <67743055+FuadAbdullah@users.noreply.github.com>
2025-05-16 13:12:31 +0800

    Add table aws_delegate_admin (#2421) (#2477)

---
+++ aws/table_aws_organizations_delegated_services_for_account.go
@@ -0,0 +1,122 @@
+package aws
+
+import (
+	"context"
+
+	"github.com/aws/aws-sdk-go-v2/service/organizations"
+	"github.com/aws/aws-sdk-go-v2/service/organizations/types"
+
+	"github.com/turbot/steampipe-plugin-sdk/v5/grpc/proto"
+	"github.com/turbot/steampipe-plugin-sdk/v5/plugin"
+	"github.com/turbot/steampipe-plugin-sdk/v5/plugin/transform"
+)
+
+func tableAwsOrganizationsDelegatedServicesForAccount(_ context.Context) *plugin.Table {
+
+	return &plugin.Table{
+		Name:        "aws_organizations_delegated_services_for_account",
+		Description: "AWS Organizations Delegated Services For Account",
+		List: &plugin.ListConfig{
+			ParentHydrate: listOrganizationsDelegatedAdmins, // Use Delegated Administrator as parent per recommendation. Referenced table_aws_cloudwatch_log_stream. This function can be found in table_aws_organizations_delegated_administrator
+			Hydrate:       listDelegatedServices,
+			Tags:          map[string]string{"service": "organizations", "action": "ListDelegatedServicesForAccount"},
+			KeyColumns: []*plugin.KeyColumn{ // Make delegated_account_id optional, user can still query `where` using this column.
+				{Name: "delegated_account_id", Require: plugin.Optional},
+			},
+		},
+		Columns: awsGlobalRegionColumns([]*plugin.Column{
+			{
+				Name:        "delegated_account_id",
+				Description: "The unique identifier (account ID) of the delegated administrator account for which services are listed.",
+				Type:        proto.ColumnType_STRING,
+			},
+			{
+				Name:        "service_principal",
+				Description: "The service principal delegated to the administrator account.",
+				Type:        proto.ColumnType_STRING,
+			},
+			{
+				Name:        "delegation_enabled_date",
+				Description: "The date when the delegation was enabled.",
+				Type:        proto.ColumnType_TIMESTAMP,
+			},
+
+			// Standard columns for all tables
+			{
+				Name:        "title",
+				Description: resourceInterfaceDescription("title"),
+				Type:        proto.ColumnType_STRING,
+				Transform:   transform.FromField("ServicePrincipal"),
+			},
+		}),
+	}
+}
+
+func listDelegatedServices(ctx context.Context, d *plugin.QueryData, h *plugin.HydrateData) (interface{}, error) {
+	delegatedAccount := h.Item.(types.DelegatedAdministrator) // Get delegated administrator details
+
+	delegatedAccountId := d.EqualsQualString("delegated_account_id") // Get delegated_account_id from where statement
+
+	// Minimize API calls
+	if delegatedAccountId != "" {
+		if delegatedAccountId != *delegatedAccount.Id {
+			return nil, nil
+		}
+	}
+
+	// Get Client
+	svc, err := OrganizationClient(ctx, d)
+	if err != nil {
+		plugin.Logger(ctx).Error("aws_organizations_delegated_services_for_account.ListDelegatedServicesForAccount", "client_error", err)
+		return nil, err
+	}
+
+	// Limiting the result
+	maxItems := int32(20)
+
+	// Reduce the page size if a smaller limit is provided
+	if d.QueryContext.Limit != nil {
+		limit := int32(*d.QueryContext.Limit)
+		if limit < maxItems {
+			maxItems = limit
+		}
+	}
+	params := &organizations.ListDelegatedServicesForAccountInput{
+		AccountId:  delegatedAccount.Id,
+		MaxResults: &maxItems,
+	}
+
+	paginator := organizations.NewListDelegatedServicesForAccountPaginator(svc, params, func(o *organizations.ListDelegatedServicesForAccountPaginatorOptions) {
+		o.Limit = maxItems
+		o.StopOnDuplicateToken = true
+	})
+
+	for paginator.HasMorePages() {
+		output, err := paginator.NextPage(ctx)
+		if err != nil {
+			plugin.Logger(ctx).Error("aws_organizations_delegated_services_for_account.ListDelegatedServicesForAccount", "api_error", err)
+			return nil, err
+		}
+
+		for _, service := range output.DelegatedServices {
+			// Stream a new struct that includes the AccountId used for the API call
+			d.StreamListItem(ctx, delegatedServiceInfo{
+				DelegatedService:   service,
+				DelegatedAccountId: *delegatedAccount.Id,
+			})
+
+			// Context may get cancelled due to manual cancellation or if the limit has been reached
+			if d.RowsRemaining(ctx) == 0 {
+				return nil, nil
+			}
+		}
+	}
+
+	return nil, nil
+}
+
+// Define a struct to hold the DelegatedService and the AccountId used to fetch it.\
+type delegatedServiceInfo struct {
+	types.DelegatedService
+	DelegatedAccountId string
+}
---
+++ docs/tables/aws_organizations_delegated_services_for_account.md
@@ -0,0 +1,65 @@
+---
+title: "Steampipe Table: aws_organizations_delegated_services_for_account - Query AWS Organizations Delegated Services for an Account using SQL"
+description: "Allows users to query AWS Organizations delegated services for a specific account, providing details on which services have been granted delegated administrative privileges."
+folder: "Organizations"
+---
+
+# Table: aws_organizations_delegated_services_for_account - Query AWS Organizations Delegated Services for an Account using SQL
+
+The AWS Organizations Delegated Services for an Account is a resource that allows you to query information about the services that have been delegated administrative privileges for a given AWS account within an AWS Organization service. This allows you to query details about these services, including the service principal and the date the delegation was enabled. This is useful for managing and auditing delegated service administration within your organization.
+
+## Table Usage Guide
+
+The `aws_organizations_delegated_administrator` table in Steampipe lets you query the services granted delegated administration access to a specific account. You specify the delegated account ID, and the table returns a list of services and their delegation status. The schema outlines various attributes of each delegated service.
+
+**Important Notes:**
+- This table requires specifying a `delegated_account_id` to retrieve data.  It doesn't list all delegated services across all accounts in the organization.
+- The `delegated_account_id` is the ID of the account which has been granted delegated administration, *not* the management account making the API calls.
+
+## Examples
+
+### Basic info
+Retrieve basic information about all delegated services.
+
+```sql+postgres
+select
+  delegated_account_id,
+  service_principal,
+  delegation_enabled_date
+from
+  aws_organizations_delegated_services_for_account
+```
+
+```sql+sqlite
+select
+  delegated_account_id,
+  service_principal,
+  delegation_enabled_date
+from
+  aws_organizations_delegated_services_for_account
+```
+
+### Basic info for a specific account
+Retrieve basic information about all delegated services for a specific account.
+
+```sql+postgres
+select
+  delegated_account_id,
+  service_principal,
+  delegation_enabled_date
+from
+  aws_organizations_delegated_services_for_account
+where
+  delegated_account_id = '123456789012';
+```
+
+```sql+sqlite
+select
+  delegated_account_id,
+  service_principal,
+  delegation_enabled_date
+from
+  aws_organizations_delegated_services_for_account
+where
+  delegated_account_id = '123456789012';
+```
