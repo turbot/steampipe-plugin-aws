883e945e8fa4cf7be51f1111e562921d4cc0b3f9
ParthaI <partha@turbot.com>
2025-05-22 11:42:21 +0530

    Updated the missing  column description

--- aws/table_aws_cost_usage.go
+++ aws/table_aws_cost_usage.go
@@ -76,19 +76,19 @@ func tableAwsCostAndUsage(_ context.Context) *plugin.Table {
 				// Quals columns - to filter the lookups
 				{
 					Name:        "granularity",
-					Description: "",
+					Description: "The AWS cost granularity. Valid values are DAILY, MONTHLY, or HOURLY.",
 					Type:        proto.ColumnType_STRING,
 					Hydrate:     hydrateCostAndUsageQuals,
 				},
 				{
 					Name:        "dimension_type_1",
-					Description: "",
+					Description: "The first dimension to group results by. Valid values include AZ, INSTANCE_TYPE, LINKED_ACCOUNT, LINKED_ACCOUNT_NAME, OPERATION, PURCHASE_TYPE, REGION, SERVICE, SERVICE_CODE, USAGE_TYPE, USAGE_TYPE_GROUP, RECORD_TYPE, OPERATING_SYSTEM, TENANCY, SCOPE, PLATFORM, SUBSCRIPTION_ID, LEGAL_ENTITY_NAME, DEPLOYMENT_OPTION, DATABASE_ENGINE, CACHE_ENGINE, INSTANCE_TYPE_FAMILY, BILLING_ENTITY, RESERVATION_ID, RESOURCE_ID, RIGHTSIZING_TYPE, SAVINGS_PLANS_TYPE, SAVINGS_PLAN_ARN, PAYMENT_OPTION.",
 					Type:        proto.ColumnType_STRING,
 					Hydrate:     hydrateCostAndUsageQuals,
 				},
 				{
 					Name:        "dimension_type_2",
-					Description: "",
+					Description: "The second dimension to group results by. Valid values include AZ, INSTANCE_TYPE, LINKED_ACCOUNT, LINKED_ACCOUNT_NAME, OPERATION, PURCHASE_TYPE, REGION, SERVICE, SERVICE_CODE, USAGE_TYPE, USAGE_TYPE_GROUP, RECORD_TYPE, OPERATING_SYSTEM, TENANCY, SCOPE, PLATFORM, SUBSCRIPTION_ID, LEGAL_ENTITY_NAME, DEPLOYMENT_OPTION, DATABASE_ENGINE, CACHE_ENGINE, INSTANCE_TYPE_FAMILY, BILLING_ENTITY, RESERVATION_ID, RESOURCE_ID, RIGHTSIZING_TYPE, SAVINGS_PLANS_TYPE, SAVINGS_PLAN_ARN, PAYMENT_OPTION.",
 					Type:        proto.ColumnType_STRING,
 					Hydrate:     hydrateCostAndUsageQuals,
 				},
