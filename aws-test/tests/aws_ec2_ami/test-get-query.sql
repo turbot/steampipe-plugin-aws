ac94a1c89fdfda81112793ce0bfc2aa3e8aaee4a
Steffen Gebert <steffen.gebert@emnify.com>
2026-06-21 15:44:40 +0200

    Extend `aws_ec2_ami` table
    AWS has added a couple of fields to DescribeImageOutput,
    among them the time when an image was launch last,
    as well as AMI ancestor information.

--- aws-test/tests/aws_ec2_ami/test-get-query.sql
+++ aws-test/tests/aws_ec2_ami/test-get-query.sql
@@ -1,3 +1,3 @@
-select name, image_id, image_type, image_location, architecture, description, ena_support, hypervisor, owner_id, platform_details, public, root_device_name, root_device_type, sriov_net_support, usage_operation, virtualization_type, block_device_mappings, tags_src
+select name, image_id, image_type, image_location, architecture, description, ena_support, hypervisor, owner_id, platform_details, public, root_device_name, root_device_type, sriov_net_support, usage_operation, virtualization_type, block_device_mappings, tags_src, deregistration_protection, free_tier_eligible, image_allowed, source_image_id, source_image_region, last_launched_time
 from aws.aws_ec2_ami
 where image_id = '{{ output.resource_id.value }}'
